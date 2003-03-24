668fd648d4304fe400faf625893a7b2e111c96bc
Emmanuel Anne <emanne@users.sourceforge.net>
2003-03-24 17:25:20 +0000

    keyboard now uses scancodes instead of ascii codes

--- src/includes/keymap.h
+++ src/includes/keymap.h
@@ -2,8 +2,11 @@
   Hatari
 */
 
+#include <SDL_keyboard.h>
+
 extern char Keymap_RemapKeyToSTScanCode(unsigned int Key);
 extern void Keymap_LoadRemapFile(char *pszFileName);
 extern void Keymap_DebounceAllKeys(void);
-extern void Keymap_KeyDown(unsigned int sdlkey, unsigned int sdlmod);
-extern void Keymap_KeyUp(unsigned int sdlkey, unsigned int sdlmod);
+extern void Keymap_KeyDown(SDL_keysym *sdlkey);
+extern void Keymap_KeyUp(SDL_keysym *sdlkey);
+extern void Keymap_FromScancodes();
