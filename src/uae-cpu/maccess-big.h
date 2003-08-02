fb3b7ce8e7a587d830127bf123a385e1e7bbb941
Thomas Huth <thothy@users.sourceforge.net>
2003-08-02 15:45:48 +0000

    Added accelerated memory access functions.

--- src/uae-cpu/maccess-big.h
+++ src/uae-cpu/maccess-big.h
@@ -1,7 +1,7 @@
  /*
   * UAE - The Un*x Amiga Emulator - CPU core
   *
-  * Generic memory access functions.
+  * Memory access functions for big endian systems like the PowerPC.
   *
   * Copyright 1996 Bernd Schmidt
   *
@@ -11,30 +11,17 @@
   * your option any later version. Read the file gpl.txt for details.
   */
 
-/* Try to use accelerated memory access functions: */
-#if defined(__GNUC__) && defined(__i386__) && !defined(__STRICT_ANSI__)
-# include "maccess-i86.h"
-#elif defined(powerpc) || defined(__mc68000__) || defined(__mc68020__)
-# include "maccess-big.h"
-#endif
-
-
 #ifndef UAE_MACCESS_H
 #define UAE_MACCESS_H
 
-
 static __inline__ uae_u32 do_get_mem_long(uae_u32 *a)
 {
-    uae_u8 *b = (uae_u8 *)a;
-    
-    return (*b << 24) | (*(b+1) << 16) | (*(b+2) << 8) | (*(b+3));
+    return *a;
 }
 
 static __inline__ uae_u16 do_get_mem_word(uae_u16 *a)
 {
-    uae_u8 *b = (uae_u8 *)a;
-    
-    return (*b << 8) | (*(b+1));
+    return *a;
 }
 
 static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
@@ -44,20 +31,12 @@ static __inline__ uae_u8 do_get_mem_byte(uae_u8 *a)
 
 static __inline__ void do_put_mem_long(uae_u32 *a, uae_u32 v)
 {
-    uae_u8 *b = (uae_u8 *)a;
-    
-    *b = v >> 24;
-    *(b+1) = v >> 16;    
-    *(b+2) = v >> 8;
-    *(b+3) = v;
+    *a = v;
 }
 
 static __inline__ void do_put_mem_word(uae_u16 *a, uae_u16 v)
 {
-    uae_u8 *b = (uae_u8 *)a;
-    
-    *b = v >> 8;
-    *(b+1) = v;
+    *a = v;
 }
 
 static __inline__ void do_put_mem_byte(uae_u8 *a, uae_u8 v)
