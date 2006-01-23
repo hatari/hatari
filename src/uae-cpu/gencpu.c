8b3a60d484e51a1ddc05c5e96f3369c1846a03db
Thomas Huth <thothy@users.sourceforge.net>
2006-01-23 19:47:49 +0000

    Improved PEA and LEA cycles again (thanks to Ijor for the explanation)

--- src/uae-cpu/gencpu.c
+++ src/uae-cpu/gencpu.c
@@ -22,7 +22,7 @@
  * This file is distributed under the GNU Public License, version 2 or at
  * your option any later version. Read the file gpl.txt for details.
  */
-char GenCpu_rcsid[] = "Hatari $Id: gencpu.c,v 1.9 2005-12-25 18:30:23 thothy Exp $";
+char GenCpu_rcsid[] = "Hatari $Id: gencpu.c,v 1.10 2006-01-23 19:47:49 thothy Exp $";
 
 #include <ctype.h>
 #include <string.h>
@@ -1474,9 +1474,9 @@ static void gen_opcode (unsigned long int opcode)
 	genamode (curi->dmode, "dstreg", curi->size, "dst", 2, 0);
 	genastore ("srca", curi->dmode, "dstreg", curi->size, "dst");
         /* Set correct cycles: According to the M68K User Manual, LEA takes 12
-         * cycles in Ad8r and PC8r mode, but it takes 16 cycles on a real ST: */
+         * cycles in Ad8r and PC8r mode, but it takes 14 (or 16) cycles on a real ST: */
         if (curi->smode == Ad8r || curi->smode == PC8r)
-          insn_n_cycles = 16;
+          insn_n_cycles = 14;
 	break;
     case i_PEA:
 	genamode (curi->smode, "srcreg", curi->size, "src", 0, 0);
@@ -1488,14 +1488,14 @@ static void gen_opcode (unsigned long int opcode)
           case Aind:  insn_n_cycles=12; break;
           case Ad16:  insn_n_cycles=16; break;
           /* Note: according to the M68K User Manual, PEA takes 20 cycles for
-           * the Ad8r mode, but on a real ST, it takes 24 cycles! */
-          case Ad8r:  insn_n_cycles=24; break;
+           * the Ad8r mode, but on a real ST, it takes 22 (or 24) cycles! */
+          case Ad8r:  insn_n_cycles=22; break;
           case absw:  insn_n_cycles=16; break;
           case absl:  insn_n_cycles=20; break;
           case PC16:  insn_n_cycles=16; break;
           /* Note: PEA with PC8r takes 20 cycles according to the User Manual,
-           * but it takes 24 cycles on a real ST: */
-          case PC8r:  insn_n_cycles=24; break;
+           * but it takes 22 (or 24) cycles on a real ST: */
+          case PC8r:  insn_n_cycles=22; break;
          }
 	break;
     case i_DBcc:
