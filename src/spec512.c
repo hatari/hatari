45b6f3025c9372de7854340b6db174d9761303fd
Thomas Huth <thothy@users.sourceforge.net>
2006-08-30 19:54:38 +0000

    Sourcecode beautification

--- src/spec512.c
+++ src/spec512.c
@@ -18,7 +18,7 @@
   very simple. Speed is a problem, though, as the palette can change once every
   4 pixels - that's a lot of processing.
 */
-const char Spec512_rcsid[] = "Hatari $Id: spec512.c,v 1.15 2006-02-23 21:00:43 thothy Exp $";
+const char Spec512_rcsid[] = "Hatari $Id: spec512.c,v 1.16 2006-08-30 19:54:38 thothy Exp $";
 
 #include <SDL_byteorder.h>
 
@@ -35,10 +35,11 @@ const char Spec512_rcsid[] = "Hatari $Id: spec512.c,v 1.15 2006-02-23 21:00:43 t
 /* Store writes to palette by cycles per scan line, colour and index in ST */
 typedef struct
 {
-  int LineCycles;       /* Number of cycles into line (MUST be div by 4) */
-  Uint16 Colour;        /* ST Colour value */
-  Uint16 Index;         /* Index into ST palette (0...15) */
-} CYCLEPALETTE;
+	int LineCycles;       /* Number of cycles into line (MUST be div by 4) */
+	Uint16 Colour;        /* ST Colour value */
+	Uint16 Index;         /* Index into ST palette (0...15) */
+}
+CYCLEPALETTE;
 
 /* 314k; 1024-bytes per line */
 static CYCLEPALETTE CyclePalettes[(MAX_SCANLINES_PER_FRAME+1)*MAX_CYCLEPALETTES_PERLINE];
@@ -51,7 +52,10 @@ static int nScanLine, ScanLineCycleCount;
 static BOOL bIsSpec512Display;
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-static const int STRGBPalEndianTable[16] = {0,2,1,3,8,10,9,11,4,6,5,7,12,14,13,15};
+static const int STRGBPalEndianTable[16] =
+{
+	0,2,1,3,8,10,9,11,4,6,5,7,12,14,13,15
+};
 #endif
 
 
@@ -61,11 +65,11 @@ static const int STRGBPalEndianTable[16] = {0,2,1,3,8,10,9,11,4,6,5,7,12,14,13,1
 */
 BOOL Spec512_IsImage(void)
 {
-  /* Normal Low res screen? */
-  if ( (STRes==ST_LOW_RES) && (bIsSpec512Display) )
-    return(TRUE);
+	/* Normal Low res screen? */
+	if (STRes == ST_LOW_RES && bIsSpec512Display)
+		return TRUE;
 
-  return(FALSE);
+	return FALSE;
 }
 
 
@@ -76,12 +80,14 @@ BOOL Spec512_IsImage(void)
 */
 void Spec512_StartVBL(void)
 {
-  /* Clear number of cycle palettes on each frame */
-  memset(nCyclePalettes, 0x0, (nScanlinesPerFrame+1)*sizeof(int));
-  /* Clear number of times accessed on entry in palette (used to check if is true Spectrum 512 image) */
-  memset(nPalettesAccess, 0x0, (nScanlinesPerFrame+1)*sizeof(int));
-  /* Set as not Spectrum 512 displayed image */
-  bIsSpec512Display = FALSE;
+	/* Clear number of cycle palettes on each frame */
+	memset(nCyclePalettes, 0x0, (nScanlinesPerFrame+1)*sizeof(int));
+
+	/* Clear number of times accessed on entry in palette (used to check if is true Spectrum 512 image) */
+	memset(nPalettesAccess, 0x0, (nScanlinesPerFrame+1)*sizeof(int));
+
+	/* Set as not Spectrum 512 displayed image */
+	bIsSpec512Display = FALSE;
 }
 
 
@@ -92,58 +98,58 @@ void Spec512_StartVBL(void)
 */
 void Spec512_StoreCyclePalette(Uint16 col, Uint32 addr)
 {
-  CYCLEPALETTE *pTmpCyclePalette;
-  int FrameCycles, ScanLine, nHorPos;
+	CYCLEPALETTE *pTmpCyclePalette;
+	int FrameCycles, ScanLine, nHorPos;
 
-  CycleColour = col;
-  CycleColourIndex = (addr-0xff8240)>>1;
+	CycleColour = col;
+	CycleColourIndex = (addr-0xff8240)>>1;
 
-  /* Find number of cycles into frame */
-  FrameCycles = Cycles_GetCounterOnWriteAccess(CYCLES_COUNTER_VIDEO);
+	/* Find number of cycles into frame */
+	FrameCycles = Cycles_GetCounterOnWriteAccess(CYCLES_COUNTER_VIDEO);
 
-  /* Find scan line we are currently on and get index into cycle-palette table */
-  ScanLine = FrameCycles / nCyclesPerLine;
-  pTmpCyclePalette = &CyclePalettes[ (ScanLine*MAX_CYCLEPALETTES_PERLINE) + nCyclePalettes[ScanLine] ];
+	/* Find scan line we are currently on and get index into cycle-palette table */
+	ScanLine = FrameCycles / nCyclesPerLine;
+	pTmpCyclePalette = &CyclePalettes[ (ScanLine*MAX_CYCLEPALETTES_PERLINE) + nCyclePalettes[ScanLine] ];
 
-  nHorPos = FrameCycles % nCyclesPerLine;
+	nHorPos = FrameCycles % nCyclesPerLine;
 
-  /* Temporary hack until we've got better CPU cycles emulation:
-   * The position needs to be corrected for a few positions.
-   * I think it might be related to the shifter which handles 16 pixels
-   * at a time / plane, since here we arrive every 20 cycles there are
-   * synchronizations problems. Now the very good question is : why does
-   * it happen only for the left part of the screen ???!!!!!
-   * If we add the same correction for the right part, then bad black pixels
-   * appear. This definetely requires some investigation... */
+	/* Temporary hack until we've got better CPU cycles emulation:
+	 * The position needs to be corrected for a few positions.
+	 * I think it might be related to the shifter which handles 16 pixels
+	 * at a time / plane, since here we arrive every 20 cycles there are
+	 * synchronizations problems. Now the very good question is : why does
+	 * it happen only for the left part of the screen ???!!!!!
+	 * If we add the same correction for the right part, then bad black pixels
+	 * appear. This definetely requires some investigation... */
 #if 0
-  if (nHorPos == 104 || nHorPos == 124 || nHorPos == 144)
-    nHorPos += 4;
+	if (nHorPos == 104 || nHorPos == 124 || nHorPos == 144)
+		nHorPos += 4;
 #endif
 
-  /* Do we have a previous entry at the same cycles? If so, 68000 have used a 'move.l' instruction so stagger writes */
-  if (nCyclePalettes[ScanLine]>0)
-  {
-    /* In case the ST uses a move.l or a movem.l to update colors, we need
-     * to add at least 4 cycles between each color: */
-    if ((pTmpCyclePalette-1)->LineCycles >= nHorPos)
-      nHorPos = (pTmpCyclePalette-1)->LineCycles + 4;
-  }
+	/* Do we have a previous entry at the same cycles? If so, 68000 have used a 'move.l' instruction so stagger writes */
+	if (nCyclePalettes[ScanLine] > 0)
+	{
+		/* In case the ST uses a move.l or a movem.l to update colors, we need
+		 * to add at least 4 cycles between each color: */
+		if ((pTmpCyclePalette-1)->LineCycles >= nHorPos)
+			nHorPos = (pTmpCyclePalette-1)->LineCycles + 4;
+	}
 
-  /* Store palette access */
-  pTmpCyclePalette->LineCycles = nHorPos;           /* Cycles into scanline */
-  pTmpCyclePalette->Colour = CycleColour;           /* Store ST/STe color RGB */
-  pTmpCyclePalette->Index = CycleColourIndex;       /* And index (0...15) */
+	/* Store palette access */
+	pTmpCyclePalette->LineCycles = nHorPos;           /* Cycles into scanline */
+	pTmpCyclePalette->Colour = CycleColour;           /* Store ST/STe color RGB */
+	pTmpCyclePalette->Index = CycleColourIndex;       /* And index (0...15) */
 
-  /* Increment count (this can never overflow as you cannot write to the palette more than 'MAX_CYCLEPALETTES_PERLINE' times per scanline) */
-  nCyclePalettes[ScanLine]++;
+	/* Increment count (this can never overflow as you cannot write to the palette more than 'MAX_CYCLEPALETTES_PERLINE' times per scanline) */
+	nCyclePalettes[ScanLine]++;
 
-  /* Check if program wrote to certain palette entry multiple times on a single scan-line  */
-  /* If we did then we must be using a Spectrum512 image or some kind of colour cycling... */
-  nPalettesAccess[ScanLine]++;
-  if (nPalettesAccess[ScanLine] >= 32)
-  {
-    bIsSpec512Display = TRUE;
-  }
+	/* Check if program wrote to certain palette entry multiple times on a single scan-line  */
+	/* If we did then we must be using a Spectrum512 image or some kind of colour cycling... */
+	nPalettesAccess[ScanLine]++;
+	if (nPalettesAccess[ScanLine] >= 32)
+	{
+		bIsSpec512Display = TRUE;
+	}
 }
 
 
@@ -153,36 +159,37 @@ void Spec512_StoreCyclePalette(Uint16 col, Uint32 addr)
 */
 void Spec512_StartFrame(void)
 {
-  int i;
+	int i;
 
-  /* Set so screen gets full-update when returns from Spectrum 512 display */
-  Screen_SetFullUpdate();
+	/* Set so screen gets full-update when returns from Spectrum 512 display */
+	Screen_SetFullUpdate();
 
-  /* Set terminators on each line, so when scan during conversion we know when to stop */
-  for (i = 0; i < (nScanlinesPerFrame+1); i++)
-  {
-    pCyclePalette = &CyclePalettes[ (i*MAX_CYCLEPALETTES_PERLINE) + nCyclePalettes[i] ];
-    pCyclePalette->LineCycles = -1;          /* Term */
-  }
+	/* Set terminators on each line, so when scan during conversion we know when to stop */
+	for (i = 0; i < (nScanlinesPerFrame+1); i++)
+	{
+		pCyclePalette = &CyclePalettes[ (i*MAX_CYCLEPALETTES_PERLINE) + nCyclePalettes[i] ];
+		pCyclePalette->LineCycles = -1;          /* Term */
+	}
 
-  /* Copy first line palette, kept in 'HBLPalettes' and store to 'STRGBPalette' */
-  for(i=0; i<16; i++)
-  {
+	/* Copy first line palette, kept in 'HBLPalettes' and store to 'STRGBPalette' */
+	for (i = 0; i < 16; i++)
+	{
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    STRGBPalette[STRGBPalEndianTable[i]] = ST2RGB[pHBLPalettes[i]];
+		STRGBPalette[STRGBPalEndianTable[i]] = ST2RGB[pHBLPalettes[i]];
 #else
-    STRGBPalette[i] = ST2RGB[pHBLPalettes[i]];
+		STRGBPalette[i] = ST2RGB[pHBLPalettes[i]];
 #endif
-  }
 
-  /* Ready for first call to 'Spec512_ScanLine' */
-  nScanLine = 0;
-  if (OverscanMode & OVERSCANMODE_TOP)
-    nScanLine += OVERSCAN_TOP;
+	}
 
-  /* Skip to first line(where start to draw screen from) */
-  for (i=0; i<(STScreenStartHorizLine+(nStartHBL-OVERSCAN_TOP)); i++)
-    Spec512_ScanWholeLine();
+	/* Ready for first call to 'Spec512_ScanLine' */
+	nScanLine = 0;
+	if (OverscanMode & OVERSCANMODE_TOP)
+		nScanLine += OVERSCAN_TOP;
+
+	/* Skip to first line(where start to draw screen from) */
+	for (i = 0; i < (STScreenStartHorizLine+(nStartHBL-OVERSCAN_TOP)); i++)
+		Spec512_ScanWholeLine();
 }
 
 
@@ -193,14 +200,14 @@ void Spec512_StartFrame(void)
 */
 void Spec512_ScanWholeLine(void)
 {
-  /* Store pointer to line of palette cycle writes */
-  pCyclePalette = &CyclePalettes[nScanLine*MAX_CYCLEPALETTES_PERLINE];
-  /* Ready for next scan line */
-  nScanLine++;
+	/* Store pointer to line of palette cycle writes */
+	pCyclePalette = &CyclePalettes[nScanLine*MAX_CYCLEPALETTES_PERLINE];
+	/* Ready for next scan line */
+	nScanLine++;
 
-  /* Update palette entries until we reach start of displayed screen */
-  ScanLineCycleCount = 0;
-  Spec512_EndScanLine();        /* Read whole line of palettes and update 'STRGBPalette' */
+	/* Update palette entries until we reach start of displayed screen */
+	ScanLineCycleCount = 0;
+	Spec512_EndScanLine();        /* Read whole line of palettes and update 'STRGBPalette' */
 }
 
 
@@ -210,20 +217,21 @@ void Spec512_ScanWholeLine(void)
 */
 void Spec512_StartScanLine(void)
 {
-  int i;
+	int i;
 
-  /* Store pointer to line of palette cycle writes */
-  pCyclePalette = &CyclePalettes[nScanLine*MAX_CYCLEPALETTES_PERLINE];
-  /* Ready for next scan line */
-  nScanLine++;
+	/* Store pointer to line of palette cycle writes */
+	pCyclePalette = &CyclePalettes[nScanLine*MAX_CYCLEPALETTES_PERLINE];
+	/* Ready for next scan line */
+	nScanLine++;
 
-  /* Update palette entries until we reach start of displayed screen */
-  ScanLineCycleCount = 0;
-  for(i=0; i<((SCREEN_START_CYCLE-16)/4); i++)  /* This '16' is as we've already added in the 'move' instruction timing */
-    Spec512_UpdatePaletteSpan();                /* Update palette for this 4-cycle period */
-  /* And skip for left border is not using overscan display to user */
-  for (i=0; i<(STScreenLeftSkipBytes/2); i++)   /* Eg, 16 bytes = 32 pixels or 8 palette periods */
-    Spec512_UpdatePaletteSpan();
+	/* Update palette entries until we reach start of displayed screen */
+	ScanLineCycleCount = 0;
+	for(i=0; i<((SCREEN_START_CYCLE-16)/4); i++)  /* This '16' is as we've already added in the 'move' instruction timing */
+		Spec512_UpdatePaletteSpan();              /* Update palette for this 4-cycle period */
+
+	/* And skip for left border is not using overscan display to user */
+	for (i=0; i<(STScreenLeftSkipBytes/2); i++)   /* Eg, 16 bytes = 32 pixels or 8 palette periods */
+		Spec512_UpdatePaletteSpan();
 }
 
 
@@ -233,9 +241,9 @@ void Spec512_StartScanLine(void)
 */
 void Spec512_EndScanLine(void)
 {
-  /* Continue to reads palette until complete so have correct version for next line */
-  while (ScanLineCycleCount < nCyclesPerLine)
-    Spec512_UpdatePaletteSpan();
+	/* Continue to reads palette until complete so have correct version for next line */
+	while (ScanLineCycleCount < nCyclesPerLine)
+		Spec512_UpdatePaletteSpan();
 }
 
 
@@ -245,15 +253,16 @@ void Spec512_EndScanLine(void)
 */
 void Spec512_UpdatePaletteSpan(void)
 {
-  if( pCyclePalette->LineCycles == ScanLineCycleCount )
-   {
-    /* Need to update palette with new entry */
+	if( pCyclePalette->LineCycles == ScanLineCycleCount )
+	{
+		/* Need to update palette with new entry */
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    STRGBPalette[STRGBPalEndianTable[pCyclePalette->Index]] = ST2RGB[pCyclePalette->Colour];
+		STRGBPalette[STRGBPalEndianTable[pCyclePalette->Index]] = ST2RGB[pCyclePalette->Colour];
 #else
-    STRGBPalette[pCyclePalette->Index] = ST2RGB[pCyclePalette->Colour];
+		STRGBPalette[pCyclePalette->Index] = ST2RGB[pCyclePalette->Colour];
 #endif
-    pCyclePalette += 1;
-   }
-  ScanLineCycleCount += 4;      /* Next 4 cycles */
+
+		pCyclePalette += 1;
+	}
+	ScanLineCycleCount += 4;      /* Next 4 cycles */
 }
--- src/ymFormat.c
+++ src/ymFormat.c
@@ -6,7 +6,7 @@
 
   YM File output, for use with STSound etc...
 */
-const char YMFormat_rcsid[] = "Hatari $Id: ymFormat.c,v 1.13 2006-02-08 22:49:27 eerot Exp $";
+const char YMFormat_rcsid[] = "Hatari $Id: ymFormat.c,v 1.14 2006-08-30 19:54:38 thothy Exp $";
 
 #include "main.h"
 #include "configuration.h"
@@ -31,39 +31,39 @@ unsigned char *pYMWorkspace = NULL, *pYMData;
 */
 BOOL YMFormat_BeginRecording(char *pszYMFileName)
 {
-  /* Free any previous data, don't save */
-  YMFormat_FreeRecording();
+	/* Free any previous data, don't save */
+	YMFormat_FreeRecording();
 
-  /* Make sure we have a proper filename to use */
-  if (!pszYMFileName || strlen(pszYMFileName)<=0)
-  {
-    return FALSE;
-  }
+	/* Make sure we have a proper filename to use */
+	if (!pszYMFileName || strlen(pszYMFileName)<=0)
+	{
+		return FALSE;
+	}
 
-  /* Create YM workspace */
-  pYMWorkspace = (unsigned char *)malloc(YM_RECORDSIZE);
+	/* Create YM workspace */
+	pYMWorkspace = (unsigned char *)malloc(YM_RECORDSIZE);
 
-  if (!pYMWorkspace)
-  {
-    /* Failed to allocate memory, cannot record */
-    bRecordingYM = FALSE;
-    return FALSE;
-  }
+	if (!pYMWorkspace)
+	{
+		/* Failed to allocate memory, cannot record */
+		bRecordingYM = FALSE;
+		return FALSE;
+	}
 
-  /* Get workspace pointer and store 4 byte header */
-  pYMData = pYMWorkspace;
-  *pYMData++ = 'Y';
-  *pYMData++ = 'M';
-  *pYMData++ = '3';
-  *pYMData++ = '!';
+	/* Get workspace pointer and store 4 byte header */
+	pYMData = pYMWorkspace;
+	*pYMData++ = 'Y';
+	*pYMData++ = 'M';
+	*pYMData++ = '3';
+	*pYMData++ = '!';
 
-  bRecordingYM = TRUE;          /* Ready to record */
-  nYMVBLS = 0;                  /* Number of VBLs of information */
+	bRecordingYM = TRUE;          /* Ready to record */
+	nYMVBLS = 0;                  /* Number of VBLs of information */
 
-  /* And inform user */
-  Log_AlertDlg(LOG_INFO, "YM sound data recording has been started.");
+	/* And inform user */
+	Log_AlertDlg(LOG_INFO, "YM sound data recording has been started.");
 
-  return TRUE;
+	return TRUE;
 }
 
 
@@ -73,21 +73,24 @@ BOOL YMFormat_BeginRecording(char *pszYMFileName)
 */
 void YMFormat_EndRecording(void)
 {
-  /* Have recorded information? */
-  if (pYMWorkspace && nYMVBLS) {
-    /* Convert YM to correct format(list of register 1, then register 2...) */
-    if (YMFormat_ConvertToStreams()) {
-      /* Save YM File */
-      if ( strlen(ConfigureParams.Sound.szYMCaptureFileName)>0 ) {
-        File_Save(ConfigureParams.Sound.szYMCaptureFileName, pYMWorkspace,(size_t)(nYMVBLS*NUM_PSG_SOUND_REGISTERS)+4, FALSE);
-        /* And inform user */
-        Log_AlertDlg(LOG_INFO, "YM sound data recording has been stopped.");
-      }
-    }
-  }
+	/* Have recorded information? */
+	if (pYMWorkspace && nYMVBLS)
+	{
+		/* Convert YM to correct format(list of register 1, then register 2...) */
+		if (YMFormat_ConvertToStreams())
+		{
+			/* Save YM File */
+			if (strlen(ConfigureParams.Sound.szYMCaptureFileName) > 0)
+			{
+				File_Save(ConfigureParams.Sound.szYMCaptureFileName, pYMWorkspace,(size_t)(nYMVBLS*NUM_PSG_SOUND_REGISTERS)+4, FALSE);
+				/* And inform user */
+				Log_AlertDlg(LOG_INFO, "YM sound data recording has been stopped.");
+			}
+		}
+	}
 
-  /* And free */
-  YMFormat_FreeRecording();
+	/* And free */
+	YMFormat_FreeRecording();
 }
 
 
@@ -97,13 +100,13 @@ void YMFormat_EndRecording(void)
 */
 void YMFormat_FreeRecording(void)
 {
-  /* Free workspace */
-  if (pYMWorkspace)
-    free(pYMWorkspace);
-  pYMWorkspace = NULL;
+	/* Free workspace */
+	if (pYMWorkspace)
+		free(pYMWorkspace);
+	pYMWorkspace = NULL;
 
-  /* Stop recording */
-  bRecordingYM = FALSE;
+	/* Stop recording */
+	bRecordingYM = FALSE;
 }
 
 
@@ -113,77 +116,81 @@ void YMFormat_FreeRecording(void)
 */
 void YMFormat_UpdateRecording(void)
 {
-  int i;
+	int i;
 
-  /* Can record this VBL information? */
-  if (bRecordingYM) {
-    /* Copy VBL registers to workspace */
-    for(i=0; i<(NUM_PSG_SOUND_REGISTERS-1); i++)
-      *pYMData++ = PSGRegisters[i];
-    /* Handle register '13'(PSG_REG_ENV_SHAPE) correctly - store 0xFF is did not write to this frame */
-    if (bEnvelopeFreqFlag)
-      *pYMData++ = PSGRegisters[PSG_REG_ENV_SHAPE];
-    else
-      *pYMData++ = 0xff;
+	/* Can record this VBL information? */
+	if (bRecordingYM)
+	{
+		/* Copy VBL registers to workspace */
+		for(i=0; i<(NUM_PSG_SOUND_REGISTERS-1); i++)
+			*pYMData++ = PSGRegisters[i];
+		/* Handle register '13'(PSG_REG_ENV_SHAPE) correctly - store 0xFF is did not write to this frame */
+		if (bEnvelopeFreqFlag)
+			*pYMData++ = PSGRegisters[PSG_REG_ENV_SHAPE];
+		else
+			*pYMData++ = 0xff;
 
-    /* Increase VBL count */
-    nYMVBLS++;
-    /* If run out of workspace, just save */
-    if (nYMVBLS>=YM_MAX_VBLS)
-      YMFormat_EndRecording();
-  }
+		/* Increase VBL count */
+		nYMVBLS++;
+		/* If run out of workspace, just save */
+		if (nYMVBLS>=YM_MAX_VBLS)
+			YMFormat_EndRecording();
+	}
 }
 
 
 /*-----------------------------------------------------------------------*/
 /*
   Convert YM data to stream for output
-
+ 
   Data is:
     4 Byte header 'YM3!'
     VBL Count x 14 PSG registers
   BUT
     We need data in a register stream, eg Reg 0, VBL 1, VBL 2, VBL n and then next register...
-
+ 
   Convert to new workspace and return TRUE if all OK
 */
 BOOL YMFormat_ConvertToStreams(void)
 {
-  unsigned char *pNewYMWorkspace;
-  unsigned char *pTmpYMData, *pNewYMData;
-  unsigned char *pTmpYMStream, *pNewYMStream;
-  int Reg, Count;
+	unsigned char *pNewYMWorkspace;
+	unsigned char *pTmpYMData, *pNewYMData;
+	unsigned char *pTmpYMStream, *pNewYMStream;
+	int Reg, Count;
 
-  /* Allocate new workspace to convert data to */
-  pNewYMWorkspace = (unsigned char *)malloc(YM_RECORDSIZE);
-  if (pNewYMWorkspace) {
-    /* Convert data, first copy over header */
-    pTmpYMData = pYMWorkspace;
-    pNewYMData = pNewYMWorkspace;
-    *pNewYMData++ = *pTmpYMData++;
-    *pNewYMData++ = *pTmpYMData++;
-    *pNewYMData++ = *pTmpYMData++;
-    *pNewYMData++ = *pTmpYMData++;
+	/* Allocate new workspace to convert data to */
+	pNewYMWorkspace = (unsigned char *)malloc(YM_RECORDSIZE);
+	if (pNewYMWorkspace)
+	{
+		/* Convert data, first copy over header */
+		pTmpYMData = pYMWorkspace;
+		pNewYMData = pNewYMWorkspace;
+		*pNewYMData++ = *pTmpYMData++;
+		*pNewYMData++ = *pTmpYMData++;
+		*pNewYMData++ = *pTmpYMData++;
+		*pNewYMData++ = *pTmpYMData++;
 
-    /* Now copy over each stream */
-    for(Reg=0; Reg<NUM_PSG_SOUND_REGISTERS; Reg++) {
-      /* Get pointer to source / destination */
-      pTmpYMStream = pTmpYMData + Reg;
-      pNewYMStream = pNewYMData + (Reg*nYMVBLS);
+		/* Now copy over each stream */
+		for(Reg=0; Reg<NUM_PSG_SOUND_REGISTERS; Reg++)
+		{
+			/* Get pointer to source / destination */
+			pTmpYMStream = pTmpYMData + Reg;
+			pNewYMStream = pNewYMData + (Reg*nYMVBLS);
 
-      /* Copy recording VBLs worth */
-      for(Count=0; Count<nYMVBLS; Count++) {
-        *pNewYMStream++ = *pTmpYMStream;
-        pTmpYMStream += NUM_PSG_SOUND_REGISTERS;
-      }
-    }
+			/* Copy recording VBLs worth */
+			for(Count=0; Count<nYMVBLS; Count++)
+			{
+				*pNewYMStream++ = *pTmpYMStream;
+				pTmpYMStream += NUM_PSG_SOUND_REGISTERS;
+			}
+		}
 
-    /* Delete old workspace and assign new */
-    free(pYMWorkspace);
-    pYMWorkspace = pNewYMWorkspace;
+		/* Delete old workspace and assign new */
+		free(pYMWorkspace);
+		pYMWorkspace = pNewYMWorkspace;
 
-    return(TRUE);
-  }
-  else
-    return(FALSE);
+		return TRUE;
+	}
+	else
+		return FALSE;
 }
