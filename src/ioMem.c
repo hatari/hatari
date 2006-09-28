5c4a90a5c22b62fa36fcf82fbfe82681bf7428f8
Thomas Huth <thothy@users.sourceforge.net>
2006-09-28 22:58:25 +0000

    Added missing break statement

--- src/ioMem.c
+++ src/ioMem.c
@@ -28,7 +28,7 @@
   Also note the 'mirror' (or shadow) registers of the PSG - this is used by most
   games.
 */
-const char IoMem_rcsid[] = "Hatari $Id: ioMem.c,v 1.13 2006-09-12 21:21:05 thothy Exp $";
+const char IoMem_rcsid[] = "Hatari $Id: ioMem.c,v 1.14 2006-09-28 22:58:25 thothy Exp $";
 
 #include "main.h"
 #include "configuration.h"
@@ -101,7 +101,7 @@ void IoMem_Init(void)
 		case MACHINE_ST:  pInterceptAccessFuncs = IoMemTable_ST; break;
 		case MACHINE_STE: pInterceptAccessFuncs = IoMemTable_STE; break;
 		case MACHINE_TT: pInterceptAccessFuncs = IoMemTable_TT; break;
-		case MACHINE_FALCON: pInterceptAccessFuncs = IoMemTable_Falcon;
+		case MACHINE_FALCON: pInterceptAccessFuncs = IoMemTable_Falcon; break;
 	}
 
 	/* Now set the correct handlers */
