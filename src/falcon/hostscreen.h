055b14b21398c664fc85631956aef1bb6aac88ee
Thomas Huth <thothy@users.sourceforge.net>
2006-10-08 12:11:29 +0000

    hostscreen.h now also uses araglue.h

--- src/falcon/hostscreen.h
+++ src/falcon/hostscreen.h
@@ -10,14 +10,7 @@
 
 #include <SDL.h>
 
-// defines to get Aranym code running in Hatari... they should be removed later
-#define uint32 Uint32
-#define uint16 Uint16
-#define uint8 Uint8
-#define int32 Sint32
-#define int16 Sint16
-#define int8 Sint8
-
+#include "araglue.h"
 
 /**
  * This macro handles the endianity for 24 bit per item data
