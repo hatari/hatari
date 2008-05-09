a112c9470d0f50048807df2ac6d09f7608339c4a
Eero Tamminen <eerot@users.berlios.de>
2008-05-10 01:43:12 +0300

    - add code for reading Hatari debugger output. fifo had problems,   but a regular file seems to work as well for now - the window hide/show works now properly and there are labels   for showing the debugger output and address, but they need   still a lot of fixes and almost none of the buttons do anything yet

--- python-ui/debugui.py
+++ python-ui/debugui.py
@@ -14,29 +14,49 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+import os
+import time
+import select
 # use correct version of pygtk/gtk
 import pygtk
 pygtk.require('2.0')
 import gtk
+import pango
 import gobject
 
 class HatariDebugUI():
     # constants
     _DISASM = 1
     _MEMDUMP = 2
+    fifopath = "/tmp/hatari-ui-" + os.getenv("USER") + ".fifo"
     
     def __init__(self, hatari, icon):
         self.hatari = hatari
-        self.hatari.change_option("--debug")
+        self.dumpmode = self._MEMDUMP
+        self.address = 0
+        
+        self.hatari.pause()
+        #raw_input("attach strace now, then press Enter\n")
+        os.unlink(self.fifopath)
+        # TODO: why fifo doesn't work properly (blocks forever on read)
+        #os.mkfifo(self.fifopath)
+        self.hatari.debug_command("f " + self.fifopath)
         self.window = self.create_ui("Hatari Debug UI", icon)
+        time.sleep(0.1)
+        self.fifo = open(self.fifopath, "r")
+        self.dump_address(self.address)
         self.show()
-    
+        
     def create_ui(self, title, icon):
         hbox1 = gtk.HBox()
         stop = gtk.ToggleButton("Stopped")
-        stop.connect("toggled", self.stop_cb)
         stop.set_active(True)
+        stop.connect("toggled", self.stop_cb)
         hbox1.add(stop)
+
+        self.address_label =  gtk.Label()
+        hbox1.add(self.address_label)
+        
         buttons = (
             ("<<", self.pageup_cb),
             ("<",  self.up_cb),
@@ -51,8 +71,11 @@ class HatariDebugUI():
         hbox2 = gtk.HBox()
         memdump = gtk.RadioButton(None, "Memdump")
         disasm = gtk.RadioButton(memdump, "Disasm")
+        if self.dumpmode == self._MEMDUMP:
+            memdump.set_active(True)
+        else:
+            disasm.set_active(True)
         memdump.connect("toggled", self.memdump_cb)
-        memdump.set_active(True)
         hbox2.add(memdump)
         hbox2.add(disasm)
 
@@ -70,6 +93,9 @@ class HatariDebugUI():
 
         vbox = gtk.VBox()
         vbox.add(hbox1)
+        self.memory_label = gtk.Label()
+        self.memory_label.set_attributes(pango.parse_markup("<tt></tt>")[0])
+        vbox.add(self.memory_label)
         vbox.add(hbox2)
         
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
@@ -82,9 +108,32 @@ class HatariDebugUI():
     def stop_cb(self, widget):
         if widget.get_active():
             self.hatari.pause()
+            self.dump_address(self.address)
         else:
             self.hatari.unpause()
-    
+
+    def dump_address(self, address):
+        self.address_label.set_text("0x%06x" % address)
+        if self.dumpmode == self._MEMDUMP:
+            cmd = "m"
+        else:
+            cmd = "d"
+        # request memory data from Hatari and wait until it's available
+        self.hatari.debug_command("%s %06x" % (cmd, address))
+        self.memory_label.set_text(self.get_data())
+        self.address = address
+
+    def get_data(self):
+        # wait until data is available, wait some more for all
+        # of it and only then it can be read
+        print "Request&wait data from Hatari..."
+        select.select([self.fifo], [], [])
+        time.sleep(0.1)
+        print "...read the data."
+        text = "\n" + "".join(self.fifo.readlines())
+        print text
+        return text
+
     def pageup_cb(self, widget):
         print "TODO: page back in memory"
 
@@ -104,10 +153,12 @@ class HatariDebugUI():
         else:
             print "Dumpmode on DISASM"
             self.dumpmode = self._DISASM
+        self.dump_address(self.address)
 
     def registers_cb(self, widget):
         print "TODO: show these in window and allow changing registers"
         self.hatari.debug_command("r")
+        self.get_data()
 
     def monitor_cb(self, widget):
         print "TODO: add monitor window for given memory address range"
@@ -122,12 +173,12 @@ class HatariDebugUI():
         print "TODO: show tracelog and allow changing trace settings"
 
     def show(self):
+        self.hatari.pause()
         self.window.show_all()
         self.window.deiconify()
-        self.hatari.pause()
 
     def hide(self, widget, arg):
-        # no window destroy
+        # continue Hatari, hide window, don't destroy it
         self.hatari.unpause()
         self.window.hide()
         return True
