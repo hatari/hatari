55641d216c97e47ea3395fe2332d551635e76e0f
Thomas Huth <huth@users.berlios.de>
2009-02-28 10:10:34 +0100

    Added files for compiling with Visual-C. Thanks to Kenneth Kaufman for the patch!

---
+++ Visual.Studio/VisualStudioFix.c
@@ -0,0 +1,43 @@
+#if defined(_VCWIN_)
+#pragma comment(lib, ".\\SDL\\lib\\sdl.lib")			// sdl.lib	
+#pragma comment(lib, ".\\zlib\\win32\\zlib1.lib")		// zlib1.lib 
+#endif
+
+#if defined(_VCWIN_)
+	#include <tchar.h>
+	#include <stdio.h>
+	#include <stdarg.h>
+#endif
+
+extern	FILE *TraceFile;
+
+#if defined(_VCWIN_)
+	#ifndef _INC_HATARI_TRACE
+	#define _INC_HATARI_TRACE
+
+	#ifdef HATARI_TRACE_ACTIVATED
+		void	HATARI_TRACE( int level, ...)
+		{
+			va_list	x;
+			va_start(x,level);
+			if ( HatariTraceFlags & level ) _vftprintf(TraceFile,level, x);
+			va_end	(x);
+		};
+	#else		/* HATARI_TRACE_ACTIVATED */
+		void	HATARI_TRACE( int level, ...)
+		{
+		}
+
+	#endif		/* HATARI_TRACE_ACTIVATED */
+
+	void HATARI_TRACE_PRINT(char*	strFirstString, ...)	
+	{
+		va_list	x;
+		va_start(x,strFirstString);
+		_vftprintf(TraceFile,strFirstString, x);
+		va_end	(x);				
+	};
+
+	#endif
+
+#endif
