a77e29622cfc47575a57b8b9a9466444b138439b
Eero Tamminen <eerot@users.berlios.de>
2009-11-29 18:10:38 +0200

    add function to check that address range is on valid ST memory area

--- src/includes/stMemory.h
+++ src/includes/stMemory.h
@@ -23,6 +23,10 @@ extern Uint8 STRam[16*1024*1024];
 
 extern Uint32 STRamEnd;
 
+/* TODO: when Hatari will support TT/fast-RAM, take it into account
+ * in STRAM_ADDR() and STMemory_ValidArea().
+ */
+
 /* Offset ST address to PC pointer: */
 #if ENABLE_SMALL_MEM
 # define STRAM_ADDR(Var) \
@@ -32,6 +36,25 @@ extern Uint32 STRamEnd;
 #else
 # define STRAM_ADDR(Var)  ((unsigned long)STRam+((Uint32)(Var) & 0x00ffffff))
 #endif
+
+
+/**
+ * Check whether given memory address and size are within
+ * valid ST memory area (i.e. read/write from/to there doesn't
+ * overwrite Hatari's own memory & cause potential segfaults)
+ * and that the size is positive.
+ * 
+ * If they are; return true, otherwise false.
+ */
+static inline bool STMemory_ValidArea(Uint32 addr, int size)
+{
+	if (size >= 0 && addr+size < 0xff0000 &&
+	    (addr+size < STRamEnd || addr >= 0xe00000))
+	{
+		return true;
+	}
+	return false;
+}
 
 /*-----------------------------------------------------------------------*/
 /*
