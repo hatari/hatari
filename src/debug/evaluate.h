fa0bbe3eced9180a1a0dcf835a42ea943295cfc8
Eero Tamminen <eerot@users.berlios.de>
2010-01-10 20:37:08 +0200

    correct comment/define name

--- src/debug/evaluate.h
+++ src/debug/evaluate.h
@@ -1,12 +1,12 @@
 /*
-  Hatari - calculate.h
+  Hatari - evaluate.h
 
   This file is distributed under the GNU Public License, version 2 or at
   your option any later version. Read the file gpl.txt for details.
 */
 
-#ifndef HATARI_CALCULATE_H
-#define HATARI_CALCULATE_H
+#ifndef HATARI_EVALUATE_H
+#define HATARI_EVALUATE_H
 
 extern bool Eval_Number(const char *value, Uint32 *number);
 extern int Eval_Range(char *str, Uint32 *lower, Uint32 *upper);
