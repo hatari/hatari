7dcb72ddd361aed065e9f696392fac427f6a445d
Eero Tamminen <eerot@users.berlios.de>
2010-01-16 00:35:28 +0200

    add "info" subcommands "osheader" and "basepage" + arg handling for last one

--- src/debug/debugInfo.c
+++ src/debug/debugInfo.c
@@ -15,15 +15,153 @@ const char DebugInfo_fileid[] = "Hatari debuginfo.c : " __DATE__ " " __TIME__;
 #include "debugui.h"
 #include "ioMem.h"
 #include "configuration.h"
+#include "evaluate.h"
 
+
+/* ------------------------------------------------------------------
+ * TOS information
+ */
+#define OS_SYSBASE 0x4F2
+#define OS_HEADER_SIZE 0x30
+
+#define COOKIE_JAR 0x5A0
+
+#define BASEPAGE_SIZE 0x100
+
+#define GEM_MAGIC 0x87654321
+#define GEM_MUPB_SIZE 0xC
+
+#define RESET_MAGIC 0x31415926
+#define RESET_VALID 0x426
+#define RESET_VECTOR 0x42A
+
+#define COUNTRY_SPAIN 4
+
+/**
+ * DebugInfo_GetSysbase: set osversion to given argument.
+ * return sysbase address on success and zero on failure.
+ */
+static Uint32 DebugInfo_GetSysbase(Uint16 *osversion)
+{
+	Uint32 sysbase = STMemory_ReadLong(OS_SYSBASE);
+
+	if (!STMemory_ValidArea(sysbase, OS_HEADER_SIZE)) {
+		fprintf(stderr, "Invalid TOS base address!\n");
+		return 0;
+	}
+	if (STMemory_ReadLong(sysbase+0x08) != sysbase) {
+		fprintf(stderr, "Sysbase and os_beg address in OS header mismatch!\n");
+		return 0;
+	}
+	*osversion = STMemory_ReadWord(sysbase+0x02);
+	return sysbase;
+}
+
+/**
+ * DebugInfo_Basepage: show TOS process basepage information
+ * at given address.
+ */
+static void DebugInfo_Basepage(Uint32 basepage)
+{
+	Uint32 env, cmd;
+	
+	fprintf(stderr, "Process basepage information:\n");
+	if (!STMemory_ValidArea(basepage, BASEPAGE_SIZE) ||
+	    STMemory_ReadLong(basepage) != basepage) {
+		fprintf(stderr, "- address 0x%06x is invalid!\n", basepage);
+		return;
+	}
+	fprintf(stderr, "- TPA start      : 0x%06x\n", STMemory_ReadLong(basepage));
+	fprintf(stderr, "- TPA end +1     : 0x%06x\n", STMemory_ReadLong(basepage+0x04));
+	fprintf(stderr, "- Text segment   : 0x%06x\n", STMemory_ReadLong(basepage+0x08));
+	fprintf(stderr, "- Text size      : 0x%x\n",   STMemory_ReadLong(basepage+0x0C));
+	fprintf(stderr, "- Data segment   : 0x%06x\n", STMemory_ReadLong(basepage+0x10));
+	fprintf(stderr, "- Data size      : 0x%x\n",   STMemory_ReadLong(basepage+0x14));
+	fprintf(stderr, "- BSS segment    : 0x%06x\n", STMemory_ReadLong(basepage+0x18));
+	fprintf(stderr, "- BSS size       : 0x%x\n",   STMemory_ReadLong(basepage+0x1C));
+	fprintf(stderr, "- Process DTA    : 0x%06x\n", STMemory_ReadLong(basepage+0x20));
+	fprintf(stderr, "- Parent basepage: 0x%06x\n", STMemory_ReadLong(basepage+0x24));
+
+	env = STMemory_ReadLong(basepage+0x2C);
+	fprintf(stderr, "- Environment    : 0x%06x\n", env);
+	if (STMemory_ValidArea(env, 4096)) {
+		fprintf(stderr, "'%s'\n", STRam+env);
+	}
+	cmd = STMemory_ReadLong(basepage+0x2C);
+	fprintf(stderr, "- Command line   :\n'%s'\n", STRam+basepage+0x80);
+}
+
+/**
+ * DebugInfo_OSHeader: display TOS OS Header
+ */
+static void DebugInfo_OSHeader(Uint32 dummy)
+{
+	Uint32 sysbase, gemblock, basepage;
+	Uint16 osversion, osconf;
+
+	sysbase = DebugInfo_GetSysbase(&osversion);
+	if (!sysbase) {
+		return;
+	}
+	fprintf(stderr, "OS base address: 0x%06x\n", sysbase);
+	fprintf(stderr, "OS RAM end+1   : 0x%06x\n", STMemory_ReadLong(sysbase+0x0C));
+	fprintf(stderr, "TOS version    : 0x%x\n", osversion);
+
+	fprintf(stderr, "Reset handler  : 0x%06x\n", STMemory_ReadLong(sysbase+0x04));
+	fprintf(stderr, "Reset vector   : 0x%06x\n", STMemory_ReadLong(RESET_VECTOR));
+	fprintf(stderr, "Reset valid    : 0x%x (valid=0x%x)\n", STMemory_ReadLong(RESET_VALID), RESET_MAGIC);
+
+	gemblock = STMemory_ReadLong(sysbase+0x14);
+	fprintf(stderr, "GEM Memory Usage Parameter Block:\n");
+	if (STMemory_ValidArea(gemblock, GEM_MUPB_SIZE)) {
+		fprintf(stderr, "- Block addr: 0x%06x\n", gemblock);
+		fprintf(stderr, "- GEM magic : 0x%x (valid=0x%x)\n", STMemory_ReadLong(gemblock), GEM_MAGIC);
+		fprintf(stderr, "- GEM entry : 0x%06x\n", STMemory_ReadLong(gemblock+4));
+		fprintf(stderr, "- GEM end   : 0x%06x\n", STMemory_ReadLong(gemblock+8));
+	} else {
+		fprintf(stderr, "- is at INVALID 0x%06x address.\n", gemblock);
+	}
+
+	fprintf(stderr, "OS date        : 0x%x\n", STMemory_ReadLong(sysbase+0x14));
+	fprintf(stderr, "OS DOS date    : 0x%x\n", STMemory_ReadLong(sysbase+0x1E));
+
+	osconf = STMemory_ReadWord(sysbase+0x1C);
+	fprintf(stderr, "OS Conf bits   : lang=%d, %s\n", osconf>>1, osconf&1 ? "PAL":"NTSC");
+	fprintf(stderr, "Cookie Jar     : 0x%06x\n", STMemory_ReadLong(COOKIE_JAR));
+
+	/* last 3 OS header fields are only available as of TOS 1.02 */
+	if (osversion >= 0x0102) {
+		fprintf(stderr, "Memory pool    : 0x%06x\n", STMemory_ReadLong(sysbase+0x20));
+		fprintf(stderr, "Kbshift addr   : 0x%06x\n", STMemory_ReadLong(sysbase+0x24));
+		basepage = STMemory_ReadLong(sysbase+0x28);
+	} else {
+		/* TODO: GEMDOS memory pool address for TOS 1.0? */
+		fprintf(stderr, "Kbshift addr : 0x000E1B\n");
+		if((osconf>>1) == COUNTRY_SPAIN) {
+			basepage = 0x873C;
+		} else {
+			basepage = 0x602C;
+		}
+	}
+	if (STMemory_ValidArea(basepage, 4)) {
+		fprintf(stderr, "Running program: 0x%06x -> basepage: 0x%06x\n", basepage, STMemory_ReadLong(basepage));
+	} else {
+		fprintf(stderr, "Invalid basepage pointer address 0x%06x!\n", basepage);
+	}
+}
+
+
+/* ------------------------------------------------------------------
+ * Falcon HW information
+ */
 
 /**
  * DebugInfo_Videl : display the Videl registers values.
  */
-static void DebugInfo_Videl(void)
+static void DebugInfo_Videl(Uint32 dummy)
 {
 	if (ConfigureParams.System.nMachineType != MACHINE_FALCON) {
-		fprintf(stderr, "No Videl\n");
+		fprintf(stderr, "Not Falcon - no Videl!\n");
 		return;
 	}
 
@@ -59,10 +197,10 @@ static void DebugInfo_Videl(void)
 /**
  * DebugInfo_Crossbar : display the Crossbar registers values.
  */
-static void DebugInfo_Crossbar(void)
+static void DebugInfo_Crossbar(Uint32 dummy)
 {
 	if (ConfigureParams.System.nMachineType != MACHINE_FALCON) {
-		fprintf(stderr, "No Crossbar\n");
+		fprintf(stderr, "Not Falcon - no Crossbar!\n");
 		return;
 	}
 
@@ -96,12 +234,20 @@ static void DebugInfo_Crossbar(void)
 }
 
 
+/* ------------------------------------------------------------------
+ * Debugger & readline TAB completion integration
+ */
+
 static const struct {
 	const char *name;
-	void (*func)(void);
+	void (*func)(Uint32 arg);
+	bool novalue;
+	const char *info;
 } infotable[] = {
-	{ "crossbar", DebugInfo_Crossbar },
-	{ "videl",    DebugInfo_Videl }
+	{ "basepage", DebugInfo_Basepage, false, "program basepage info at <given address>" },
+	{ "crossbar", DebugInfo_Crossbar, true,  "Falcon crossbar HW register values" },
+	{ "osheader", DebugInfo_OSHeader, true,  "TOS OS header information" },
+	{ "videl",    DebugInfo_Videl,    true,  "Falcon Videl HW register values" }
 };
 
 
@@ -136,20 +282,28 @@ char *DebugInfo_MatchCommand(const char *text, int state)
 int DebugInfo_Command(int nArgc, char *psArgs[])
 {
 	const char *cmd;
+	Uint32 value = 0;
+	bool ok = true;
 	int i;
 
-	if (nArgc > 1) {
-		cmd = psArgs[1];
+	if (nArgc > 2) {
+		ok = Eval_Number(psArgs[2], &value);
+	}
+	if (ok && nArgc > 1) {
+		cmd = psArgs[1];		
 		for (i = 0; i < ARRAYSIZE(infotable); i++) {
-			if (strcmp(cmd, infotable[i].name) == 0) {
-				infotable[i].func();
+			if (strcmp(cmd, infotable[i].name) == 0 &&
+			    infotable[i].novalue != !(!value)) {
+				infotable[i].func(value);
 				return DEBUGGER_CMDDONE;
 			}
 		}
 	}
 	fprintf(stderr, "Info subcommands are:\n");
 	for (i = 0; i < ARRAYSIZE(infotable); i++) {
-		fprintf(stderr, "- %s\n", infotable[i].name);
+		fprintf(stderr, "- %s: show %s%s\n",
+			infotable[i].name, infotable[i].info,
+			infotable[i].novalue ? "":" (needs argument)");
 	}
 	return DEBUGGER_CMDDONE;
 }
