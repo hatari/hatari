408ae1d93eeba8f970720ad6b303b5f0d0ba6ae9
Thomas Huth <huth@users.berlios.de>
2010-02-28 10:35:09 +0100

    Added Math library to CMake build system. This should fix bug #16851.

---
+++ cmake/FindMath.cmake
@@ -0,0 +1,15 @@
+
+if(MATH_INCLUDE_DIR)
+  # Already in cache, be silent
+  set(MATH_FIND_QUIETLY TRUE)
+endif(MATH_INCLUDE_DIR)
+
+find_path(MATH_INCLUDE_DIR math.h)
+
+find_library(MATH_LIBRARY NAMES m)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MATH DEFAULT_MSG
+                                  MATH_LIBRARY MATH_INCLUDE_DIR)
+
+mark_as_advanced(MATH_LIBRARY MATH_INCLUDE_DIR)
