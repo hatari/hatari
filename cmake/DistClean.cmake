6eb4ce24269ef15d8ae50f987998237a7a8dca92
Thomas Huth <huth@users.berlios.de>
2010-06-13 11:29:12 +0200

    Improved distclean target

--- cmake/DistClean.cmake
+++ cmake/DistClean.cmake
@@ -4,10 +4,20 @@
 
 if(UNIX)
 	add_custom_target(distclean  COMMENT "Cleaning up for distribution")
-	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt cmake_install.cmake
-			    Makefile config.h)
+	# Clean up Hatari specific files:
+	foreach(CLEAN_FILE  config.h src/hatari src/uae-cpu/build68k
+			src/uae-cpu/cpudefs.c src/uae-cpu/cpuemu.c
+			src/uae-cpu/cpustbl.c src/uae-cpu/cputbl.h
+			src/uae-cpu/gencpu tools/hmsa/hmsa)
 		add_custom_command(TARGET distclean POST_BUILD
-			COMMAND find . -depth -name ${CLEAN_FILE} | xargs rm -r
+			COMMAND rm -f ${CLEAN_FILE}
+			DEPENDS clean)
+	endforeach(CLEAN_FILE)
+	# Clean up files that can appear at multiple places:
+	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt '*.a' '*.1.gz'
+			cmake_install.cmake Makefile)
+		add_custom_command(TARGET distclean POST_BUILD
+			COMMAND find . -depth -name ${CLEAN_FILE} | xargs rm -rf
 			DEPENDS clean)
 	endforeach(CLEAN_FILE)
 endif(UNIX)
