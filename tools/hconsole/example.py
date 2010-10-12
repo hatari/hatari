e207dc78c6d6c3fb93e913aa8a7c90a511bd55d8
Eero Tamminen <eerot@users.berlios.de>
2010-10-12 22:15:28 +0300

    example.py can/loads commands from the separate hconsole input file
    Therefore removed the duplication functionality from this.
    Added also debugger input file example calling for this.

--- tools/hconsole/example.py
+++ tools/hconsole/example.py
@@ -3,26 +3,46 @@
 # This is an example of scripting hatari using hconsole
 #
 # Run with:
-# ./example.py --tos etos512k.img
+#   ./example.py --tos etos512k.img
+# Or if hconsole is installed to the system:
+#   /usr/share/hatari/hconsole/example.py --tos etos512k.img
 
-import hconsole
-run = hconsole.Main().run
+import hconsole, os, sys
 
-# wait for TOS to boot
-run("sleep 3")
+# path for this script
+path = os.path.dirname(sys.argv[0])
+# current work directory
+cwd = os.path.abspath(os.path.curdir)
 
-# Start EmuTOS console with ^Z:
-# Control down, press Z, Control up
-# (Control key scancode is 29)
-run("keydown 29")
-run("keypress Z")
-run("keyup 29")
+# shortcuts to hconsole stuff
+main = hconsole.Main()
+code = hconsole.Scancode
+
+# execute commands from external file in current directory
+main.script(path + "/example-commands")
+
+# define shortcut functions for entering specific key presses
+def backspace():
+    main.run("keypress %s" % code.Backspace)
+def enter():
+    main.run("keypress %s" % code.Return)
+
+# loop removing some of previously script output
+for i in range(20):
+    backspace()
+enter()
 
 # output some text to EmuTOS console in a loop
 for i in range(5):
-    run("text echo Welcome to 'hatariconsole'")
-    # press Enter
-    run("keypress 28")
+    main.run("text echo Welcome to 'hconsole'")
+    enter()
 
-run("sleep 3")
-run("quit")
+# ask Hatari debugger to parse breakpoint etc commands from file
+main.run("parse %s/example-debugger" % path)
+
+# hit a Getdrv() breakpoint when EmuTOS prompt is redrawn
+enter()
+
+# wait few secs and kill Hatari
+main.run("sleep 3")
+main.run("kill")
