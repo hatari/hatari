9455745897f43e402950b030f31dcca8760e3455
Laurent Sallafranque <laurent.sallafranque@free.fr>
2010-11-30 00:18:10 +0100

    fix: correct wrong cycles computing for BCLR_aa, BCHG_aa, BSET_aa. According to the doc, cycles = 4 + ea + 2*X_YWaitState (Ea = 0)

--- src/falcon/dsp_cpu.c
+++ src/falcon/dsp_cpu.c
@@ -1568,10 +1568,7 @@ static void dsp_bchg_aa(void)
 	dsp_core->registers[DSP_REG_SR] &= BITMASK(16)-(1<<DSP_SR_C);
 	dsp_core->registers[DSP_REG_SR] |= newcarry<<DSP_SR_C;
 
-	dsp_core->instr_cycle += 2;
-	if (addr>=0x200) {
-		dsp_core->instr_cycle += XY_WAITSTATE;
-	}
+	dsp_core->instr_cycle += 2 + 2*XY_WAITSTATE;
 }
 
 static void dsp_bchg_ea(void)
@@ -1678,10 +1675,7 @@ static void dsp_bclr_aa(void)
 	dsp_core->registers[DSP_REG_SR] &= BITMASK(16)-(1<<DSP_SR_C);
 	dsp_core->registers[DSP_REG_SR] |= newcarry<<DSP_SR_C;
 
-	dsp_core->instr_cycle += 2;
-	if (addr>=0x200) {
-		dsp_core->instr_cycle += XY_WAITSTATE;
-	}
+	dsp_core->instr_cycle += 2 + 2*XY_WAITSTATE;
 }
 
 static void dsp_bclr_ea(void)
@@ -1777,10 +1771,7 @@ static void dsp_bset_aa(void)
 	dsp_core->registers[DSP_REG_SR] &= BITMASK(16)-(1<<DSP_SR_C);
 	dsp_core->registers[DSP_REG_SR] |= newcarry<<DSP_SR_C;
 
-	dsp_core->instr_cycle += 2;
-	if (addr>=0x200) {
-		dsp_core->instr_cycle += XY_WAITSTATE;
-	}
+	dsp_core->instr_cycle += 2 + 2*XY_WAITSTATE;
 }
 
 static void dsp_bset_ea(void)
