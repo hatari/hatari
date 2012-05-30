3b6ca9ce613bb753f1e528d814f0929ae0d80254
Eero Tamminen <oak@helsinkinet.fi>
2012-05-30 23:59:49 +0300

    skip intermediate dirs in zip2st

--- tools/zip2st.sh
+++ tools/zip2st.sh
@@ -15,8 +15,10 @@ fi
 # one ZIPFILE given?
 if [ $# -lt 1 ] || [ -z "$1" ] || [ $# -gt 2 ]; then
 	name=${0##*/}
-	echo "Convert .ZIP files to .ST disk images."
-	echo "This script requires the mtools package."
+	echo "Convert a .zip archive file to a .st disk image."
+	echo
+	echo "Single intermediate directories in the zip"
+	echo "file are skipped."
 	echo
 	echo "Usage:"
 	echo " $name srcname.zip [destname.st]"
@@ -73,9 +75,35 @@ unzip "$ZIPFILE" -d "$TEMPDIR" || exit 2
 echo "chmod -R u+rw $TEMPDIR/*"
 chmod -R u+rw $TEMPDIR/*
 
+echo
+step=$(($step+1))
+echo "$step) Checking/skipping intermediate directories..."
+ZIPDIR=$TEMPDIR
+while true; do
+	count=$(ls $ZIPDIR|wc -l)
+	if [ $count -ne 1 ]; then
+		if [ $count -eq 0 ]; then
+			echo "ERROR: zip content is empty!"
+			exit 1
+		fi
+		# more than one dir/file
+		break
+	fi
+	dir=$(ls $ZIPDIR)
+	if [ \! -d "$ZIPDIR/$dir" ]; then
+		# not dir
+		break
+	fi
+	if [ -z "$(echo $dir|grep -v -i '^auto$')" ]; then
+		# don't skip AUTO dir
+		break
+	fi
+	echo "- $dir"
+	ZIPDIR=$ZIPDIR/$dir
+done
 
 # size of reserved sectors, FATs & root dir + zip content size
-size=$((24 + $(du -ks $TEMPDIR|awk '{print $1}')))
+size=$((24 + $(du -ks $ZIPDIR|awk '{print $1}')))
 
 # find a suitable disk size supported by mformat and Atari ST
 disksize=0
@@ -108,8 +136,8 @@ if [ $disksize -gt 0 ]; then
 	echo
 	step=$(($step+1))
 	echo "$step) Copying data to disk image..."
-	echo "MTOOLS_NO_VFAT=1 mcopy -i $STFILE -spmv $TEMPDIR/* ::"
-	MTOOLS_NO_VFAT=1 mcopy -i "$STFILE" -spmv $TEMPDIR/* ::
+	echo "MTOOLS_NO_VFAT=1 mcopy -i $STFILE -spmv $ZIPDIR/* ::"
+	MTOOLS_NO_VFAT=1 mcopy -i "$STFILE" -spmv $ZIPDIR/* ::
 else
 	echo "ERROR: zip contents don't fit to a floppy image ($size > 2880 KB)."
 fi
