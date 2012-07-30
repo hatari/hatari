7581f68cbfffc3ae0c1b9a4f3899213f282e7f88
Nicolas Pomarède <npomarede@corp.free.fr>
2012-07-31 00:51:14 +0200

    Temporary call IPF_Emulate on each HBL

--- src/video.c
+++ src/video.c
@@ -342,6 +342,7 @@ const char Video_fileid[] = "Hatari video.c : " __DATE__ " " __TIME__;
 #include "falcon/videl.h"
 #include "falcon/hostscreen.h"
 #include "avi_record.h"
+#include "floppy_ipf.h"
 
 
 /* The border's mask allows to keep track of all the border tricks		*/
@@ -1582,6 +1583,10 @@ void Video_InterruptHandler_HBL ( void )
 	Video_EndHBL();					/* Check some borders removal and copy line to display buffer */
 
 	DmaSnd_STE_HBL_Update();			/* Update STE DMA sound if needed */
+
+	/* TEMP IPF */
+	IPF_Emulate ( nCyclesPerLine );
+	/* TEMP IPF */
 
 	nHBL++;						/* Increase HBL count */
 
