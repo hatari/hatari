82ea7aab8917583877bd3948c380011e50ba4eb5
Nicolas Pomarède <npomarede@corp.free.fr>
2012-09-24 23:45:57 +0200

    Fix GPL text

--- src/includes/fdc.h
+++ src/includes/fdc.h
@@ -1,7 +1,7 @@
 /*
   Hatari - fdc.h
 
-  This file is distributed under the GNU Public License, version 2 or at
+  This file is distributed under the GNU General Public License, version 2 or at
   your option any later version. Read the file gpl.txt for details.
 */
 
