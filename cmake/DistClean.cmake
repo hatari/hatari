e9d80f8b6da65c04cd80d8a937f1539af754fa5e
Thomas Huth <huth@tuxfamily.org>
2013-09-08 19:18:14 +0200

    Remove gst2ascii during distclean

--- cmake/DistClean.cmake
+++ cmake/DistClean.cmake
@@ -6,10 +6,10 @@ if(UNIX)
 	add_custom_target(distclean  COMMENT "Cleaning up for distribution")
 	# Clean up Hatari specific files:
 	foreach(CLEAN_FILE  config.h install_manifest.txt
-			src/hatari src/uae-cpu/build68k
+			src/hatari src/uae-cpu/build68k src/uae-cpu/gencpu
 			src/uae-cpu/cpudefs.c src/uae-cpu/cpuemu.c
 			src/uae-cpu/cpustbl.c src/uae-cpu/cputbl.h
-			src/uae-cpu/gencpu tools/hmsa/hmsa)
+			tools/hmsa/hmsa tools/debugger/gst2ascii)
 		add_custom_command(TARGET distclean POST_BUILD
 			COMMAND rm -f ${CLEAN_FILE}
 			DEPENDS clean)
