c4fa43504956170d948c891ef34fa5e34df68d88
Nicolas Pomarède <npomarede@corp.free.fr>
2013-09-25 23:59:36 +0200

    Allow the user to override mingw's default values This allows to use mingw32 or mingw-w64 by giving -D parameters to cmake

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -1,15 +1,30 @@
 # This Toolchain file is used to cross compile the windows
 # version of Hatari under linux using mingw32
 # use : cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain-mingw32.cmake
+#
+# If MINGW_EXE_PREFIX is defined, we use this to set the name
+# for gcc/g++/windres required for cross compilation
+#
+# If MINGW_ROOT_PATH is defined, we use this to build the full
+# path used when looking for lib/ and include/ in mingw's sysroot/
+
 
 # The name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # mingw32 versions of the different tools
 # (change these depending on your system settings)
-SET(CMAKE_C_COMPILER i586-pc-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i586-pc-mingw32-g++)
-SET(CMAKE_RC_COMPILER i586-pc-mingw32-windres)
+IF(NOT MINGW_EXE_PREFIX)
+  # Some default values if MINGW_EXE_PREFIX is not defined
+  SET(CMAKE_C_COMPILER i586-pc-mingw32-gcc)
+  SET(CMAKE_CXX_COMPILER i586-pc-mingw32-g++)
+  SET(CMAKE_RC_COMPILER i586-pc-mingw32-windres)
+ELSE()
+  # Use the value provided to set mingw's tools
+  SET(CMAKE_C_COMPILER ${MINGW_EXE_PREFIX}-gcc)
+  SET(CMAKE_CXX_COMPILER ${MINGW_EXE_PREFIX}-g++)
+  SET(CMAKE_RC_COMPILER ${MINGW_EXE_PREFIX}-windres)
+ENDIF()
 
 # Base directory for the target environment
 # We use the output from '-print-sysroot'
@@ -20,7 +35,17 @@ EXECUTE_PROCESS(
 )
 # bin/, include/, lib/ and share/ are often in "mingw/"
 # You might need to adjust the path for your system
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}/mingw)
+IF(NOT MINGW_ROOT_PATH)
+  # Default value if MINGW_ROOT_PATH is not defined
+  SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}/mingw)
+ELSE()
+  # Use the value passed to cmake
+  SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}/${MINGW_ROOT_PATH})
+ENDIF()
+
+
+
+# Make the path absolute, a relative path could confuse some systems
 get_filename_component ( CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ABSOLUTE )
 
 # Uncomment this line with your own values if above doesn't work
