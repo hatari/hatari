c1ca830b7d90c6270994e9321203af30d3954249
Nicolas Pomarède <npomarede@corp.free.fr>
2014-01-12 19:28:55 +0100

    Parse the STX file, alloc buffers and init internal variables

--- src/floppy_stx.c
+++ src/floppy_stx.c
@@ -30,10 +30,21 @@ const char floppy_stx_fileid[] = "Hatari floppy_stx.c : " __DATE__ " " __TIME__;
 
 typedef struct
 {
+	STX_MAIN_STRUCT		*ImageBuffer[ MAX_FLOPPYDRIVES ];	/* For the STX disk images */
+
 } STX_STRUCT;
 
 
 static STX_STRUCT	STX_State;			/* All variables related to the STX support */
+
+
+
+
+/*--------------------------------------------------------------*/
+/* Local functions prototypes					*/
+/*--------------------------------------------------------------*/
+
+static void	STX_BuildSectorsSimple ( STX_TRACK_STRUCT *pStxTrack , Uint8 *p );
 
 
 
@@ -124,6 +135,13 @@ bool STX_WriteDisk(const char *pszFileName, Uint8 *pBuffer, int ImageSize)
  */
 bool	STX_Init ( void )
 {
+	int	i;
+
+	for ( i=0 ; i<MAX_FLOPPYDRIVES ; i++ )
+	{
+		STX_State.ImageBuffer[ i ] = NULL;
+	}
+
 	return true;
 }
 
@@ -136,6 +154,8 @@ bool	STX_Init ( void )
 bool	STX_Insert ( int Drive , Uint8 *pImageBuffer , long ImageSize )
 {
 	fprintf ( stderr , "STX : STX_Insert drive=%d buf=%p size=%ld\n" , Drive , pImageBuffer , ImageSize );
+
+	STX_BuildStruct ( pImageBuffer , 1 );
 
 	return true;
 }
@@ -153,5 +173,259 @@ bool	STX_Eject ( int Drive )
 	return true;
 }
 
+
+
+
+
+static Uint16	STX_ReadU16 ( Uint8 *p )
+{
+	return (p[1]<<8) +p[0];
+}
+
+static Uint32	STX_ReadU32 ( Uint8 *p )
+{
+	return (p[3]<<24) + (p[2]<<16) + (p[1]<<8) +p[0];
+}
+
+
+void	STX_FreeStruct ( STX_MAIN_STRUCT *pStxMain )
+{
+	int			Track;
+
+	if ( !pStxMain )
+		return;
+
+	for ( Track = 0 ; Track < pStxMain->TracksCount ; Track++ )
+	{
+		free ( (pStxMain->pTracksStruct[ Track ]).pSectorsStruct );
+	}
+
+	free ( pStxMain->pTracksStruct );
+	free ( pStxMain );
+}
+
+
+STX_MAIN_STRUCT	*STX_BuildStruct ( Uint8 *pFileBuffer , int Debug )
+{
+
+	STX_MAIN_STRUCT		*pStxMain;
+	STX_TRACK_STRUCT	*pStxTrack;
+	STX_SECTOR_STRUCT	*pStxSector;
+	Uint8			*p;
+	Uint8			*p_cur;
+	int			Track;
+	int			Sector;
+	Uint8			*pFuzzyData;
+
+	pStxMain = malloc ( sizeof ( STX_MAIN_STRUCT ) );
+	if ( !pStxMain )
+		return NULL;
+	memset ( pStxMain , 0 , sizeof ( STX_MAIN_STRUCT ) );
+
+	p = pFileBuffer;
+
+	/* Read file's header */
+	memcpy ( pStxMain->FileID , p , 4 ); p += 4;
+	pStxMain->Version	=	STX_ReadU16 ( p ); p += 2;
+	pStxMain->ImagingTool	=	STX_ReadU16 ( p ); p += 2;
+	pStxMain->Reserved_1	=	STX_ReadU16 ( p ); p += 2;
+	pStxMain->TracksCount	=	*p++;;
+	pStxMain->Revision	=	*p++;
+	pStxMain->Reserved_2	=	STX_ReadU32 ( p ); p += 4;
+
+	if ( Debug )	fprintf ( stderr , "STX header ID='%.4s' Version=%4.4x ImagingTool=%4.4x Reserved1=%4.4x"
+				" TrackCount=%d Revision=%2.2x Reserved2=%x\n" , pStxMain->FileID , pStxMain->Version ,
+				pStxMain->ImagingTool  , pStxMain->Reserved_1 , pStxMain->TracksCount , pStxMain->Revision ,
+				pStxMain->Reserved_2 );
+
+
+	pStxTrack = malloc ( sizeof ( STX_TRACK_STRUCT ) * pStxMain->TracksCount );
+	if ( !pStxTrack )
+	{
+		STX_FreeStruct ( pStxMain );
+		return NULL;
+	}
+	memset ( pStxTrack , 0 , sizeof ( STX_TRACK_STRUCT ) * pStxMain->TracksCount );
+	pStxMain->pTracksStruct = pStxTrack;
+
+	/* Parse all the track blocks */
+	for ( Track = 0 ; Track < pStxMain->TracksCount ; Track++ )
+// 	for ( Track = 0 ; Track < 4 ; Track++ )
+	{
+		p_cur = p;
+
+		pStxTrack->BlockSize		=	STX_ReadU32 ( p ); p += 4;
+		pStxTrack->FuzzySize		=	STX_ReadU32 ( p ); p += 4;
+		pStxTrack->SectorsCount		=	STX_ReadU16 ( p ); p += 2;
+		pStxTrack->Flags		=	STX_ReadU16 ( p ); p += 2;
+		pStxTrack->MFMSize		=	STX_ReadU16 ( p ); p += 2;
+		pStxTrack->TrackNumber		=	*p++;
+		pStxTrack->RecordType		=	*p++;
+
+		if ( pStxTrack->SectorsCount == 0 )			/* Empty / non formatted track */
+		{
+			pStxTrack->pSectorsStruct = NULL;
+			goto next_track;
+		}
+
+		/* Track contains some sectors */
+		pStxSector = malloc ( sizeof ( STX_SECTOR_STRUCT ) * pStxTrack->SectorsCount );
+		if ( !pStxSector )
+		{
+			STX_FreeStruct ( pStxMain );
+			return NULL;
+		}
+		memset ( pStxSector , 0 , sizeof ( STX_SECTOR_STRUCT ) * pStxTrack->SectorsCount );
+		pStxTrack->pSectorsStruct = pStxSector;
+
+
+		/* Do we have some sector infos after the track header or only sector data ? */
+		if ( ( pStxTrack->Flags & STX_TRACK_FLAG_SECTOR_BLOCK ) == 0 )
+		{
+			/* The track only contains SectorsCount sectors of 512 bytes */
+			STX_BuildSectorsSimple ( pStxTrack , p );
+			goto next_track;
+		}
+
+
+		/* Start of the optionnal fuzzy bits data */
+		pStxTrack->pFuzzyData = p + pStxTrack->SectorsCount * STX_SECTOR_BLOCK_SIZE;
+
+		/* Start of the optionnal track data */
+		pStxTrack->pTrackData = pStxTrack->pFuzzyData + pStxTrack->FuzzySize;
+
+		if ( ( pStxTrack->Flags & STX_TRACK_FLAG_TRACK_IMAGE ) == 0 )
+		{
+			pStxTrack->TrackImageSyncPosition = 0;
+			pStxTrack->TrackImageSize = 0;
+			pStxTrack->pTrackImageData = NULL;
+			pStxTrack->pSectorsImageData = pStxTrack->pTrackData;
+		}
+		else if ( ( pStxTrack->Flags & STX_TRACK_FLAG_TRACK_IMAGE_SYNC ) == 0 )	/* Track with size+data */
+		{
+			pStxTrack->TrackImageSyncPosition = 0;
+			pStxTrack->TrackImageSize = STX_ReadU16 ( pStxTrack->pTrackData );
+			pStxTrack->pTrackImageData = pStxTrack->pTrackData + 2;
+			pStxTrack->pSectorsImageData = pStxTrack->pTrackImageData + pStxTrack->TrackImageSize;
+		}
+		else									/* Track with sync offset + size + data */
+		{
+			pStxTrack->TrackImageSyncPosition = STX_ReadU16 ( pStxTrack->pTrackData );
+			pStxTrack->TrackImageSize = STX_ReadU16 ( pStxTrack->pTrackData + 2 );
+			pStxTrack->pTrackImageData = pStxTrack->pTrackData + 4;
+			pStxTrack->pSectorsImageData = pStxTrack->pTrackImageData + pStxTrack->TrackImageSize;
+		}
+
+
+		/* Parse all the sectors in this track */
+		pFuzzyData = pStxTrack->pFuzzyData;
+		for ( Sector = 0 ; Sector < pStxTrack->SectorsCount ; Sector++ )
+		{
+			pStxSector = &(pStxTrack->pSectorsStruct[ Sector ]);
+
+			pStxSector->DataOffset = STX_ReadU32 ( p ); p += 4;
+			pStxSector->BitPosition = STX_ReadU16 ( p ); p += 2;
+			pStxSector->ReadTime = STX_ReadU16 ( p ); p += 2;
+			pStxSector->ID_Track = *p++;
+			pStxSector->ID_Head = *p++;
+			pStxSector->ID_Sector = *p++;
+			pStxSector->ID_Size = *p++;
+			pStxSector->ID_CRC = STX_ReadU16 ( p ); p += 2;
+			pStxSector->FDC_Status = *p++;
+			pStxSector->Reserved = *p++;
+
+			/* Check if sector has data */
+			if ( ( pStxSector->FDC_Status & STX_SECTOR_FLAG_RNF ) == 0 )
+			{
+				/* Check if SectorSize is valid */
+				if ( pStxSector->ID_Size & ~FDC_SECTOR_SIZE_MASK )
+				{
+					fprintf ( stderr , "STX : invalid ID_Size=%d on track %d sector %d\n" ,
+						  pStxSector->ID_Size , Track , Sector );
+				}
+
+				pStxSector->SectorSize = 128 << ( pStxSector->ID_Size & FDC_SECTOR_SIZE_MASK );
+
+				pStxSector->pData = pStxTrack->pTrackData + pStxSector->DataOffset;
+				if ( pStxSector->FDC_Status & STX_SECTOR_FLAG_FUZZY )
+				{
+					pStxSector->pFuzzyData = pFuzzyData;
+					pFuzzyData += pStxSector->SectorSize;
+				}
+// 				if ( pStxSector->FDC_Status & STX_SECTOR_FLAG_VARIABLE_TIME )
+// 					pStxSector->pTimingData = NULL;
+			}
+		}
+
+
+next_track:
+		if ( Debug )
+		{
+			fprintf ( stderr , "  track %3d BlockSize=%d FuzzySize=%d SectorsCount=%4.4x Flags=%4.4x"
+				" MFMSize=%d TrackNumber=%d TrackSide=%d RecordType=%x"
+				" TrackImage=%s (%d bytes, sync=%4.4x)\n" ,
+				Track , pStxTrack->BlockSize ,
+				pStxTrack->FuzzySize , pStxTrack->SectorsCount , pStxTrack->Flags , pStxTrack->MFMSize ,
+				pStxTrack->TrackNumber & 0x7f , ( pStxTrack->TrackNumber >> 7 ) & 0x01 , pStxTrack->RecordType ,
+				pStxTrack->pTrackImageData ? "yes" : "no" , pStxTrack->TrackImageSize , pStxTrack->TrackImageSyncPosition );
+
+			if ( pStxTrack->SectorsCount == 0 )
+				fprintf ( stderr , "    track empty / not formatted\n" );
+			else
+				for ( Sector = 0 ; Sector < pStxTrack->SectorsCount ; Sector++ )
+				{
+					pStxSector = &(pStxTrack->pSectorsStruct[ Sector ]);
+					fprintf ( stderr , "    sector %2d DataOffset=%d BitPosition=%d ReadTime=%d"
+						" [track=%2.2x head=%2.2x sector=%2.2x size=%2.2x crc=%4.4x]"
+						" FdcStatus=%2.2x Reserved=%2.2x\n" ,
+						Sector , pStxSector->DataOffset , pStxSector->BitPosition ,
+						pStxSector->ReadTime ,  pStxSector->ID_Track ,  pStxSector->ID_Head ,
+						pStxSector->ID_Sector , pStxSector->ID_Size , pStxSector->ID_CRC ,
+						pStxSector->FDC_Status , pStxSector->Reserved );
+				}
+		}
+
+		p = p_cur + pStxTrack->BlockSize;			/* Next Track block */
+		pStxTrack++;
+	}
+
+
+
+exit(0);
+
+	return pStxMain;
+}
+
+
+
+
+/*-----------------------------------------------------------------------*/
+/**
+ * When a track only consists of the content of each 512 bytes sector and
+ * no timings informations, we must compute some default values for each
+ * sector, as well as the position of the corresponding 512 bytes of data.
+ * This is only used when storing unprotected tracks.
+ */
+
+static void	STX_BuildSectorsSimple ( STX_TRACK_STRUCT *pStxTrack , Uint8 *p )
+{
+	int	Sector;
+
+	for ( Sector = 0 ; Sector < pStxTrack->SectorsCount ; Sector++ )
+	{
+		pStxTrack->pSectorsStruct[ Sector ].DataOffset = 0;
+		pStxTrack->pSectorsStruct[ Sector ].BitPosition = 0;
+		pStxTrack->pSectorsStruct[ Sector ].ReadTime = 0;
+		pStxTrack->pSectorsStruct[ Sector ].ID_Track = pStxTrack->TrackNumber & 0x7f;
+		pStxTrack->pSectorsStruct[ Sector ].ID_Head = ( pStxTrack->TrackNumber >> 7 ) & 0x01;
+		pStxTrack->pSectorsStruct[ Sector ].ID_Sector = Sector + 1;
+		pStxTrack->pSectorsStruct[ Sector ].ID_Size = FDC_SECTOR_SIZE_512;
+		pStxTrack->pSectorsStruct[ Sector ].ID_CRC = 0;
+		pStxTrack->pSectorsStruct[ Sector ].FDC_Status = 0;
+		pStxTrack->pSectorsStruct[ Sector ].Reserved = 0;
+
+		pStxTrack->pSectorsStruct[ Sector ].pData = p + Sector * 512;
+	}
+}
 
 
--- src/includes/floppy_stx.h
+++ src/includes/floppy_stx.h
@@ -5,6 +5,99 @@
   your option any later version. Read the file gpl.txt for details.
 */
 
+
+
+
+typedef struct {
+	/* Content of the STX sector block (16 bytes) */
+	Uint32		DataOffset;				/* Offset of sector data in the track data */
+	Uint16		BitPosition;				/* Position in bits from the start of the track */
+	Uint16		ReadTime;				/* in ms */
+
+	Uint8		ID_Track;				/* Content of the Address Field */
+	Uint8		ID_Head;
+	Uint8		ID_Sector;
+	Uint8		ID_Size;
+	Uint16		ID_CRC;
+
+	Uint8		FDC_Status;				/* FDC status and flags for this sector */
+	Uint8		Reserved;				/* Unused, always 0 */
+
+	/* Other internal variables */
+	Uint16		SectorSize;				/* In bytes, depends on ID_Size */
+	Uint8		*pData;					/* Bytes for this sector or null if RNF */
+	Uint8		*pFuzzyData;				/* Fuzzy mask for this sector or null if no fuzzy bits */
+	Uint8		*pTimingData;				/* Data for variable bit width or null */
+} STX_SECTOR_STRUCT;
+
+#define	STX_SECTOR_BLOCK_SIZE		( 4+2+2+1+1+1+1+2+1+1)	/* Size of the sector block in an STX file = 16 bytes */
+
+#define	STX_SECTOR_FLAG_VARIABLE_TIME	(1<<0)			/* bit 0, if set, this sector has variable bit width */
+#define	STX_SECTOR_FLAG_CRC		(1<<3)			/* bit 3, if set, there's a CRC error */
+#define	STX_SECTOR_FLAG_RNF		(1<<4)			/* bit 4, if set, there's no sector data */
+#define	STX_SECTOR_FLAG_RECORD_TYPE	(1<<5)			/* bit 5, if set, deleted data */
+#define	STX_SECTOR_FLAG_FUZZY		(1<<7)			/* bit 7, if set, this sector has fuzzy bits */
+
+#define	STX_SECTOR_READ_TIME_DEFAULT	16384			/* Default value if ReadTime==0 */
+
+
+typedef struct {
+	/* Content of the STX track block (16 bytes) */
+	Uint32		BlockSize;				/* Number of bytes in this track block */
+	Uint32		FuzzySize;				/* Number of bytes in fuzzy mask */
+	Uint16		SectorsCount;				/* Number of sector blocks in this track */
+	Uint16		Flags;					/* Flags for this track */
+	Uint16		MFMSize;				/* Number of MFM bytes in this track */
+	Uint8		TrackNumber;				/* bits 0-6 = track number   bit 7 = side */
+	Uint8		RecordType;				/* Unused */
+
+	/* Other internal variables */
+	STX_SECTOR_STRUCT	*pSectorsStruct;		/* All the sectors struct for this track or null */
+
+	Uint8			*pTrackData;			/* */
+	Uint16			TrackImageSyncPosition;
+	Uint16			TrackImageSize;			/* Number of bytes in pTrackImageData */
+	Uint8			*pTrackImageData;		/* Optionnal data as returned by the read track command */
+	Uint8			*pSectorsImageData;		/* Optionnal data for the sectors of this track */
+
+	Uint8			*pFuzzyData;			/* Fuzzy mask data for all the fuzzy sectors of the track */
+} STX_TRACK_STRUCT;
+
+#define	STX_TRACK_BLOCK_SIZE		( 4+4+2+2+2+1+1 )	/* Size of the track block in an STX file = 16 bytes */
+
+#define	STX_TRACK_FLAG_SECTOR_BLOCK	(1<<0)			/* bit 0, if set, this track contains sector blocks */
+#define	STX_TRACK_FLAG_TRACK_IMAGE	(1<<6)			/* bit 6, if set, this track contains a track image */
+#define	STX_TRACK_FLAG_TRACK_IMAGE_SYNC	(1<<7)			/* bit 7, if set, the track image has a sync position */
+
+
+
+
+typedef struct {
+	/* Content of the STX header block (16 bytes) */
+	char		FileID[ 4 ];				/* Should be "RSY\0" */
+	Uint16		Version;				/* Only version 3 is supported */
+	Uint16		ImagingTool;				/* 0x01 (Atari Tool) or 0xCC (Discovery Cartridge) */
+	Uint16		Reserved_1;				/* Unused */
+	Uint8		TracksCount;				/* Number of track blocks in this file */
+	Uint8		Revision;				/* 0x00 (old Pasti file)   0x02 (new Pasti file) */
+	Uint32		Reserved_2;				/* Unused */
+
+	/* Other internal variables */
+	STX_TRACK_STRUCT	*pTracksStruct;
+} STX_MAIN_STRUCT;
+
+#define	STX_MAIN_BLOCK_SIZE		( 4+2+2+2+1+1+4 )	/* Size of the header block in an STX file = 16 bytes */
+
+
+
+#define	FDC_SECTOR_SIZE_MASK			0x03		/* Only bits 0-1 of the Sector size in the ID field are used by the WD1772 */
+
+#define	FDC_SECTOR_SIZE_128			0		/* Sector size used in the ID fields */
+#define	FDC_SECTOR_SIZE_256			1
+#define	FDC_SECTOR_SIZE_512			2
+#define	FDC_SECTOR_SIZE_1024			3
+
+
 extern void	STX_MemorySnapShot_Capture(bool bSave);
 extern bool	STX_FileNameIsSTX(const char *pszFileName, bool bAllowGZ);
 extern Uint8	*STX_ReadDisk(const char *pszFileName, long *pImageSize, int *pImageType);
@@ -13,4 +106,7 @@ extern bool	STX_WriteDisk(const char *pszFileName, Uint8 *pBuffer, int ImageSize
 extern bool	STX_Init ( void );
 extern bool	STX_Insert ( int Drive , Uint8 *pImageBuffer , long ImageSize );
 extern bool	STX_Eject ( int Drive );
+
+extern void	STX_FreeStruct ( STX_MAIN_STRUCT *pStxMain );
+extern STX_MAIN_STRUCT *STX_BuildStruct ( Uint8 *pFileBuffer , int Debug );
 
