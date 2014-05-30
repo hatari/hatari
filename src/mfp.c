3d6eae0425bebc0fefadb887681acdccc6ed6b5b
Nicolas Pomarède <npomarede@corp.free.fr>
2014-05-30 14:17:36 +0200

    Increase MFP jitter for Lethal Xcess

--- src/mfp.c
+++ src/mfp.c
@@ -783,7 +783,7 @@ static int MFP_StartTimer_AB(Uint8 TimerControl, Uint16 TimerData, interrupt_id 
 		if ( ( M68000_GetPC() == 0x14d78 ) && ( STMemory_ReadLong ( 0x14d6c ) == 0x11faff75 ) )
 		{
 //			fprintf ( stderr , "mfp add jitter %d\n" , TimerClockCycles );
-			TimerClockCycles += rand()%3-1;		/* add jitter for wod2 */
+			TimerClockCycles += rand()%5-2;		/* add jitter for wod2 */
 		}
 
 		if (LOG_TRACE_LEVEL(TRACE_MFP_START))
