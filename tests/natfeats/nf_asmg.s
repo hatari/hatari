c5b9adeb12f989549dbddc43270e11f432ccbc9b
Eero Tamminen <oak@helsinkinet.fi>
2014-06-01 19:26:11 +0300

    rename & fix NF bindings GCC assembly file
    - fix: GCC 2.x Gas doesn't like C++ comments
    - fix: exported symbol name

--- tests/natfeats/nf_asmg.s
+++ tests/natfeats/nf_asmg.s
@@ -17,7 +17,7 @@
  */
         .globl  _nf_id
         .globl  _nf_call
-        .globl  _nf_cookie
+        .globl  _detect_nf
 
 /* illegal exception vector */
         .equ vec_illegal, 0x10
@@ -34,7 +34,7 @@
  * otherwise exception handler change bombs
  */
 _detect_nf:
-        clr.l   d0               // assume no NatFeats available
+        clr.l   d0               /* assume no NatFeats available */
         move.l  sp,a1
         move.l  vec_illegal,a0
         move.l  #fail_nf,vec_illegal
@@ -43,11 +43,11 @@ _detect_nf:
 #ifdef __mcoldfire__
 #error Conflict with instruction mvs.b d0,d1
 #else
-        .dc.w   0x7300              // Jump to NATFEAT_ID
+        .dc.w   0x7300              /* Jump to NATFEAT_ID */
 #endif
         tst.l   d0
         beq.s   fail_nf
-        moveq   #1,d0               // NatFeats detected
+        moveq   #1,d0               /* NatFeats detected */
 
 fail_nf:
         move.l  a1,sp
@@ -63,12 +63,12 @@ nf_version_name:
  * map native features to NF ID
  */
 _nf_id:
-        .dc.w   0x7300  // Conflict with ColdFire instruction mvs.b d0,d1
+        .dc.w   0x7300  /* Conflict with ColdFire instruction mvs.b d0,d1 */
         rts
 
 /*
  * call native feature by its ID
  */
 _nf_call:
-        .dc.w   0x7301  // Conflict with ColdFire instruction mvs.b d1,d1
+        .dc.w   0x7301  /* Conflict with ColdFire instruction mvs.b d1,d1 */
         rts
