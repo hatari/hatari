7f0deb7295839fc6bc19c998e1b7fe974379c02e
Thomas Huth <huth@tuxfamily.org>
2014-08-04 14:48:09 +0200

    Clean up comments in SDLMain.m and SDLMain.h

--- src/gui-osx/SDLMain.m
+++ src/gui-osx/SDLMain.m
@@ -1,18 +1,10 @@
-//
-//  HatariAppDelegate.m
-//  Hatari
-//
-//  Créé le 12/06/13 par Miguel Saro.
-//  Tout droits réservés, - Cocoa Pod -, 2013.
-//
-
-/*   SDLMain.m - main entry point for our Cocoa-ized SDL app
+/*
+    SDLMain.m - main entry point for our Cocoa-ized SDL app
        Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
        Non-NIB-Code & other changes: Max Horn <max@quendi.de>
+       Modifications for Hatari by Miguel Saro and Jerome Vernet
 
     Feel free to customize this file to suit your needs
-
-
 */
 
 /* Use this flag to determine whether we use SDLMain.nib or not */
