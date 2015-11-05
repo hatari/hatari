109c7e4f1bcfaab9594fcec22eb9ce9f47cbb839
Laurent Sallafranque <laurent.sallafranque@free.fr>
2015-11-05 22:41:53 +0100

    another fix for dsp adressing mode by Andreas, for testing purpose

--- src/falcon/dsp_cpu.c
+++ src/falcon/dsp_cpu.c
@@ -152,9 +152,9 @@ static void dsp_compute_ssh_ssl(void);
 
 static void opcode8h_0(void);
 
-static void dsp_update_rn(Uint32 numreg, Sint16 modifier, Sint8 sign);
+static void dsp_update_rn(Uint32 numreg, Sint16 modifier);
 static void dsp_update_rn_bitreverse(Uint32 numreg);
-static void dsp_update_rn_modulo(Uint32 numreg, Sint16 modifier, Sint8 sign);
+static void dsp_update_rn_modulo(Uint32 numreg, Sint16 modifier);
 static int dsp_calc_ea(Uint32 ea_mode, Uint32 *dst_addr);
 static int dsp_calc_cc(Uint32 cc_code);
 
@@ -1561,7 +1561,7 @@ static void dsp_compute_ssh_ssl(void)
  *	Effective address calculation
  **********************************/
 
-static void dsp_update_rn(Uint32 numreg, Sint16 modifier, Sint8 sign)
+static void dsp_update_rn(Uint32 numreg, Sint16 modifier)
 {
 	Sint16 value;
 	Uint16 m_reg;
@@ -1570,14 +1570,14 @@ static void dsp_update_rn(Uint32 numreg, Sint16 modifier, Sint8 sign)
 	if (m_reg == 65535) {
 		/* Linear addressing mode */
 		value = (Sint16) dsp_core.registers[DSP_REG_R0+numreg];
-		value += modifier*sign;
+		value += modifier;
 		dsp_core.registers[DSP_REG_R0+numreg] = ((Uint32) value) & BITMASK(16);
 	} else if (m_reg == 0) {
 		/* Bit reversed carry update */
 		dsp_update_rn_bitreverse(numreg);
 	} else if (m_reg<=32767) {
 		/* Modulo update */
-		dsp_update_rn_modulo(numreg, modifier, sign);
+		dsp_update_rn_modulo(numreg, modifier);
 	} else {
 		/* Undefined */
 	}
@@ -1624,57 +1624,51 @@ static void dsp_update_rn_bitreverse(Uint32 numreg)
 	dsp_core.registers[DSP_REG_R0+numreg] = value;
 }
 
-static void dsp_update_rn_modulo(Uint32 numreg, Sint16 modifier, Sint8 sign)
+static void dsp_update_rn_modulo(Uint32 numreg, Sint16 modifier)
 {
-	Uint16 bufsize, modulo, lobound, hibound, bufmask;
-	Sint16 r_reg, orig_modifier=modifier;
-	float p;
+	Uint16 bufsize, bufmask, modulo, abs_modifier;
+	Uint32 r_reg, lobound, hibound;
 
+	r_reg = dsp_core.registers[DSP_REG_R0+numreg]|0x10000;
 	modulo = dsp_core.registers[DSP_REG_M0+numreg]+1;
+
+
 	bufsize = 1;
-	bufmask = BITMASK(16);
 	while (bufsize < modulo) {
 		bufsize <<= 1;
-		bufmask <<= 1;
 	}
+	bufmask = bufsize - 1;
 
-	lobound = dsp_core.registers[DSP_REG_R0+numreg] & bufmask;
+
+	lobound = r_reg - (r_reg&bufmask);
 	hibound = lobound + modulo - 1;
 
-	r_reg = (Sint16) dsp_core.registers[DSP_REG_R0+numreg];
 
-
-	// [LS]
-	// If Nn>M, the result is data dependent and unpredictable, except for the special case where
-	// Nn=P * 2exp(k) , a multiple of the block size where P is a positive integer.
-	p = (float)(orig_modifier/bufsize);
-	if ((p == (int)p) && (orig_modifier>modulo) && (p>0.)) {
-		r_reg += p*bufsize*sign;
+	if (modifier<0) {
+		abs_modifier = -modifier;
 	} else {
-		modifier *= sign;
-		if (orig_modifier>modulo) {
-			while (modifier>bufsize) {
-				r_reg += bufsize;
-				modifier -= bufsize;
-			}
-			while (modifier<-bufsize) {
-				r_reg -= bufsize;
-				modifier += bufsize;
-			}
-		}
+		abs_modifier = modifier;
+	}
 
+
+	if (abs_modifier>modulo) {
+		if (abs_modifier&bufmask) {
+			fprintf(stderr,"Dsp: Modulo addressing result unpredictable\n");
+		} else {
+			r_reg += modifier;
+		}
+	} else {
 		r_reg += modifier;
 
-		if (orig_modifier!=modulo) {
-			if (r_reg>hibound) {
-				r_reg -= modulo;
-			} else if (r_reg<lobound) {
-				r_reg += modulo;
-			}
+
+		if (r_reg>hibound) {
+			r_reg -= modulo;
+		} else if (r_reg<lobound) {
+			r_reg += modulo;
 		}
 	}
 
-	dsp_core.registers[DSP_REG_R0+numreg] = ((Uint32) r_reg) & BITMASK(16);
+	dsp_core.registers[DSP_REG_R0+numreg] = r_reg & BITMASK(16);
 }
 
 static int dsp_calc_ea(Uint32 ea_mode, Uint32 *dst_addr)
@@ -1687,22 +1681,22 @@ static int dsp_calc_ea(Uint32 ea_mode, Uint32 *dst_addr)
 		case 0:
 			/* (Rx)-Nx */
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
-			dsp_update_rn(numreg, dsp_core.registers[DSP_REG_N0+numreg], -1);
+			dsp_update_rn(numreg, -dsp_core.registers[DSP_REG_N0+numreg]);
 			break;
 		case 1:
 			/* (Rx)+Nx */
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
-			dsp_update_rn(numreg, dsp_core.registers[DSP_REG_N0+numreg], +1);
+			dsp_update_rn(numreg, dsp_core.registers[DSP_REG_N0+numreg]);
 			break;
 		case 2:
 			/* (Rx)- */
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
-			dsp_update_rn(numreg, 1, -1);
+			dsp_update_rn(numreg, -1);
 			break;
 		case 3:
 			/* (Rx)+ */
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
-			dsp_update_rn(numreg, 1, +1);
+			dsp_update_rn(numreg, 1);
 			break;
 		case 4:
 			/* (Rx) */
@@ -1712,7 +1706,7 @@ static int dsp_calc_ea(Uint32 ea_mode, Uint32 *dst_addr)
 			/* (Rx+Nx) */
 			dsp_core.instr_cycle += 2;
 			curreg = dsp_core.registers[DSP_REG_R0+numreg];
-			dsp_update_rn(numreg, dsp_core.registers[DSP_REG_N0+numreg], +1);
+			dsp_update_rn(numreg, dsp_core.registers[DSP_REG_N0+numreg]);
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
 			dsp_core.registers[DSP_REG_R0+numreg] = curreg;
 			break;
@@ -1728,7 +1722,7 @@ static int dsp_calc_ea(Uint32 ea_mode, Uint32 *dst_addr)
 		case 7:
 			/* -(Rx) */
 			dsp_core.instr_cycle += 2;
-			dsp_update_rn(numreg, 1, -1);
+			dsp_update_rn(numreg, -1);
 			*dst_addr = dsp_core.registers[DSP_REG_R0+numreg];
 			break;
 	}
