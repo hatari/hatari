7bbeb6bd8b8f6764738abe0eee74ab57172da9bc
Eero Tamminen <oak@helsinkinet.fi>
2016-04-23 11:47:11 +0300

    Fix --desktop-st with SDL2
    Commit 5964 disabling --desktop-st forgot to increase
    argument index for the parameter.

--- src/options.c
+++ src/options.c
@@ -1095,6 +1095,7 @@ bool Opt_ParseParameters(int argc, const char * const argv[])
 		case OPT_RESOLUTION_ST:
 #if WITH_SDL2
 			fprintf(stderr, "The --desktop-st option is not supported in this version (with SDL2)!\n");
+			i++;
 #else
 			ok = Opt_Bool(argv[++i], OPT_RESOLUTION_ST, &ConfigureParams.Screen.bKeepResolutionST);
 #endif
