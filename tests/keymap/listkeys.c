d8b6d5a23c58bf5db3bb8fff7a062f6db4b043ce
Eero Tamminen <oak@helsinkinet.fi>
2016-10-05 00:08:53 +0300

    Listkeys is no-op under SDL2
    SDL2 doesn't support iterating over all key codes.

--- tests/keymap/listkeys.c
+++ tests/keymap/listkeys.c
@@ -1,4 +1,3 @@
-
 /* Print out all the keysyms we have, just to verify them */
 
 #include <stdio.h>
@@ -10,16 +9,21 @@
 
 int main(int argc, char *argv[])
 {
+#if SDL_MAJOR_VERSION > 1
+	#warning "SDL2 doesn't support iterating over all keys"
+	printf("Sorry, SDL2 doesn't support iterating over all keys.\n");
+#else
 	SDLKey key;
-
-	if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		fprintf(stderr, "Couldn't initialize SDL: %s\n",
 			SDL_GetError());
 		exit(1);
 	}
-	for ( key=SDLK_FIRST; key<SDLK_LAST; ++key ) {
+
+	for (key = SDLK_FIRST; key < SDLK_LAST; ++key) {
 		printf("Key #%d, \"%s\"\n", key, SDL_GetKeyName(key));
 	}
 	SDL_Quit();
+#endif
 	return 0;
 }
