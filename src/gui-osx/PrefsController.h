45b5fa9df67e1263c24b53bc8b75ec01a687cc71
Nicolas Pomarède <npomarede@corp.free.fr>
2018-02-11 17:18:52 +0100

    Add constant NSAlertStyleInformational for older macOS 10.11 (patch by Benoit Tuduri)

--- src/gui-osx/PrefsController.h
+++ src/gui-osx/PrefsController.h
@@ -8,6 +8,10 @@
 #import <Cocoa/Cocoa.h>
 
 
+#if (!defined MAC_OS_X_VERSION_10_12) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12
+#define NSAlertStyleInformational NSInformationalAlertStyle
+#endif
+
 @interface PrefsController : NSObject
 {
 	// Preferences window
