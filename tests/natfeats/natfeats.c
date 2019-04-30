7a01ee2662c96c6339f40da9741a00d59673aa3e
Eero Tamminen <oak@helsinkinet.fi>
2019-05-01 00:47:50 +0300

    NatFeats: update / expand test code
    - Add end-user wrapper functions for missing NF functions
    - Add (disabled) test code for NF_VERSION
    - Fix GCC 4.6 Cconws() warnings with (void) cast
    - Fit error msg to 40 col screen

--- tests/natfeats/natfeats.c
+++ tests/natfeats/natfeats.c
@@ -1,7 +1,7 @@
 /*
  * natfeat.c - NatFeats API examples
  *
- * Copyright (c) 2014 by Eero Tamminen
+ * Copyright (c) 2014-2016, 2019 by Eero Tamminen
  * 
  * NF initialization & calling is based on EmuTOS code,
  * Copyright (c) 2001-2003 The EmuTOS development team
@@ -17,6 +17,8 @@
 #endif
 #include "natfeats.h"
 
+#define MSG_NF_MISSING \
+	"\r\nStart Hatari with option:\r\n\t--natfeats yes\r\n"
 
 /* NatFeats available & initialized */
 static int nf_ok;
@@ -39,9 +41,40 @@ int nf_init(void)
 		nfid_debugger = nf_id("NF_DEBUGGER");
 		nfid_fastforward = nf_id("NF_FASTFORWARD");
 	} else {
-		Cconws("Native Features initialization failed!\r\n");
+		(void)Cconws("Native Features initialization failed!\r\n");
 	}
 	return nf_ok;
+}
+
+long nf_version(void)
+{
+	long id;
+	if(nf_ok && (id = nf_id("NF_VERSION"))) {
+		return nf_call(id);
+	} else {
+		(void)Cconws("NF_VERSION unavailable!\r\n");
+		return 0;
+	}
+}
+
+static long getname(char *buf, long len, int subid)
+{
+	long id;
+	if (nf_ok && (id = nf_id("NF_NAME"))) {
+		return nf_call(id | subid, buf, (long)len);
+	} else {
+		(void)Cconws("NF_NAME unavailable!\r\n");
+		return 0;
+	}
+}
+
+long nf_name(char *buf, long len)
+{
+	return getname(buf, len, 0x0);
+}
+long nf_fullname(char *buf, long len)
+{
+	return getname(buf, len, 0x1);
 }
 
 long nf_print(const char *text)
@@ -49,7 +82,7 @@ long nf_print(const char *text)
 	if (nfid_print) {
 		return nf_call(nfid_print, text);
 	} else {
-		Cconws("NF_STDERR unavailable!\r\n");
+		(void)Cconws("NF_STDERR unavailable!\r\n");
 		return 0;
 	}
 }
@@ -59,7 +92,7 @@ long nf_debugger(void)
 	if (nfid_debugger) {
 		return nf_call(nfid_debugger);
 	} else {
-		Cconws("NF_DEBUGGER unavailable!\r\n");
+		(void)Cconws("NF_DEBUGGER unavailable!\r\n");
 		return 0;
 	}
 }
@@ -69,22 +102,39 @@ long nf_fastforward(long enabled)
 	if (nfid_fastforward) {
 		return nf_call(nfid_fastforward, enabled);
 	} else {
-		Cconws("NF_FASTFORWARD unavailable!\r\n");
+		(void)Cconws("NF_FASTFORWARD unavailable!\r\n");
 		return 0;
 	}
 }
 
-void nf_shutdown(void)
+static void halt_reset(int subid)
 {
 	long id;
 	if(nf_ok && (id = nf_id("NF_SHUTDOWN"))) {
 		void *sup = (void*)Super(0);
 		/* needs to be called in supervisor mode */
-		nf_call(id);
+		nf_call(id | subid);
 		Super(sup);
 	} else {
-		Cconws("NF_SHUTDOWN unavailable!\r\n");
+		(void)Cconws("NF_SHUTDOWN unavailable!\r\n");
 	}
+}
+
+void nf_shutdown(void)
+{
+	halt_reset(0x0);
+}
+void nf_reset(void)
+{
+	halt_reset(0x1);
+}
+void nf_reset_cold(void)
+{
+	halt_reset(0x2);
+}
+void nf_poweroff(void)
+{
+	halt_reset(0x3);
 }
 
 void nf_exit(long exitval)
@@ -94,7 +144,7 @@ void nf_exit(long exitval)
 		nf_call(id, exitval);
 	} else {
 		/* NF_EXIT is Hatari specific, NF_SHUTDOWN isn't */
-		Cconws("NF_EXIT unavailable, trying NF_SHUTDOWN...\r\n");
+		(void)Cconws("NF_EXIT unavailable, trying NF_SHUTDOWN...\r\n");
 		nf_shutdown();
 	}
 }
@@ -104,26 +154,38 @@ void nf_exit(long exitval)
 /* show emulator name */
 static void nf_showname(void)
 {
-	long id;
-	if (nf_ok && (id = nf_id("NF_NAME"))) {
-		long chars = 0;
-		char buffer[64];
-		id |= 0x0001;  /* name + version */
-		chars = nf_call(id, buffer, (long)(sizeof(buffer)-2));
-		buffer[chars++] = '\n';
-		buffer[chars++] = '\0';
-		nf_print(buffer);
-	} else {
-		Cconws("NF_NAME unavailable!\r\n");
-	}
+	long chars;
+	char buffer[64];
+	chars = nf_fullname(buffer, (long)(sizeof(buffer)-2));
+	buffer[chars++] = '\n';
+	buffer[chars++] = '\0';
+	nf_print(buffer);
 }
+
+#if 1
+# define nf_showversion(void)
+#else
+/* printf requires adding ahcstart.o & ahccstdi.lib to nf_ahcc.prj,
+ * but those need features not supported by Hatari's dummy TOS
+ * (used in automated tests).
+ *
+ * Seeing the output requires "--conout 2" Hatari option, as
+ * emulated display doesn't have time to refresh before exit.
+ */
+#include <stdio.h>
+static void nf_showversion(void)
+{
+	long version = nf_version();
+	printf("NF API version: 0x%x\n", version);
+}
+#endif
 
 static int wait_key(void)
 {
 	while (Cconis()) {
 		Cconin();
 	}
-	Cconws("\r\n<press key>\r\n");
+	(void)Cconws("\r\n<press key>\r\n");
 	return Cconin();
 }
 
@@ -131,7 +193,7 @@ int main()
 {
 	long old_ff;
 	if (!nf_init()) {
-		Cconws("\r\nStart Hatari with '--natfeats yes' option!\r\n");
+		(void)Cconws(MSG_NF_MISSING);
 		wait_key();
 		return 1;
 	}
