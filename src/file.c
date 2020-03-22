hatari/hatari

commit 1915c2a0e6f814ad80eed147408eea543c1a3ccd

Add a function to load and relocate PRG files

src/gemdos.c
@@ -3533,3 +3533,69 @@ void GemDOS_Boot(void)
 	/* Setup new GEMDOS handler, see "cart_asm.s" */
 	STMemory_WriteLong(0x0084, CART_GEMDOS);
 }
+
+
+/**
+ * Load and relocate a PRG file into the memory of the emulated machine.
+ */
+int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr)
+{
+	long nFileSize;
+	uint8_t *prg;
+	uint32_t nTextLen, nDataLen, nBssLen, nSymLen;
+	uint32_t nRelOff, nRelTabIdx, nCurrAddr;
+
+	prg = File_Read(psPrgName, &nFileSize, NULL);
+	if (!prg || nFileSize < 30)
+	{
+		Log_Printf(LOG_ERROR, "Failed to load '%s'.\n", psPrgName);
+		return -1;
+	}
+
+	if (prg[0] != 0x60 || prg[1] != 0x1a)  /* Check PRG magic */
+	{
+		Log_Printf(LOG_ERROR, "The file '%s' is not a valid PRG.\n", psPrgName);
+		return -1;
+	}
+
+	nTextLen = (prg[2] << 24) | (prg[3] << 16) | (prg[4] << 8) | prg[5];
+	nDataLen = (prg[6] << 24) | (prg[7] << 16) | (prg[8] << 8) | prg[9];
+	nBssLen = (prg[10] << 24) | (prg[11] << 16) | (prg[12] << 8) | prg[13];
+	nSymLen = (prg[14] << 24) | (prg[15] << 16) | (prg[16] << 8) | prg[17];
+
+	if (!STMemory_SafeCopy(baseaddr + 0x100, prg + 28, nTextLen + nDataLen, psPrgName))
+		return -1;
+
+	/* Clear BSS */
+	memset(&STRam[baseaddr + 0x100 + nTextLen + nDataLen], 0, nBssLen);
+
+	if (*(uint16_t *)&prg[26] != 0)   /* No reloc information available? */
+		return 0;
+
+	nRelTabIdx = 0x1c + nTextLen + nDataLen + nSymLen;
+	nRelOff = (prg[nRelTabIdx] << 24) | (prg[nRelTabIdx + 1] << 16)
+	          | (prg[nRelTabIdx + 2] << 8) | prg[nRelTabIdx + 3];
+
+	if (nRelOff == 0)
+		return 0;
+
+	nCurrAddr = baseaddr + 0x100 + nRelOff;
+	STMemory_WriteLong(nCurrAddr, STMemory_ReadLong(nCurrAddr) + baseaddr + 0x100);
+	nRelTabIdx += 4;
+
+	while (prg[nRelTabIdx])
+	{
+		if (prg[nRelTabIdx] == 1)
+		{
+			nRelOff += 254;
+			nRelTabIdx += 1;
+			continue;
+		}
+		nRelOff += prg[nRelTabIdx];
+		nCurrAddr = baseaddr + 0x100 + nRelOff;
+		STMemory_WriteLong(nCurrAddr, STMemory_ReadLong(nCurrAddr) + baseaddr + 0x100);
+		nRelTabIdx += 1;
+	}
+
+	return 0;
+}

src/includes/gemdos.h
@@ -30,5 +30,6 @@ extern void GemDOS_Info(FILE *fp, Uint32 bShowOpcodes);
 extern void GemDOS_InfoDTA(FILE *fp, Uint32 addrDTA);
 extern void GemDOS_OpCode(void);
 extern void GemDOS_Boot(void);
+extern int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr);
 
 #endif /* HATARI_GEMDOS_H */

commit ba46987a60d7f63d5ee00631ca6efa83f7d224c2

Fix: GEMDOS HD Pexec for files named as *.gz or *.zip
Split normal file reading to a separate File_ReadAsIs() function and
call it from original File_Read().

Gzip / Zip extension checking and automatic uncompressed is unwanted
for files accessed through GEMDOS HD emulation, such as Pexec programs
and auto-start *.INF files.  Use the new File_ReadAsIs() function for
them instead of File_Read().

Fixes a regression with an Atari game which loader Pexec'ing a file
called "out.zip".

Lilo code does GZ uncompression explicitly, so that was changed too.

Which leaves following users of File_Read() i.e. automatic extension
handling and uncompression:
* cartridge & TOS image loading
* different floppy disk format image handlers
* hmsa tool

Fixes: 1915c2a0e

src/file.c
@@ -228,8 +228,48 @@ Uint8 *File_ZlibRead(const char *pszFileName, long *pFileSize)
 
 /**
  * Read file from disk into allocated buffer and return the buffer
- * or NULL for error.  If pFileSize is non-NULL, read file size
- * is set to that.
+ * unmodified, or NULL for error.  If pFileSize is non-NULL, read
+ * file size is set to that.
+ */
+Uint8 *File_ReadAsIs(const char *pszFileName, long *pFileSize)
+{
+	Uint8 *pFile = NULL;
+	long FileSize = 0;
+	FILE *hDiskFile;
+
+	/* Open and read normal file */
+	hDiskFile = fopen(pszFileName, "rb");
+	if (hDiskFile != NULL)
+	{
+		/* Find size of file: */
+		if (fseek(hDiskFile, 0, SEEK_END) == 0)
+		{
+			FileSize = ftell(hDiskFile);
+			if (FileSize > 0 && fseek(hDiskFile, 0, SEEK_SET) == 0)
+			{
+				/* Read in... */
+				pFile = malloc(FileSize);
+				if (pFile)
+					FileSize = fread(pFile, 1, FileSize, hDiskFile);
+			}
+		}
+		fclose(hDiskFile);
+	}
+
+	/* Store size of file we read in (or 0 if failed) */
+	if (pFileSize)
+		*pFileSize = FileSize;
+
+	return pFile;        /* Return to where read in/allocated */
+}
+
+/**
+ * Read file from disk into allocated buffer and return the buffer or
+ * NULL for error.  If file is missing, given additional compression
+ * extension are checked too.  If file has one of the supported
+ * compression extension, its contents are uncompressed (in case of
+ * ZIP, first file in it is read).  If pFileSize is non-NULL, read
+ * file size is set to that.
  */
 Uint8 *File_Read(const char *pszFileName, long *pFileSize, const char * const ppszExts[])
 {
@@ -257,28 +297,11 @@ Uint8 *File_Read(const char *pszFileName, long *pFileSize, const char * const pp
 		/* It is a .ZIP file! -> Try to load the first file in the archive */
 		pFile = ZIP_ReadFirstFile(filepath, &FileSize, ppszExts);
 	}
-	else          /* It is a normal file */
+	else
 #endif  /* HAVE_LIBZ */
 	{
-		FILE *hDiskFile;
-		/* Open and read normal file */
-		hDiskFile = fopen(filepath, "rb");
-		if (hDiskFile != NULL)
-		{
-			/* Find size of file: */
-			if (fseek(hDiskFile, 0, SEEK_END) == 0)
-			{
-				FileSize = ftell(hDiskFile);
-				if (FileSize > 0 && fseek(hDiskFile, 0, SEEK_SET) == 0)
-				{
-					/* Read in... */
-					pFile = malloc(FileSize);
-					if (pFile)
-						FileSize = fread(pFile, 1, FileSize, hDiskFile);
-				}
-			}
-			fclose(hDiskFile);
-		}
+		/* It is a normal file */
+		pFile = File_ReadAsIs(filepath, &FileSize);
 	}
 	free(filepath);
 
@@ -293,6 +316,7 @@ Uint8 *File_Read(const char *pszFileName, long *pFileSize, const char * const pp
 /*-----------------------------------------------------------------------*/
 /**
  * Save file to disk, return FALSE if errors
+ * If built with ZLib support + file name ends with *.gz, compress it first
  */
 bool File_Save(const char *pszFileName, const Uint8 *pAddress, size_t Size, bool bQueryOverwrite)
 {

src/gemdos.c
@@ -4091,7 +4091,7 @@ int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr, bool bFullBpSe
 	uint32_t nRelOff, nCurrAddr;
 	uint32_t memtop;
 
-	prg = File_Read(psPrgName, &nFileSize, NULL);
+	prg = File_ReadAsIs(psPrgName, &nFileSize);
 	if (!prg)
 	{
 		Log_Printf(LOG_ERROR, "Failed to load '%s'.\n", psPrgName);

src/includes/file.h
@@ -25,6 +25,7 @@ extern bool File_ChangeFileExtension(const char *Filename_old, const char *Exten
 extern const char *File_RemoveFileNameDrive(const char *pszFileName);
 extern bool File_DoesFileNameEndWithSlash(char *pszFileName);
 extern Uint8 *File_ZlibRead(const char *pszFileName, long *pFileSize);
+extern Uint8 *File_ReadAsIs(const char *pszFileName, long *pFileSize);
 extern Uint8 *File_Read(const char *pszFileName, long *pFileSize, const char * const ppszExts[]);
 extern bool File_Save(const char *pszFileName, const Uint8 *pAddress, size_t Size, bool bQueryOverwrite);
 extern off_t File_Length(const char *pszFileName);

src/inffile.c
@@ -626,7 +626,7 @@ static char *get_inf_file(const char **set_infname, int *set_size, int *res_col)
 	GemDOS_Info(stderr, 0);
 	fprintf(stderr, "\nChecking for existing INF file '%s' -> '%s'...\n", infname, hostname);
 #endif
-	host_content = File_Read(hostname, &host_size, NULL);
+	host_content = File_ReadAsIs(hostname, &host_size);
 
 	if (host_content)
 	{

src/lilo.c
@@ -395,7 +395,7 @@ static void *load_file(const char *filename, uint32_t *length)
 #ifdef HAVE_LIBZ
 	buffer = File_ZlibRead(filename, &nFileLength);
 #else
-	buffer = File_Read(filename, &nFileLength, NULL);
+	buffer = File_ReadAsIs(filename, &nFileLength);
 #endif
 	*length = nFileLength;
 