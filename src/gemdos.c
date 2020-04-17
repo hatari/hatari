hatari/hatari

commit bcbdec8b63b22da68842d6f1630e3c1adc9cc816

Improve Fsnext() handling / compatibility
Return error for unrecognized DTA indeces.
Will help later GEMDOS HD memory snapshot handling improvements.

Zero DTA file name when there's no match with older TOS version.

src/gemdos.c
@@ -2698,12 +2698,23 @@ static bool GemDOS_SNext(void)
 		Regs[REG_D0] = GEMDOS_ENMFIL;
 		return true;
 	}
+	if (!InternalDTAs[Index].bUsed)
+	{
+		/* Invalid handle, TOS returns ENMFIL
+		 * (if Fsetdta() has been used by any process)
+		 */
+		Log_Printf(LOG_WARN, "GEMDOS Fsnext(): Invalid DTA\n");
+		Regs[REG_D0] = GEMDOS_ENMFIL;
+	}
 
 	temp = InternalDTAs[Index].found;
 	do
 	{
 		if (InternalDTAs[Index].centry >= InternalDTAs[Index].nentries)
 		{
+			/* older TOS versions zero file name if there are no (further) matches */
+			if (TosVersion < 0x0400)
+				pDTA->dta_name[0] = 0;
 			Regs[REG_D0] = GEMDOS_ENMFIL;    /* No more files */
 			return true;
 		}
@@ -2715,7 +2726,7 @@ static bool GemDOS_SNext(void)
 
 	if (ret < 0)
 	{
-		Log_Printf(LOG_WARN, "GEMDOS Fsnext(): Error setting DTA.\n");
+		Log_Printf(LOG_WARN, "GEMDOS Fsnext(): Error setting DTA\n");
 		Regs[REG_D0] = GEMDOS_EINTRN;
 		return true;
 	}

commit e031e53c2f593bfbf4fd301feafe0bda04b64e84

Correct GEMDOS HD Fsnext() invalid DTA ID error handling
Fixes: bcbdec8b63b22da68842d6f1630e3c1adc9cc816

That commit forgot return, so the error check was no-op.

Thanks to Thomas for noticing this!

src/gemdos.c
@@ -2816,6 +2816,7 @@ static bool GemDOS_SNext(void)
 		 */
 		Log_Printf(LOG_WARN, "GEMDOS Fsnext(): Invalid DTA\n");
 		Regs[REG_D0] = GEMDOS_ENMFIL;
+		return true;
 	}
 
 	temp = InternalDTAs[Index].found;