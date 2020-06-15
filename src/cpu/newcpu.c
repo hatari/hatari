bd159db83a7fc4fee21b87b4e8cbcac3d5cafd59
Nicolas Pomarede <npomarede@corp.free.fr>
2020-06-15 10:44:02 +0200

    Chipset/CPU multiplier in prefetch mode (not used in Hatari) (from WinUAE 4.4.0 beta7 2020/06/14)

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -2158,18 +2158,8 @@ static void update_68k_cycles (void)
 	}
 	if (cpucycleunit < 1)
 		cpucycleunit = 1;
-#if 0
-	if (!currprefs.cpu_cycle_exact && !currprefs.cpu_memory_cycle_exact && currprefs.cpu_compatible) {
-		if (cpucycleunit == CYCLE_UNIT / 2) {
-			cycles_mult = 0;
-		} else {
-			cycles_mult = cpucycleunit * (CYCLES_DIV / (CYCLE_UNIT / 2));
-		}
-	}
-#endif
-	if (currprefs.cpu_cycle_exact || currprefs.cpu_compatible)
-		write_log (_T("CPU cycleunit: %d (%.3f)\n"), cpucycleunit, (float)cpucycleunit / CYCLE_UNIT);
-write_log (_T("CPU cycleunit: %d (%.3f)\n"), cpucycleunit, (float)cpucycleunit / CYCLE_UNIT);
+
+	write_log (_T("CPU cycleunit: %d (%.3f)\n"), cpucycleunit, (float)cpucycleunit / CYCLE_UNIT);
 #ifndef WINUAE_FOR_HATARI
 	set_config_changed ();
 #endif
