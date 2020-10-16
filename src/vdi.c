hatari/hatari

commit 4edd0cd3a636bac3ebf481160016150f74133350

"info" command can show AES & VDI params on AES & VDI traps
Not just after the trap instruction & only when VDI mode or tracing
is enabled.

This way one doesn't need extra options & debugger files to do that on
AES & VDI opcode breakpoints.

src/vdi.c
@@ -23,6 +23,7 @@ const char VDI_fileid[] = "Hatari vdi.c : " __DATE__ " " __TIME__;
 #include "screen.h"
 #include "stMemory.h"
 #include "tos.h"
+#include "vars.h"
 #include "vdi.h"
 #include "video.h"
 
@@ -378,6 +379,28 @@ static void AES_OpcodeInfo(FILE *fp, Uint16 opcode)
 }
 
 /**
+ * Verify given VDI table pointer and store variables from
+ * it for later use. Return true for success
+ */
+static bool AES_StoreVars(Uint32 TablePtr)
+{
+	if (!STMemory_CheckAreaType(TablePtr, 24, ABFLAG_RAM))
+	{
+		Log_Printf(LOG_WARN, "AES param store failed due to invalid parameter block address 0x%x+%i\n", TablePtr, 24);
+		return false;
+	}
+	/* store values for debugger "info aes" command */
+	AESControl = STMemory_ReadLong(TablePtr);
+	AESGlobal  = STMemory_ReadLong(TablePtr+4);
+	AESIntin   = STMemory_ReadLong(TablePtr+8);
+	AESIntout  = STMemory_ReadLong(TablePtr+12);
+	AESAddrin  = STMemory_ReadLong(TablePtr+16);
+	AESAddrout = STMemory_ReadLong(TablePtr+20);
+	AESOpCode  = STMemory_ReadWord(AESControl);
+	return true;
+}
+
+/**
  * If opcodes argument is set, show AES opcode/function name table,
  * otherwise AES vectors information.
  */
@@ -394,17 +417,26 @@ void AES_Info(FILE *fp, Uint32 bShowOpcodes)
 		}
 		return;
 	}
-	if (!bVdiAesIntercept)
+	opcode = Vars_GetAesOpcode();
+	if (opcode != INVALID_OPCODE)
 	{
-		fputs("VDI/AES interception isn't enabled!\n", fp);
-		return;
+		if (!AES_StoreVars(Regs[REG_D1]))
+			return;
 	}
-	if (!AESControl)
+	else
 	{
-		fputs("No traced AES calls!\n", fp);
-		return;
+		if (!bVdiAesIntercept)
+		{
+			fputs("VDI/AES interception isn't enabled!\n", fp);
+			return;
+		}
+		if (!AESControl)
+		{
+			fputs("No traced AES calls!\n", fp);
+			return;
+		}
+		opcode = STMemory_ReadWord(AESControl);
 	}
-	opcode = STMemory_ReadWord(AESControl);
 	if (opcode != AESOpCode)
 	{
 		fputs("AES parameter block contents changed since last call!\n", fp);
@@ -733,6 +765,30 @@ static const char* VDI_Opcode2Name(Uint16 opcode, Uint16 subcode, Uint16 nintin,
 	return "???";
 }
 
+
+/**
+ * Verify given VDI table pointer and store variables from
+ * it for later use. Return true for success
+ */
+static bool VDI_StoreVars(Uint32 TablePtr)
+{
+	if (!STMemory_CheckAreaType(TablePtr, 20, ABFLAG_RAM))
+	{
+		Log_Printf(LOG_WARN, "VDI param store failed due to invalid parameter block address 0x%x+%i\n", TablePtr, 20);
+		return false;
+	}
+	/* store values for extended VDI resolution handling
+	 * and debugger "info vdi" command
+	 */
+	VDIControl = STMemory_ReadLong(TablePtr);
+	VDIIntin   = STMemory_ReadLong(TablePtr+4);
+	VDIPtsin   = STMemory_ReadLong(TablePtr+8);
+	VDIIntout  = STMemory_ReadLong(TablePtr+12);
+	VDIPtsout  = STMemory_ReadLong(TablePtr+16);
+	VDIOpCode  = STMemory_ReadWord(VDIControl);
+	return true;
+}
+
 /**
  * If opcodes argument is set, show VDI opcode/function name table,
  * otherwise VDI vectors information.
@@ -759,17 +815,26 @@ void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
 		}
 		return;
 	}
-	if (!bVdiAesIntercept)
+	opcode = Vars_GetVdiOpcode();
+	if (opcode != INVALID_OPCODE)
 	{
-		fputs("VDI/AES interception isn't enabled!\n", fp);
-		return;
+		if (!VDI_StoreVars(Regs[REG_D1]))
+			return;
 	}
-	if (!VDIControl)
+	else
 	{
-		fputs("No traced VDI calls!\n", fp);
-		return;
+		if (!bVdiAesIntercept)
+		{
+			fputs("VDI/AES interception isn't enabled!\n", fp);
+			return;
+		}
+		if (!VDIControl)
+		{
+			fputs("No traced VDI calls!\n", fp);
+			return;
+		}
+		opcode = STMemory_ReadWord(VDIControl);
 	}
-	opcode = STMemory_ReadWord(VDIControl);
 	if (opcode != VDIOpCode)
 	{
 		fputs("VDI parameter block contents changed since last call!\n", fp);
@@ -836,19 +901,8 @@ bool VDI_AES_Entry(void)
 	/* AES call? */
 	if (call == 0xC8)
 	{
-		if ( !STMemory_CheckAreaType ( TablePtr, 24, ABFLAG_RAM ) )
-		{
-			Log_Printf(LOG_WARN, "AES call failed due to invalid parameter block address 0x%x+%i\n", TablePtr, 24);
+		if (!AES_StoreVars(TablePtr))
 			return false;
-		}
-		/* store values for debugger "info aes" command */
-		AESControl = STMemory_ReadLong(TablePtr);
-		AESGlobal  = STMemory_ReadLong(TablePtr+4);
-		AESIntin   = STMemory_ReadLong(TablePtr+8);
-		AESIntout  = STMemory_ReadLong(TablePtr+12);
-		AESAddrin  = STMemory_ReadLong(TablePtr+16);
-		AESAddrout = STMemory_ReadLong(TablePtr+20);
-		AESOpCode  = STMemory_ReadWord(AESControl);
 		if (LOG_TRACE_LEVEL(TRACE_OS_AES))
 		{
 			AES_OpcodeInfo(TraceFile, AESOpCode);
@@ -864,20 +918,8 @@ bool VDI_AES_Entry(void)
 	/* VDI call? */
 	if (call == 0x73)
 	{
-		if ( !STMemory_CheckAreaType ( TablePtr, 20, ABFLAG_RAM ) )
-		{
-			Log_Printf(LOG_WARN, "VDI call failed due to invalid parameter block address 0x%x+%i\n", TablePtr, 20);
+		if (!VDI_StoreVars(TablePtr))
 			return false;
-		}
-		/* store values for extended VDI resolution handling
-		 * and debugger "info vdi" command
-		 */
-		VDIControl = STMemory_ReadLong(TablePtr);
-		VDIIntin   = STMemory_ReadLong(TablePtr+4);
-		VDIPtsin   = STMemory_ReadLong(TablePtr+8);
-		VDIIntout  = STMemory_ReadLong(TablePtr+12);
-		VDIPtsout  = STMemory_ReadLong(TablePtr+16);
-		VDIOpCode  = STMemory_ReadWord(VDIControl);
 #if ENABLE_TRACING
 		{
 		Uint16 subcode = STMemory_ReadWord(VDIControl+2*5);

src/debug/vars.c
@@ -51,7 +51,6 @@ static Uint32 GetFrameCycles(void)
 }
 
 /* helpers for TOS OS call opcode accessor functions */
-#define INVALID_OPCODE 0xFFFFu
 
 static inline Uint16 getLineOpcode(Uint8 line)
 {
@@ -113,7 +112,7 @@ static Uint32 GetXbiosOpcode(void)
 	}
 	return INVALID_OPCODE;
 }
-static Uint32 GetAesOpcode(void)
+Uint32 Vars_GetAesOpcode(void)
 {
 	if (isTrap(2)) {
 		Uint16 d0 = Regs[REG_D0];
@@ -126,7 +125,7 @@ static Uint32 GetAesOpcode(void)
 	}
 	return INVALID_OPCODE;
 }
-static Uint32 GetVdiOpcode(void)
+Uint32 Vars_GetVdiOpcode(void)
 {
 	if (isTrap(2)) {
 		Uint16 d0 = Regs[REG_D0];
@@ -155,7 +154,7 @@ static Uint32 GetNextPC(void)
 
 /* sorted by variable name so that this can be bisected */
 static const var_addr_t hatari_vars[] = {
-	{ "AesOpcode", (Uint32*)GetAesOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on AES trap" },
+	{ "AesOpcode", (Uint32*)Vars_GetAesOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on AES trap" },
 	{ "Basepage", (Uint32*)DebugInfo_GetBASEPAGE, VALUE_TYPE_FUNCTION32, 0, "invalid before Desktop is up" },
 	{ "BiosOpcode", (Uint32*)GetBiosOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on BIOS trap" },
 	{ "BSS", (Uint32*)DebugInfo_GetBSS, VALUE_TYPE_FUNCTION32, 0, "invalid before Desktop is up" },
@@ -178,7 +177,7 @@ static const var_addr_t hatari_vars[] = {
 	{ "TEXT", (Uint32*)DebugInfo_GetTEXT, VALUE_TYPE_FUNCTION32, 0, "invalid before Desktop is up" },
 	{ "TEXTEnd", (Uint32*)DebugInfo_GetTEXTEnd, VALUE_TYPE_FUNCTION32, 0, "invalid before Desktop is up" },
 	{ "VBL", (Uint32*)&nVBLs, VALUE_TYPE_VAR32, sizeof(nVBLs)*8, NULL },
-	{ "VdiOpcode", (Uint32*)GetVdiOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on VDI trap" },
+	{ "VdiOpcode", (Uint32*)Vars_GetVdiOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on VDI trap" },
 	{ "XbiosOpcode", (Uint32*)GetXbiosOpcode, VALUE_TYPE_FUNCTION32, 16, "$FFFF when not on XBIOS trap" }
 };
 

src/debug/vars.h
@@ -49,4 +49,11 @@ extern Uint32 Vars_GetValue(const var_addr_t *hvar);
 /* list variables & their names */
 extern int Vars_List(int nArgc, char *psArgv[]);
 
+/* opcode functions return for no opcode */
+#define INVALID_OPCODE 0xFFFFu
+
+/* Whether on AES/VDI trap */
+extern Uint32 Vars_GetAesOpcode(void);
+extern Uint32 Vars_GetVdiOpcode(void);
+
 #endif

commit ef4c01c10ce968c4a4e8f94c758339707a534a74

Fix VDI/AES info with ENABLE_TRACING disabled
With an earlier commit, AES/VDI_Info() functions could be used from a
AES/VDI opcode breakpoint, without AES/VDI being traced & attribute
vectors saved as part of that, because info functions call now (new)
AES/VDI_StoreVars() function to store the vectors when called directly
on the trap instruction.

However, that change broke compilation when ENABLE_TRACING was
undefined.  This fixes the compilation issue, by making most of the
functionality related to AES/VDI_Info() functions (and therefore also
":info <aes|vdi>" breakpoints) available also when ENABLE_TRACING is
undefined.

Fixes: 4edd0cd3a636bac

src/vdi.c
@@ -155,8 +155,6 @@ void VDI_SetResolution(int GEMColor, int WidthRequest, int HeightRequest)
 }
 
 
-#if ENABLE_TRACING
-
 /*-----------------------------------------------------------------------*/
 
 /* AES opcodes which have string args */
@@ -312,6 +310,7 @@ static const char* AES_Opcode2Name(Uint16 opcode)
 		return "???";
 }
 
+#if ENABLE_TRACING
 /**
  * Output AES call info, including some of args
  */
@@ -377,6 +376,7 @@ static void AES_OpcodeInfo(FILE *fp, Uint16 opcode)
 		fputs("???\n", fp);
 	fflush(fp);
 }
+#endif
 
 /**
  * Verify given VDI table pointer and store variables from
@@ -424,6 +424,9 @@ void AES_Info(FILE *fp, Uint32 bShowOpcodes)
 			return;
 	}
 	else
+#if !ENABLE_TRACING
+		fputs("Hatari build with ENABLE_TRACING required to retain AES/VDI call info!\n", fp);
+#else
 	{
 		if (!bVdiAesIntercept)
 		{
@@ -458,6 +461,7 @@ void AES_Info(FILE *fp, Uint32 bShowOpcodes)
 		AESAddrin, STMemory_ReadWord(AESControl+2*3));
 	fprintf(fp, "- Addrout: %#8x, %d longs\n",
 		AESAddrout, STMemory_ReadWord(AESControl+2*4));
+#endif
 }
 
 
@@ -795,9 +799,8 @@ static bool VDI_StoreVars(Uint32 TablePtr)
  */
 void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
 {
-	Uint16 opcode, subcode, nintin;
+	Uint16 opcode;
 	const char *extra_info;
-	const char *name;
 
 	if (bShowOpcodes)
 	{
@@ -822,6 +825,9 @@ void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
 			return;
 	}
 	else
+#if !ENABLE_TRACING
+		fputs("Hatari build with ENABLE_TRACING required to retain AES/VDI call info!\n", fp);
+#else
 	{
 		if (!bVdiAesIntercept)
 		{
@@ -840,11 +846,10 @@ void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
 		fputs("VDI parameter block contents changed since last call!\n", fp);
 		return;
 	}
-
 	fputs("Latest VDI Parameter block:\n", fp);
-	subcode = STMemory_ReadWord(VDIControl+2*5);
-	nintin = STMemory_ReadWord(VDIControl+2*3);
-	name = VDI_Opcode2Name(opcode, subcode, nintin, &extra_info);
+	Uint16 subcode = STMemory_ReadWord(VDIControl+2*5);
+	Uint16 nintin = STMemory_ReadWord(VDIControl+2*3);
+	const char *name = VDI_Opcode2Name(opcode, subcode, nintin, &extra_info);
 	fprintf(fp, "- Opcode/Subcode: %hd/%hd (%s%s%s)\n",
 		opcode, subcode, name, extra_info ? ", " : "", extra_info ? extra_info : "");
 	fprintf(fp, "- Device handle: %d\n",
@@ -858,18 +863,8 @@ void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
 		VDIIntin, STMemory_ReadWord(VDIControl+2*3));
 	fprintf(fp, "- Intout:  %#8x, %d words\n",
 		VDIIntout, STMemory_ReadWord(VDIControl+2*4));
+#endif
 }
-
-#else /* !ENABLE_TRACING */
-void AES_Info(FILE *fp, Uint32 bShowOpcodes)
-{
-	fputs("Hatari isn't configured with ENABLE_TRACING\n", fp);
-}
-void VDI_Info(FILE *fp, Uint32 bShowOpcodes)
-{
-	fputs("Hatari isn't configured with ENABLE_TRACING\n", fp);
-}
-#endif /* !ENABLE_TRACING */
 
 
 /*-----------------------------------------------------------------------*/
@@ -895,9 +890,9 @@ static inline bool VDI_isWorkstationOpen(Uint16 opcode)
 bool VDI_AES_Entry(void)
 {
 	Uint16 call = Regs[REG_D0];
+#if ENABLE_TRACING
 	Uint32 TablePtr = Regs[REG_D1];
 
-#if ENABLE_TRACING
 	/* AES call? */
 	if (call == 0xC8)
 	{
@@ -913,23 +908,23 @@ bool VDI_AES_Entry(void)
 		 */
 		return false;
 	}
-#endif
-
 	/* VDI call? */
 	if (call == 0x73)
 	{
+		Uint16 subcode, nintin;
+		const char *extra_info, *name;
+
 		if (!VDI_StoreVars(TablePtr))
 			return false;
-#if ENABLE_TRACING
-		{
-		Uint16 subcode = STMemory_ReadWord(VDIControl+2*5);
-		Uint16 nintin = STMemory_ReadWord(VDIControl+2*3);
-		const char *extra_info;
-		const char *name = VDI_Opcode2Name(VDIOpCode, subcode, nintin, &extra_info);
+		subcode = STMemory_ReadWord(VDIControl+2*5);
+		nintin = STMemory_ReadWord(VDIControl+2*3);
+		name = VDI_Opcode2Name(VDIOpCode, subcode, nintin, &extra_info);
 		LOG_TRACE(TRACE_OS_VDI, "VDI call %3hd/%3hd (%s%s%s)\n",
 			  VDIOpCode, subcode, name, extra_info ? ", " : "", extra_info ? extra_info : "");
-		}
+	}
 #endif
+	if (call == 0x73)
+	{
 		/* Only workstation open needs to be handled at trap return */
 		return bUseVDIRes && VDI_isWorkstationOpen(VDIOpCode);
 	}