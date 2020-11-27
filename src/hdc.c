hatari/hatari

commit 2d8ada4bcfc5ec9fa7f9b419e235697ce90aa5ad

Split SCSI init/uninit handling from ACSI init/uninit
This fixes internal partition count being wrong after either ACSI or
SCSI HD settings have been changed, due to SCSI drives not being
deducted on SCSI UnInit() (drive count would be corrected on next
emulation reset though, as that resets also drive count).

ACSI/SCSI/IDE comments are also fixed to use plural form as nowadays
all of them can have multiple drives/images.

src/hdc.c
@@ -853,7 +853,7 @@ int HDC_InitDevice(SCSI_DEV *dev, char *filename, unsigned long blockSize)
 }
 
 /**
- * Open the disk image file, set partitions.
+ * Open the disk image files, set partitions.
  */
 bool HDC_Init(void)
 {
@@ -876,20 +876,12 @@ bool HDC_Init(void)
 		if (!ConfigureParams.Acsi[i].bUseDevice)
 			continue;
 		if (HDC_InitDevice(&AcsiBus.devs[i], ConfigureParams.Acsi[i].sDeviceFile, ConfigureParams.Acsi[i].nBlockSize) == 0)
-		{
-			bAcsiEmuOn = true;
 			nAcsiPartitions += HDC_PartitionCount(AcsiBus.devs[i].image_file, TRACE_SCSI_CMD, NULL);
-		}
 	}
-
-	/* add SCSI partition count to ACSI ones
-	 * to support GEMDOS HD emu partition skipping
-	 */
-	nAcsiPartitions += Ncr5380_Init();
-
 	/* set total number of partitions */
 	nNumDrives += nAcsiPartitions;
-
+	if (nAcsiPartitions)
+		bAcsiEmuOn = true;
 	return bAcsiEmuOn;
 }
 
@@ -915,10 +907,7 @@ void HDC_UnInit(void)
 	free(AcsiBus.buffer);
 	AcsiBus.buffer = NULL;
 
-	Ncr5380_UnInit();
-
-	if (bAcsiEmuOn)
-		nNumDrives -= nAcsiPartitions;
+	nNumDrives -= nAcsiPartitions;
 	nAcsiPartitions = 0;
 	bAcsiEmuOn = false;
 }

src/ncr5380.c
@@ -28,8 +28,12 @@ const char NCR5380_fileid[] = "Hatari ncr5380.c";
 #include "ncr5380.h"
 #include "stMemory.h"
 #include "newcpu.h"
+#include "tos.h"
 
 #define WITH_NCR5380 1
+
+int nScsiPartitions;
+bool bScsiEmuOn;
 
 static SCSI_CTRLR ScsiBus;
 
@@ -1008,12 +1012,16 @@ void ncr5380_bput(struct soft_scsi *scsi, int reg, uae_u8 v)
 /* ***** Hatari glue code below ***** */
 
 /**
- * return number of identified partitions on existing SCSI images
+ * Open the disk image file, set partitions count.
+ * Return true if there are any.
  */
-int Ncr5380_Init(void)
+bool Ncr5380_Init(void)
 {
 #if WITH_NCR5380
-	int i, partitions = 0;
+	int i;
+
+	nScsiPartitions = 0;
+	bScsiEmuOn = false;
 
 	memset(&ScsiBus, 0, sizeof(ScsiBus));
 	ScsiBus.typestr = "SCSI";
@@ -1029,14 +1037,19 @@ int Ncr5380_Init(void)
 		if (!ConfigureParams.Scsi[i].bUseDevice)
 			continue;
 		if (HDC_InitDevice(&ScsiBus.devs[i], ConfigureParams.Scsi[i].sDeviceFile, ConfigureParams.Scsi[i].nBlockSize) == 0)
-			partitions += HDC_PartitionCount(ScsiBus.devs[i].image_file, TRACE_SCSI_CMD, NULL);
+			nScsiPartitions += HDC_PartitionCount(ScsiBus.devs[i].image_file, TRACE_SCSI_CMD, NULL);
 	}
-	return partitions;
-#else
-	return 0;
+	nNumDrives += nScsiPartitions;
+	if (nScsiPartitions)
+		bScsiEmuOn = true;
 #endif
+	return bScsiEmuOn;
 }
 
+/**
+ * Ncr5380_UnInit - close image files and free resources
+ *
+ */
 void Ncr5380_UnInit(void)
 {
 #if WITH_NCR5380
@@ -1053,6 +1066,10 @@ void Ncr5380_UnInit(void)
 	}
 	free(ScsiBus.buffer);
 	ScsiBus.buffer = NULL;
+
+	nNumDrives -= nScsiPartitions;
+	nScsiPartitions = 0;
+	bScsiEmuOn = false;
 #endif
 }
 

src/change.c
@@ -28,6 +28,7 @@ const char Change_fileid[] = "Hatari change.c : " __DATE__ " " __TIME__;
 #include "keymap.h"
 #include "m68000.h"
 #include "midi.h"
+#include "ncr5380.h"
 #include "options.h"
 #include "printer.h"
 #include "reset.h"
@@ -182,6 +183,7 @@ void Change_CopyChangedParamsToConfiguration(CNF_PARAMS *current, CNF_PARAMS *ch
 {
 	bool NeedReset;
 	bool bReInitGemdosDrive = false;
+	bool bReInitScsiEmu = false;
 	bool bReInitHdcEmu = false;
 	bool bReInitIDEEmu = false;
 	bool bReInitIoMem = false;
@@ -294,7 +296,7 @@ void Change_CopyChangedParamsToConfiguration(CNF_PARAMS *current, CNF_PARAMS *ch
 		bReInitGemdosDrive = true;
 	}
 
-	/* Did change ACSI image? */
+	/* Did change ACSI images? */
 	for (i = 0; i < MAX_ACSI_DEVS; i++)
 	{
 		if (changed->Acsi[i].bUseDevice != current->Acsi[i].bUseDevice
@@ -305,7 +307,10 @@ void Change_CopyChangedParamsToConfiguration(CNF_PARAMS *current, CNF_PARAMS *ch
 			bReInitHdcEmu = true;
 		}
 	}
-	/* Did change SCSI image? */
+	if (bReInitHdcEmu)
+		HDC_UnInit();
+
+	/* Did change SCSI images? */
 	for (i = 0; i < MAX_SCSI_DEVS; i++)
 	{
 		if (changed->Scsi[i].bUseDevice != current->Scsi[i].bUseDevice
@@ -313,13 +318,13 @@ void Change_CopyChangedParamsToConfiguration(CNF_PARAMS *current, CNF_PARAMS *ch
 		        && changed->Scsi[i].bUseDevice))
 		{
 			Dprintf("- SCSI image %i>\n", i);
-			bReInitHdcEmu = true;
+			bReInitScsiEmu = true;
 		}
 	}
-	if (bReInitHdcEmu)
-		HDC_UnInit();
+	if (bReInitScsiEmu)
+		Ncr5380_UnInit();
 
-	/* Did change IDE HD image or its settings? */
+	/* Did change IDE HD images or their settings? */
 	for (i = 0; i < MAX_IDE_DEVS; i++)
 	{
 		if (changed->Ide[i].bUseDevice != current->Ide[i].bUseDevice
@@ -399,14 +404,19 @@ void Change_CopyChangedParamsToConfiguration(CNF_PARAMS *current, CNF_PARAMS *ch
 		Keymap_LoadRemapFile(ConfigureParams.Keyboard.szMappingFileName);
 	}
 
-	/* Mount a new ACSI/SCSI HD images: */
+	/* Mount new ACSI HD images: */
 	if (bReInitHdcEmu)
 	{
-		Dprintf("- HD<\n");
+		Dprintf("- ACSI<\n");
 		HDC_Init();
 	}
-
-	/* Mount a new IDE HD images: */
+	/* Mount new SCSI HD images: */
+	if (bReInitScsiEmu)
+	{
+		Dprintf("- SCSI<\n");
+		Ncr5380_Init();
+	}
+	/* Mount new IDE HD images: */
 	if (bReInitIDEEmu && Ide_IsAvailable())
 	{
 		Dprintf("- IDE<\n");

src/gemdos.c
@@ -49,6 +49,7 @@ const char Gemdos_fileid[] = "Hatari gemdos.c : " __DATE__ " " __TIME__;
 #include "ide.h"
 #include "inffile.h"
 #include "hdc.h"
+#include "ncr5380.h"
 #include "gemdos.h"
 #include "gemdos_defines.h"
 #include "log.h"
@@ -699,11 +700,14 @@ void GemDOS_InitDrives(void)
 		}
 	}
 
-	ImagePartitions = nAcsiPartitions + nIDEPartitions;
+	ImagePartitions = nAcsiPartitions + nScsiPartitions + nIDEPartitions;
 	if (ConfigureParams.HardDisk.nGemdosDrive == DRIVE_SKIP)
 		SkipPartitions = ImagePartitions;
 	else
 		SkipPartitions = ConfigureParams.HardDisk.nGemdosDrive;
+
+	Log_Printf(LOG_DEBUG, "ACSI: %d, SCSI: %d, IDE: %d - GEMDOS skipping %d partitions.\n",
+		   nAcsiPartitions, nScsiPartitions, nIDEPartitions, SkipPartitions);
 
 	/* Now initialize all available drives */
 	for(i = 0; i < nMaxDrives; i++)
@@ -752,7 +756,7 @@ void GemDOS_InitDrives(void)
 			 *  table(s) match each other).
 			 */
 			if (i < ImagePartitions)
-				Log_Printf(LOG_WARN, "GEMDOS HD drive %c: (may) override ACSI/IDE image partitions!\n", 'A'+DriveNumber);
+				Log_Printf(LOG_WARN, "GEMDOS HD drive %c: (may) override ACSI/SCSI/IDE image partitions!\n", 'A'+DriveNumber);
 		}
 		else
 		{

src/includes/ncr5380.h
@@ -8,7 +8,10 @@
 #ifndef NCR5380_H
 #define NCR5380_H
 
-int Ncr5380_Init(void);
+extern int nScsiPartitions;
+extern bool bScsiEmuOn;
+
+bool Ncr5380_Init(void);
 void Ncr5380_UnInit(void);
 void Ncr5380_Reset(void);
 void Ncr5380_WriteByte(int addr, Uint8 byte);

src/main.c
@@ -37,6 +37,7 @@ const char Main_fileid[] = "Hatari main.c : " __DATE__ " " __TIME__;
 #include "m68000.h"
 #include "memorySnapShot.h"
 #include "midi.h"
+#include "ncr5380.h"
 #include "nvram.h"
 #include "paths.h"
 #include "printer.h"
@@ -790,11 +791,12 @@ static void Main_Init(void)
 
 	/* Init HD emulation */
 	HDC_Init();
+	Ncr5380_Init();
 	Ide_Init();
 	GemDOS_Init();
 	if (ConfigureParams.HardDisk.bUseHardDiskDirectories)
 	{
-		/* uses variables set by HDC_Init()! */
+		/* uses variables set by HDC_Init/Ncr5380_Init/Ide_Init */
 		GemDOS_InitDrives();
 	}
 
@@ -830,6 +832,7 @@ static void Main_UnInit(void)
 	Screen_ReturnFromFullScreen();
 	Floppy_UnInit();
 	HDC_UnInit();
+	Ncr5380_UnInit();
 	Midi_UnInit();
 	SCC_UnInit();
 	RS232_UnInit();

commit 1d0ab403d4d47f125c66aa63305ecd817d73f6b4

Fix: enable ACSI & SCSI even when drives don't have partitions yet
This is needed to be able to format zeroed disk images under Hatari.

Reported-By: Christian Zietz

Fixes: 2d8ada4bcfc5ec9fa7f9b419e235697ce90aa5ad

src/hdc.c
@@ -876,12 +876,13 @@ bool HDC_Init(void)
 		if (!ConfigureParams.Acsi[i].bUseDevice)
 			continue;
 		if (HDC_InitDevice(&AcsiBus.devs[i], ConfigureParams.Acsi[i].sDeviceFile, ConfigureParams.Acsi[i].nBlockSize) == 0)
+		{
 			nAcsiPartitions += HDC_PartitionCount(AcsiBus.devs[i].image_file, TRACE_SCSI_CMD, NULL);
+			bAcsiEmuOn = true;
+		}
 	}
 	/* set total number of partitions */
 	nNumDrives += nAcsiPartitions;
-	if (nAcsiPartitions)
-		bAcsiEmuOn = true;
 	return bAcsiEmuOn;
 }
 

src/ncr5380.c
@@ -1037,11 +1037,12 @@ bool Ncr5380_Init(void)
 		if (!ConfigureParams.Scsi[i].bUseDevice)
 			continue;
 		if (HDC_InitDevice(&ScsiBus.devs[i], ConfigureParams.Scsi[i].sDeviceFile, ConfigureParams.Scsi[i].nBlockSize) == 0)
+		{
 			nScsiPartitions += HDC_PartitionCount(ScsiBus.devs[i].image_file, TRACE_SCSI_CMD, NULL);
+			bScsiEmuOn = true;
+		}
 	}
 	nNumDrives += nScsiPartitions;
-	if (nScsiPartitions)
-		bScsiEmuOn = true;
 #endif
 	return bScsiEmuOn;
 }