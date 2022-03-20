hatari/hatari

commit dbc4ae4abf585b6161187abd64aa4d808102361e

Add support for the 'REPORT LUNS' command (patch by Uwe Seimet)

src/hdc.c
@@ -423,6 +423,31 @@ static void HDC_Cmd_FormatDrive(SCSI_CTRLR *ctr)
 
 
 /**
+ * Report LUNs.
+ */
+static void HDC_Cmd_ReportLuns(SCSI_CTRLR *ctr)
+{
+        SCSI_DEV *dev = &ctr->devs[ctr->target];
+	Uint8 *buf;
+
+        LOG_TRACE(TRACE_SCSI_CMD, "HDC: REPORT LUNS (%s).\n", HDC_CmdInfoStr(ctr));
+
+        buf = HDC_PrepRespBuf(ctr, 16);
+
+	// LUN list length, 8 bytes per LUN
+	buf[0] = 0;
+	buf[1] = 0;
+	buf[2] = 0;
+	buf[3] = 8;
+	memset(&buf[4], 0, 12);
+
+        ctr->status = HD_STATUS_OK;
+        dev->nLastError = HD_REQSENS_OK;
+        dev->bSetLastBlockAddr = false;
+}
+
+
+/**
  * Read capacity of our disk.
  */
 static void HDC_Cmd_ReadCapacity(SCSI_CTRLR *ctr)
@@ -606,6 +631,10 @@ static void HDC_EmulateCommandPacket(SCSI_CTRLR *ctr)
 
 	 case HD_FORMAT_DRIVE:
 		HDC_Cmd_FormatDrive(ctr);
+		break;
+
+	case HD_REPORT_LUNS:
+		HDC_Cmd_ReportLuns(ctr);
 		break;
 
 	 /* as of yet unsupported commands */
@@ -960,7 +989,8 @@ bool HDC_WriteCommandPacket(SCSI_CTRLR *ctr, Uint8 b)
 
 	/* have we received a complete 6-byte class 0 or 10-byte class 1 packet yet? */
 	if ((ctr->opcode < 0x20 && ctr->byteCount == 6) ||
-	    (ctr->opcode >= 0x20 && ctr->opcode < 0x60 && ctr->byteCount == 10))
+	    (ctr->opcode >= 0x20 && ctr->opcode < 0x60 && ctr->byteCount == 10) ||
+	    (ctr->opcode == 0xa0 && ctr->byteCount == 12))
 	{
 		/* We currently only support LUN 0, however INQUIRY must
 		 * always be handled, see SCSI standard */

src/includes/hdc.h
@@ -33,6 +33,7 @@
 #define HD_REQ_SENSE       0x03               /* Request sense */
 #define HD_SHIP            0x1B               /* Ship drive */
 #define HD_READ_CAPACITY1  0x25               /* Read capacity (class 1) */
+#define HD_REPORT_LUNS     0xa0               /* Report Luns */
 
 /* Status codes */
 #define HD_STATUS_OK       0x00

commit a0917feca548e0cc8cbeba9fd02b6550406bf3d0

Fix HD_REPORT_LUNS handling
Replaces magic constants (opcodes) by existing definitions.
Add missing check for HD_REPORT_LUNS in HDC_WriteCommandPacket().

[Modified by Thomas : Cut the patch out of a bigger patch by Uwe,
 and adjusted the commit message]

Fixes: dbc4ae4abf58 ("Add support for the 'REPORT LUNS' command")

src/hdc.c
@@ -106,7 +106,7 @@ static int HDC_GetCommandByteCount(SCSI_CTRLR *ctr)
 		ctr->opcode == 0x91 || ctr->opcode == 0x9e || ctr->opcode == 0x9f) {
 		return 16;
 	}
-	else if (ctr->opcode == 0xa0) {
+	else if (ctr->opcode == HD_REPORT_LUNS) {
 		return 12;
 	}
 	else if (ctr->opcode == 0x05 || (ctr->opcode >= 0x20 && ctr->opcode <= 0x7d)) {
@@ -1017,10 +1017,10 @@ bool HDC_WriteCommandPacket(SCSI_CTRLR *ctr, Uint8 b)
 		ctr->command[ctr->byteCount] = b;
 	++ctr->byteCount;
 
-	/* have we received a complete 6-byte class 0 or 10-byte class 1 packet yet? */
+	/* have we received a complete command packet yet? */
 	if ((ctr->opcode < 0x20 && ctr->byteCount == 6) ||
 	    (ctr->opcode >= 0x20 && ctr->opcode < 0x60 && ctr->byteCount == 10) ||
-	    (ctr->opcode == 0xa0 && ctr->byteCount == 12))
+	    (ctr->opcode == HD_REPORT_LUNS && ctr->byteCount == 12))
 	{
 		/* We currently only support LUN 0, however INQUIRY must
 		 * always be handled, see SCSI standard */
@@ -1046,7 +1046,7 @@ bool HDC_WriteCommandPacket(SCSI_CTRLR *ctr, Uint8 b)
 			}
 		}
 	}
-	else if (ctr->opcode >= 0x60)
+	else if (ctr->opcode >= 0x60 && ctr->opcode != HD_REPORT_LUNS)
 	{
 		/* Commands >= 0x60 are not supported right now */
 		ctr->status = HD_STATUS_ERROR;