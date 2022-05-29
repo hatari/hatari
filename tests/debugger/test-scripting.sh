83de5052e11d421a4228c2a5ec018a2a6f83fd7b
Eero Tamminen <oak@helsinkinet.fi>
2022-05-29 20:51:18 +0300

    Fix shellcheck warnings for debugger test script

--- tests/debugger/test-scripting.sh
+++ tests/debugger/test-scripting.sh
@@ -8,25 +8,25 @@ if [ $# -ne 1 ]; then
 fi
 
 etos=$1
-if [ \! -f $etos ]; then
+if [ ! -f "$etos" ]; then
 	echo "ERROR: given EmuTOS image file '$etos' doesn't exist!"
 	exit 1
 fi
 
 hpath=../../build/src
-if [ \! -d $hpath ]; then
-	echo "ERROR: Hatari source directory '$path' missing!"
+if [ ! -d $hpath ]; then
+	echo "ERROR: Hatari source directory '$hpath' missing!"
 	exit 1
 fi
 
 hatari=$hpath/hatari
-if [ \! -x $hatari ]; then
+if [ ! -x $hatari ]; then
 	echo "ERROR: Hatari binary '$hatari' missing!"
 	exit 1
 fi
 
 console=../../tools/hconsole/hconsole.py
-if [ \! -x $console ]; then
+if [ ! -x $console ]; then
 	echo "ERROR: Hatari console script '$console' missing!"
 	exit 1
 fi
@@ -38,7 +38,7 @@ export ASAN_OPTIONS="detect_stack_use_after_return=1,abort_on_error=1,debug=1"
 echo "TESTING: debugger input file"
 echo "============================"
 cmd="$hatari --sound off --machine falcon --tos $etos --dsp emu --parse data/debugger.ini"
-echo $cmd
+echo "$cmd"
 $cmd
 
 echo
