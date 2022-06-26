hatari/hatari

commit 1c415e4c4b66b6038612cd77a46c5665b671a3a7

MIDI error handling and logging improvements
* Reads can get also other errors than EOF
* Skip read error messsages on EOF. Successful reads are already
  traced so it's enough for EOF to be just lack of output
  (instead of constant stream of read error messages)
* Tell whether file access is buffered or not

src/midi.c
@@ -313,7 +313,7 @@ void Midi_InterruptHandler_Update(void)
 
 	/* Read the bytes in, if we have any */
 	nInChar = Midi_Host_ReadByte();
-	if (nInChar != EOF)
+	if (nInChar >= 0)
 	{
 		LOG_TRACE(TRACE_MIDI, "MIDI: Read character -> $%x\n", nInChar);
 		/* Copy into our internal queue */
@@ -326,7 +326,8 @@ void Midi_InterruptHandler_Update(void)
 #ifndef HAVE_PORTMIDI
 	else if (pMidiFhIn)
 	{
-		LOG_TRACE(TRACE_MIDI, "MIDI: read error (doesn't stop MIDI)\n");
+		if (nInChar != EOF)
+			LOG_TRACE(TRACE_MIDI, "MIDI: read error %d (does not stop MIDI)\n", nInChar);
 		clearerr(pMidiFhIn);
 	}
 #endif
@@ -347,15 +348,17 @@ void Midi_InterruptHandler_Update(void)
 static bool Midi_Host_Open(void)
 {
 #ifndef HAVE_PORTMIDI
+	int ok;
 	if (ConfigureParams.Midi.sMidiOutFileName[0])
 	{
 		/* Open MIDI output file */
 		pMidiFhOut = File_Open(ConfigureParams.Midi.sMidiOutFileName, "wb");
 		if (!pMidiFhOut)
 			return false;
-		setvbuf(pMidiFhOut, NULL, _IONBF, 0);    /* No output buffering! */
-		LOG_TRACE(TRACE_MIDI, "MIDI: Opened file '%s' for output\n",
-			 ConfigureParams.Midi.sMidiOutFileName);
+		ok = setvbuf(pMidiFhOut, NULL, _IONBF, 0);    /* No output buffering! */
+		LOG_TRACE(TRACE_MIDI, "MIDI: Opened file '%s' (%s) for output\n",
+			 ConfigureParams.Midi.sMidiOutFileName,
+			  ok == 0 ? "unbuffered" : "buffered");
 	}
 	if (ConfigureParams.Midi.sMidiInFileName[0])
 	{
@@ -363,9 +366,10 @@ static bool Midi_Host_Open(void)
 		pMidiFhIn = File_Open(ConfigureParams.Midi.sMidiInFileName, "rb");
 		if (!pMidiFhIn)
 			return false;
-		setvbuf(pMidiFhIn, NULL, _IONBF, 0);    /* No input buffering! */
-		LOG_TRACE(TRACE_MIDI, "MIDI: Opened file '%s' for input\n",
-			 ConfigureParams.Midi.sMidiInFileName);
+		ok = setvbuf(pMidiFhIn, NULL, _IONBF, 0);    /* No input buffering! */
+		LOG_TRACE(TRACE_MIDI, "MIDI: Opened file '%s' (%s) for input\n",
+			 ConfigureParams.Midi.sMidiInFileName,
+			  ok == 0 ? "unbuffered" : "buffered");
 	}
 #else
 	int i, ports;

commit 8783363a7f9fe47cff9c4a2e9fd13a4f55d7e201

Fix MIDI read error logging
It turned out I had misunderstood badly worded Glibc fgetc() manual
page.  Thomas pointed out much more precise POSIX text, which
explicitly states fgetc() returning only EOF on errors.

=> Change error logging to use errno, and call clearerr()
   only when EOF is due to operations on that stream

Fixes: 1c415e4c4b

src/midi.c
@@ -23,6 +23,7 @@
 const char Midi_fileid[] = "Hatari midi.c";
 
 #include <SDL_types.h>
+#include <errno.h>
 
 #include "main.h"
 #include "configuration.h"
@@ -313,9 +314,9 @@ void Midi_InterruptHandler_Update(void)
 
 	/* Read the bytes in, if we have any */
 	nInChar = Midi_Host_ReadByte();
-	if (nInChar >= 0)
+	if (nInChar != EOF)
 	{
-		LOG_TRACE(TRACE_MIDI, "MIDI: Read character -> $%x\n", nInChar);
+		LOG_TRACE(TRACE_MIDI, "MIDI: read byte -> $%x\n", nInChar);
 		/* Copy into our internal queue */
 		nRxDataByte = nInChar;
 		MidiStatusRegister |= ACIA_SR_RX_FULL;
@@ -323,14 +324,6 @@ void Midi_InterruptHandler_Update(void)
 		/* Do we need to generate a receive interrupt? */
 		MIDI_UpdateIRQ ();
 	}
-#ifndef HAVE_PORTMIDI
-	else if (pMidiFhIn)
-	{
-		if (nInChar != EOF)
-			LOG_TRACE(TRACE_MIDI, "MIDI: read error %d (does not stop MIDI)\n", nInChar);
-		clearerr(pMidiFhIn);
-	}
-#endif
 
 	/* Set timer */
 	CycInt_AddRelativeInterrupt ( MIDI_TRANSFER_BYTE_CYCLE , INT_CPU_CYCLE , INTERRUPT_MIDI );
@@ -583,8 +576,22 @@ static int Midi_Host_ReadByte(void)
 {
 #ifndef HAVE_PORTMIDI
 	if (pMidiFhIn && File_InputAvailable(pMidiFhIn))
-		return fgetc(pMidiFhIn);
-	else return EOF;
+	{
+		/* man 3p: fgetc() returns EOF on all errors, but
+		 * sets errno only for other than end-of-file issues
+		 */
+		errno = 0;
+		int ret = fgetc(pMidiFhIn);
+		if (ret != EOF)
+			return ret;
+		if (errno && errno != EAGAIN)
+		{
+			LOG_TRACE(TRACE_MIDI, "MIDI: read error: %s\n", strerror(errno));
+		}
+		/* affects only EOF indicator */
+		clearerr(pMidiFhIn);
+	}
+	return EOF;
 #else
 	// TODO: should these be reset with Midi_Init()?
 	static Uint8 msg[4];