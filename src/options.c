hatari/hatari

commit ae456052481afe1a1d5e1c0249efb8fbfbf9d3a8

Add --rtc-year option / config option
There can be old Atari programs that use dates, but do not handle
e.g. Y2K gracefully.  This workaround can be used with them.

Notes:

* Suitable year offset is calculated at start, instead of on every
  query, to handle gracefully new year transition (avoid clock going
  backwards on year change).

* This means that RTC year cannot be overridden afterwards, from
  Hatari debugger, or through its remote API.

src/options.c
@@ -171,8 +171,9 @@ enum {
 
 	OPT_MACHINE,		/* system options */
 	OPT_BLITTER,
-	OPT_VME,
 	OPT_DSP,
+	OPT_VME,
+	OPT_RTC_YEAR,
 	OPT_TIMERD,
 	OPT_FASTBOOT,
 
@@ -447,6 +448,8 @@ static const opt_t HatariOptions[] = {
 	  "<x>", "DSP emulation (x = none/dummy/emu, Falcon only)" },
 	{ OPT_VME,	NULL, "--vme",
 	  "<x>", "VME mode (x = none/dummy, MegaSTE/TT only)" },
+	{ OPT_RTC_YEAR,   NULL, "--rtc-year",
+	  "<x>", "Set initial year for RTC (0, 1980 <= x < 2080)" },
 	{ OPT_TIMERD,    NULL, "--timer-d",
 	  "<bool>", "Patch Timer-D (about doubles ST emulation speed)" },
 	{ OPT_FASTBOOT, NULL, "--fast-boot",
@@ -1050,7 +1053,7 @@ static bool Opt_HandleArgument(const char *path)
 bool Opt_ParseParameters(int argc, const char * const argv[])
 {
 	int ncpu, skips, planes, cpuclock, threshold, memsize;
-	int dev, port, freq, temp, drive;
+	int dev, port, freq, temp, drive, year;
 	const char *errstr, *str;
 	int i, ok = true;
 	float zoom;
@@ -1929,6 +1932,7 @@ bool Opt_ParseParameters(int argc, const char * const argv[])
 		case OPT_TIMERD:
 			ok = Opt_Bool(argv[++i], OPT_TIMERD, &ConfigureParams.System.bPatchTimerD);
 			break;
+
 		case OPT_FASTBOOT:
 			ok = Opt_Bool(argv[++i], OPT_FASTBOOT, &ConfigureParams.System.bFastBoot);
 			break;
@@ -1973,6 +1977,15 @@ bool Opt_ParseParameters(int argc, const char * const argv[])
 				return Opt_ShowError(OPT_VME, argv[i], "Unknown VME type");
 			}
 			bLoadAutoSave = false; /* TODO: needed? */
+			break;
+
+		case OPT_RTC_YEAR:
+			year = atoi(argv[++i]);
+			if(year && (year < 1980 || year >= 2080))
+			{
+				return Opt_ShowError(OPT_CPUCLOCK, argv[i], "Invalid RTC year");
+			}
+			ConfigureParams.System.nRtcYear = year;
 			break;
 
 			/* sound options */

doc/hatari.1
@@ -497,6 +497,12 @@ With "none", register access causes errors (=no VME HW), which is
 needed for Linux to boot with TT emulation until there's full SCU
 interrupt support.  No VME tracing support.
 .TP
+.B \-\-rtc\-year <x>
+With the default value 0, RTC date and time are taken from the host.
+If application does not handle current dates, this can be used to
+change RTC year to a more compatible one.  See also "--gemdos-time"
+option.
+.TP
 .B \-\-timer\-d <bool>
 Patch redundantly high Timer-D frequency set by TOS.  This can
 increase Hatari speed significantly (especially for ST/e emulation) as

doc/manual.html
@@ -1008,6 +1008,12 @@ With "none", register access causes errors (=no VME HW), which is
 needed for Linux to boot with TT emulation until there's full SCU
 interrupt support.  No VME tracing support.
 </p>
+<p class="parameter">--rtc-year &lt;x&gt;</p>
+<p class="paramdesc">With the default value 0, RTC date and time are
+taken from the host. If application does not handle current dates,
+this can be used to change RTC year to a more compatible one.  See
+also "--gemdos-time" option.
+</p>
 <p class="parameter">--dsp &lt;x&gt;</p>
 <p class="paramdesc">Falcon DSP emulation (x = none, dummy
 or emu, Falcon only)</p>

src/configuration.c
@@ -479,6 +479,7 @@ static const struct Config_Tag configs_System[] =
 	{ "bBlitter", Bool_Tag, &ConfigureParams.System.bBlitter },
 	{ "nDSPType", Int_Tag, &ConfigureParams.System.nDSPType },
 	{ "nVMEType", Int_Tag, &ConfigureParams.System.nVMEType },
+	{ "nRtcYear", Int_Tag, &ConfigureParams.System.nRtcYear },
 	{ "bPatchTimerD", Bool_Tag, &ConfigureParams.System.bPatchTimerD },
 	{ "bFastBoot", Bool_Tag, &ConfigureParams.System.bFastBoot },
 	{ "bFastForward", Bool_Tag, &ConfigureParams.System.bFastForward },
@@ -761,6 +762,7 @@ void Configuration_SetDefault(void)
 	ConfigureParams.System.nCpuFreq = 8;	nCpuFreqShift = 0;
 	ConfigureParams.System.nDSPType = DSP_TYPE_NONE;
 	ConfigureParams.System.nVMEType = VME_TYPE_DUMMY; /* for TOS MegaSTE detection */
+	ConfigureParams.System.nRtcYear = 0;
 	ConfigureParams.System.bAddressSpace24 = true;
 	ConfigureParams.System.n_FPUType = FPU_NONE;
 	ConfigureParams.System.bCompatibleFPU = true; /* JIT */

src/includes/configuration.h
@@ -386,6 +386,7 @@ typedef struct
   bool bBlitter;                  /* TRUE if Blitter is enabled */
   DSPTYPE nDSPType;               /* how to "emulate" DSP */
   VMETYPE nVMEType;               /* how to "emulate" SCU/VME */
+  int nRtcYear;
   bool bPatchTimerD;
   bool bFastBoot;                 /* Enable to patch TOS for fast boot */
   bool bFastForward;

src/includes/rtc.h
@@ -10,6 +10,7 @@
 #ifndef HATARI_RTC_H
 #define HATARI_RTC_H
 
+extern void Rtc_Init(void);
 extern void Rtc_SecondsUnits_ReadByte(void);
 extern void Rtc_SecondsTens_ReadByte(void);
 extern void Rtc_MinutesUnits_ReadByte(void);

src/main.c
@@ -44,6 +44,7 @@ const char Main_fileid[] = "Hatari main.c";
 #include "reset.h"
 #include "resolution.h"
 #include "rs232.h"
+#include "rtc.h"
 #include "scc.h"
 #include "screen.h"
 #include "sdlgui.h"
@@ -771,7 +772,8 @@ static void Main_Init(void)
 	IoMem_Init();
 	NvRam_Init();
 	Sound_Init();
-	
+	Rtc_Init();
+
 	/* done as last, needs CPU & DSP running... */
 	DebugUI_Init();
 }

src/rtc.c
@@ -62,12 +62,14 @@ const char Rtc_fileid[] = "Hatari rtc.c";
 #include <time.h>
 
 #include "main.h"
+#include "configuration.h"
 #include "ioMem.h"
 #include "rtc.h"
 
 
 static bool rtc_bank;           /* RTC bank select (0=normal, 1=configuration(?)) */
 static Sint8 fake_am, fake_amz;
+static int year_offset;
 
 
 /*-----------------------------------------------------------------------*/
@@ -80,6 +82,20 @@ static struct tm* get_localtime(void)
 	/* Get system time */
 	time_t nTimeTicks = time(NULL);
 	return localtime(&nTimeTicks);
+}
+
+/*-----------------------------------------------------------------------*/
+/**
+ * tm->tm_year starts from 1900, GEMDOS year from 1980
+ * Set suitable tm->tm_year offset for GEMDOS
+ */
+void Rtc_Init(void)
+{
+	year_offset = 80;
+	if (!ConfigureParams.System.nRtcYear)
+		return;
+
+	year_offset += 1900 + get_localtime()->tm_year - ConfigureParams.System.nRtcYear;
 }
 
 /*-----------------------------------------------------------------------*/
@@ -231,14 +247,13 @@ void Rtc_MonthTens_ReadByte(void)
 	IoMem[0xfffc35] = (get_localtime()->tm_mon + 1) / 10;
 }
 
-
 /*-----------------------------------------------------------------------*/
 /**
  * Read year units.
  */
 void Rtc_YearUnits_ReadByte(void)
 {
-	IoMem[0xfffc37] = get_localtime()->tm_year % 10;
+	IoMem[0xfffc37] = (get_localtime()->tm_year - year_offset) % 10;
 }
 
 
@@ -248,7 +263,7 @@ void Rtc_YearUnits_ReadByte(void)
  */
 void Rtc_YearTens_ReadByte(void)
 {
-	IoMem[0xfffc39] = (get_localtime()->tm_year - 80) / 10;
+	IoMem[0xfffc39] = (get_localtime()->tm_year - year_offset) / 10;
 }
 
 

commit baaa4ac8e41f66e1b5ab94d9a4e2da4777a2e732

Fix RTC option define ID in error message
Reported by Uwe Seimet.

Fixes: ae45605248

src/options.c
@@ -1983,7 +1983,7 @@ bool Opt_ParseParameters(int argc, const char * const argv[])
 			year = atoi(argv[++i]);
 			if(year && (year < 1980 || year >= 2080))
 			{
-				return Opt_ShowError(OPT_CPUCLOCK, argv[i], "Invalid RTC year");
+				return Opt_ShowError(OPT_RTC_YEAR, argv[i], "Invalid RTC year");
 			}
 			ConfigureParams.System.nRtcYear = year;
 			break;