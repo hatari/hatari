hatari/hatari

commit 887c862e446d4204089596ace3907f06f2f3649b

Make more files independent from SDL_types.h, use types from stdint.h instead

src/sound.c
@@ -281,7 +281,7 @@ static ymu16	Vol3Voices = 0;				/* volume 0-0x1f for voices having a constant vo
 
 
 /* Global variables that can be changed/read from other parts of Hatari */
-Uint8		SoundRegs[ 14 ];
+uint8_t		SoundRegs[ 14 ];
 
 int		YmVolumeMixing = YM_TABLE_MIXING;
 
@@ -297,7 +297,7 @@ int		YM2149_Resample_Method = YM2149_RESAMPLE_METHOD_WEIGHTED_AVERAGE_N;
 
 bool		bEnvelopeFreqFlag;			/* Cleared each frame for YM saving */
 
-Sint16		AudioMixBuffer[AUDIOMIXBUFFER_SIZE][2];	/* Ring buffer to store mixed audio output (YM2149, DMA sound, ...) */
+int16_t		AudioMixBuffer[AUDIOMIXBUFFER_SIZE][2];	/* Ring buffer to store mixed audio output (YM2149, DMA sound, ...) */
 int		AudioMixBuffer_pos_write;		/* Current writing position into above buffer */
 int		AudioMixBuffer_pos_read;		/* Current reading position into above buffer */
 
@@ -1405,7 +1405,7 @@ static ymsample	YM2149_NextSample_250 ( void )
  * time an YM register is changed.
  */
 #define BIT_SHIFT 24
-void	Sound_WriteReg( int reg , Uint8 data )
+void Sound_WriteReg(int reg, uint8_t data)
 {
 	switch (reg)
 	{
@@ -1756,7 +1756,7 @@ static int Sound_GenerateSamples(Uint64 CPU_Clock)
  * This is called to built samples up until this clock cycle
  * Sound_Update() can be called several times during a VBL
  */
-void Sound_Update( Uint64 CPU_Clock)
+void Sound_Update(uint64_t CPU_Clock)
 {
 	int pos_write_prev = AudioMixBuffer_pos_write;
 	int Samples_Nbr;

src/tos.c
@@ -47,9 +47,9 @@ const char TOS_fileid[] = "Hatari tos.c";
 #define TEST_PRG_START (TEST_PRG_BASEPAGE + 0x100)
 
 bool bIsEmuTOS;
-Uint32 EmuTosVersion;
-Uint16 TosVersion;                      /* eg. 0x0100, 0x0102 */
-Uint32 TosAddress, TosSize;             /* Address in ST memory and size of TOS image */
+uint32_t EmuTosVersion;
+uint16_t TosVersion;                      /* eg. 0x0100, 0x0102 */
+uint32_t TosAddress, TosSize;             /* Address in ST memory and size of TOS image */
 bool bTosImageLoaded = false;           /* Successfully loaded a TOS image? */
 bool bRamTosImage;                      /* true if we loaded a RAM TOS image */
 bool bUseTos = true;                    /* false if we run in TOS-less test mode */
@@ -81,13 +81,13 @@ enum
 /* This structure is used for patching the TOS ROMs */
 typedef struct
 {
-	Uint16 Version;       /* TOS version number */
+	uint16_t Version;       /* TOS version number */
 	Sint16 Country;       /* TOS country code: -1 if it does not matter, 0=US, 1=Germany, 2=France, etc. */
 	const char *pszName;  /* Name of the patch */
 	int Flags;            /* When should the patch be applied? (see enum above) */
-	Uint32 Address;       /* Where the patch should be applied */
-	Uint32 OldData;       /* Expected first 4 old bytes */
-	Uint32 Size;          /* Length of the patch */
+	uint32_t Address;       /* Where the patch should be applied */
+	uint32_t OldData;       /* Expected first 4 old bytes */
+	uint32_t Size;          /* Length of the patch */
 	const void *pNewData; /* Pointer to the new bytes */
 } TOS_PATCH;
 
@@ -103,16 +103,16 @@ static const char pszAtariLogo[] = "draw Atari Logo";
 static const char pszSTbook[] = "disable MCU access on ST-Book";
 static const char pszNoSparrowHw[] = "disable Sparrow hardware access";
 
-//static Uint8 pRtsOpcode[] = { 0x4E, 0x75 };  /* 0x4E75 = RTS */
-static const Uint8 pNopOpcodes[] = { 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71,
+//static uint8_t pRtsOpcode[] = { 0x4E, 0x75 };  /* 0x4E75 = RTS */
+static const uint8_t pNopOpcodes[] = { 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71,
         0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71,
         0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71, 0x4E, 0x71 };  /* 0x4E71 = NOP */
-static const Uint8 pMouseOpcode[] = { 0xD3, 0xC1 };  /* "ADDA.L D1,A1" (instead of "ADDA.W D1,A1") */
-static const Uint8 pRomCheckOpcode206[] = { 0x60, 0x00, 0x00, 0x98 };  /* BRA $e00894 */
-static const Uint8 pRomCheckOpcode207[] = { 0x60, 0x00, 0x00, 0x98 };  /* BRA $e00892 */
-static const Uint8 pRomCheckOpcode306[] = { 0x60, 0x00, 0x00, 0xB0 };  /* BRA $e00886 */
-static const Uint8 pRomCheckOpcode404[] = { 0x60, 0x00, 0x00, 0x94 };  /* BRA $e00746 */
-static const Uint8 pBraOpcode[] = { 0x60 };  /* 0x60XX = BRA */
+static const uint8_t pMouseOpcode[] = { 0xD3, 0xC1 };  /* "ADDA.L D1,A1" (instead of "ADDA.W D1,A1") */
+static const uint8_t pRomCheckOpcode206[] = { 0x60, 0x00, 0x00, 0x98 };  /* BRA $e00894 */
+static const uint8_t pRomCheckOpcode207[] = { 0x60, 0x00, 0x00, 0x98 };  /* BRA $e00892 */
+static const uint8_t pRomCheckOpcode306[] = { 0x60, 0x00, 0x00, 0xB0 };  /* BRA $e00886 */
+static const uint8_t pRomCheckOpcode404[] = { 0x60, 0x00, 0x00, 0x94 };  /* BRA $e00746 */
+static const uint8_t pBraOpcode[] = { 0x60 };  /* 0x60XX = BRA */
 
 /*
  * Routine for drawing the Atari logo.
@@ -120,7 +120,7 @@ static const Uint8 pBraOpcode[] = { 0x60 };  /* 0x60XX = BRA */
  * We cannot use the vdi yet (the screen workstation has not yet been opened),
  * but we can take into account extended VDI modes.
  */
-static const Uint8 pAtariLogo[] = {
+static const uint8_t pAtariLogo[] = {
 	0x3e, 0x3c, 0x00, 0x01,     /* move.w    #planes, d7; number will be patched below */
 	0x2c, 0x3c, 0, 0, 1, 64,    /* move.l    #linewidth, d6; number will be patched below */
 	0x22, 0x78, 0x04, 0x4e,     /* movea.l   (_v_bas_ad).w,a1 */
@@ -153,24 +153,24 @@ static const Uint8 pAtariLogo[] = {
 	0x4e, 0x71
 };
 
-static const Uint8 p060movep1[] = {	/* replace MOVEP */
+static const uint8_t p060movep1[] = {	/* replace MOVEP */
 	0x70, 0x0c,			/* moveq #12,d0 */
 	0x42, 0x30, 0x08, 0x00,		/* loop: clr.b 0,(d0,a0) */
 	0x55, 0x40,			/* subq  #2,d0 */
 	0x4a, 0x40,			/* tst.w d0 */
 	0x66, 0xf6,			/* bne.s loop */
 };
-static const Uint8 p060movep2[] = {		/* replace MOVEP */
+static const uint8_t p060movep2[] = {		/* replace MOVEP */
 	0x41, 0xf8, 0xfa, 0x26,			/* lea    0xfffffa26.w,a0 */
 	0x20, 0xfc, 0x00, 0x00, 0x00, 0x88,	/* move.l #$00000088,(a0)+ */
 	0x20, 0xbc, 0x00, 0x01, 0x00, 0x05,	/* move.l #$00010005,(a0) */
 	0x4a, 0x38, 0x0a, 0x87			/* tst.b  $a87.w */
 };
-static const Uint8 p060movep3_1[] = {		/* replace MOVEP */
+static const uint8_t p060movep3_1[] = {		/* replace MOVEP */
 	0x4e, 0xb9, 0x00, 0xe7, 0xf0, 0x00,	/* jsr     $e7f000 */
 	0x4e, 0x71				/* nop */
 };
-static const Uint8 p060movep3_2[] = {		/* replace MOVEP $28(a2),d7 */
+static const uint8_t p060movep3_2[] = {		/* replace MOVEP $28(a2),d7 */
 
 	0x00, 0x7c, 0x07, 0x00,			/* ori       #$700,sr */
 	0x1e, 0x2a, 0x00, 0x28,			/* move.b    $28(a2),d7 */
@@ -183,10 +183,10 @@ static const Uint8 p060movep3_2[] = {		/* replace MOVEP $28(a2),d7 */
 	0x4e, 0x75				/* rts */
 };
 
-static const Uint8 pFalconExtraRAM_1[] = {
+static const uint8_t pFalconExtraRAM_1[] = {
 	0x4e, 0xb9, 0x00, 0xe7, 0xf1, 0x00	/* jsr       $e7f100 */
 };
-static const Uint8 pFalconExtraRAM_2[] = {	/* call maddalt() to declare the extra RAM */
+static const uint8_t pFalconExtraRAM_2[] = {	/* call maddalt() to declare the extra RAM */
 	0x20, 0x38, 0x05, 0xa4,			/* move.l    $05a4.w,d0 */
 	0x67, 0x18,				/* beq.s     $ba2d2 */
 	0x04, 0x80, 0x01, 0x00, 0x00, 0x00,	/* subi.l    #$1000000,d0 */
@@ -727,7 +727,7 @@ void TOS_MemorySnapShot_Capture(bool bSave)
  *
  * Set logpatch_addr if patch for that is needed.
  */
-static void TOS_FixRom(Uint32 *logopatch_addr)
+static void TOS_FixRom(uint32_t *logopatch_addr)
 {
 	int nGoodPatches, nBadPatches;
 	short TosCountry;
@@ -958,14 +958,14 @@ static uint8_t *TOS_LoadImage(void)
 	TosSize = nFileSize;
 
 	/* Check for RAM TOS images first: */
-	if (SDL_SwapBE32(*(Uint32 *)pTosFile) == 0x46FC2700)
+	if (SDL_SwapBE32(*(uint32_t *)pTosFile) == 0x46FC2700)
 	{
 		int nRamTosLoaderSize;
 		Log_Printf(LOG_WARN, "Detected a RAM TOS - this will probably not work very well!\n");
 		/* RAM TOS images have a 256 bytes loader function before the real image
 		 * starts (34 bytes for TOS 4.92). Since we directly copy the image to the right
 		 * location later, we simply skip this additional header here: */
-		if (SDL_SwapBE32(*(Uint32 *)(pTosFile+34)) == 0x602E0492)
+		if (SDL_SwapBE32(*(uint32_t *)(pTosFile+34)) == 0x602E0492)
 			nRamTosLoaderSize = 0x22;
 		else
 			nRamTosLoaderSize = 0x100;
@@ -979,20 +979,20 @@ static uint8_t *TOS_LoadImage(void)
 	}
 
 	/* Check for EmuTOS ... (0x45544F53 = 'ETOS') */
-	bIsEmuTOS = (SDL_SwapBE32(*(Uint32 *)&pTosFile[0x2c]) == 0x45544F53);
+	bIsEmuTOS = (SDL_SwapBE32(*(uint32_t *)&pTosFile[0x2c]) == 0x45544F53);
 	if (bIsEmuTOS)
 	{
 		/* The magic value 'OSXH' indicates an extended header */
-		if (SDL_SwapBE32(*(Uint32 *)&pTosFile[0x34]) == 0x4F535848)
-			EmuTosVersion = SDL_SwapBE32(*(Uint32 *)&pTosFile[0x3c]);
+		if (SDL_SwapBE32(*(uint32_t *)&pTosFile[0x34]) == 0x4F535848)
+			EmuTosVersion = SDL_SwapBE32(*(uint32_t *)&pTosFile[0x3c]);
 		else
 			EmuTosVersion = 0;	/* Older than 1.0 */
 	}
 
 	/* Now, look at start of image to find Version number and address */
-	TosVersion = SDL_SwapBE16(*(Uint16 *)&pTosFile[2]);
-	TosAddress = SDL_SwapBE32(*(Uint32 *)&pTosFile[8]);
-	if (TosVersion == 0x206 && SDL_SwapBE16(*(Uint16 *)&pTosFile[30]) == 0x186A)
+	TosVersion = SDL_SwapBE16(*(uint16_t *)&pTosFile[2]);
+	TosAddress = SDL_SwapBE32(*(uint32_t *)&pTosFile[8]);
+	if (TosVersion == 0x206 && SDL_SwapBE16(*(uint16_t *)&pTosFile[30]) == 0x186A)
 		TosVersion = 0x208;
 
 	/* Check for reasonable TOS version: */
@@ -1103,8 +1103,8 @@ static uint8_t *TOS_FakeRomForTesting(void)
 int TOS_InitImage(void)
 {
 	uint8_t *pTosFile = NULL;
-	Uint32 logopatch_addr = 0;
-	Uint16 osconf, countrycode;
+	uint32_t logopatch_addr = 0;
+	uint16_t osconf, countrycode;
 
 	bTosImageLoaded = false;
 

src/acia.c
@@ -233,21 +233,21 @@ ACIA_STRUCT		*pACIA_MIDI;
 static void		ACIA_Init_Pointers ( ACIA_STRUCT *pAllACIA );
 
 static void		ACIA_Set_Line_IRQ_MFP ( int bit );
-static Uint8 		ACIA_Get_Line_CTS_Dummy ( void );
-static Uint8 		ACIA_Get_Line_DCD_Dummy ( void );
+static uint8_t		ACIA_Get_Line_CTS_Dummy ( void );
+static uint8_t		ACIA_Get_Line_DCD_Dummy ( void );
 static void		ACIA_Set_Line_RTS_Dummy ( int bit );
 
 static void		ACIA_Set_Timers_IKBD ( void *pACIA );
 static void		ACIA_Start_InterruptHandler_IKBD ( ACIA_STRUCT *pACIA , int InternalCycleOffset );
 
-static Uint8		ACIA_MasterReset ( ACIA_STRUCT *pACIA , Uint8 CR );
+static uint8_t		ACIA_MasterReset ( ACIA_STRUCT *pACIA , uint8_t CR );
 
 static void		ACIA_UpdateIRQ ( ACIA_STRUCT *pACIA );
 
-static Uint8		ACIA_Read_SR ( ACIA_STRUCT *pACIA );
-static void		ACIA_Write_CR ( ACIA_STRUCT *pACIA , Uint8 CR );
-static Uint8		ACIA_Read_RDR ( ACIA_STRUCT *pACIA );
-static void		ACIA_Write_TDR ( ACIA_STRUCT *pACIA , Uint8 TDR );
+static uint8_t		ACIA_Read_SR ( ACIA_STRUCT *pACIA );
+static void		ACIA_Write_CR ( ACIA_STRUCT *pACIA , uint8_t CR );
+static uint8_t		ACIA_Read_RDR ( ACIA_STRUCT *pACIA );
+static void		ACIA_Write_TDR ( ACIA_STRUCT *pACIA , uint8_t TDR );
 
 static void		ACIA_Prepare_TX ( ACIA_STRUCT *pACIA );
 static void		ACIA_Prepare_RX ( ACIA_STRUCT *pACIA );
@@ -269,7 +269,7 @@ static void		ACIA_Clock_RX ( ACIA_STRUCT *pACIA );
  * seem to be transmitted (maybe with errors ?). So we default
  * to 9600 bauds to avoid a lock if a program uses tx/rx after a reset.
  */
-void	ACIA_Init ( ACIA_STRUCT *pAllACIA , Uint32 TX_Clock , Uint32 RX_Clock )
+void	ACIA_Init ( ACIA_STRUCT *pAllACIA , uint32_t TX_Clock , uint32_t RX_Clock )
 {
 	int	i;
 
@@ -395,9 +395,9 @@ static void	ACIA_Set_Line_IRQ_MFP ( int bit )
  * When CTS is high, TDRE should always be set to 0
  * Note : this is not connected on an ST, so we always return 0.
  */
-static Uint8 	ACIA_Get_Line_CTS_Dummy ( void )
+static uint8_t ACIA_Get_Line_CTS_Dummy ( void )
 {
-	Uint8		bit;
+	uint8_t bit;
 
 	bit = 0;
 	LOG_TRACE ( TRACE_ACIA, "acia get cts=%d VBL=%d HBL=%d\n" , bit , nVBLs , nHBL );
@@ -409,9 +409,9 @@ static Uint8 	ACIA_Get_Line_CTS_Dummy ( void )
  * Read the Data Carrier Detect (DCD) pin
  * Note : this is not connected on an ST, so we always return 0.
  */
-static Uint8 	ACIA_Get_Line_DCD_Dummy ( void )
+static uint8_t ACIA_Get_Line_DCD_Dummy ( void )
 {
-	Uint8		bit;
+	uint8_t bit;
 
 	bit = 0;
 	LOG_TRACE ( TRACE_ACIA, "acia get dcd=%d VBL=%d HBL=%d\n" , bit , nVBLs , nHBL );
@@ -654,11 +654,11 @@ void	ACIA_IKBD_Write_TDR ( void )
  * This also returns the new state of the RTS bit, that must be updated
  * in ACIA_Write_CR.
  */
-static Uint8	ACIA_MasterReset ( ACIA_STRUCT *pACIA , Uint8 CR )
+static uint8_t	ACIA_MasterReset ( ACIA_STRUCT *pACIA , uint8_t CR )
 {
-	Uint8		dcd_bit;
-	Uint8		cts_bit;
-	Uint8		rts_bit;
+	uint8_t		dcd_bit;
+	uint8_t		cts_bit;
+	uint8_t		rts_bit;
 
 
 	LOG_TRACE ( TRACE_ACIA, "acia %s master reset VBL=%d HBL=%d\n" , pACIA->ACIA_Name , nVBLs , nHBL );
@@ -702,7 +702,7 @@ static Uint8	ACIA_MasterReset ( ACIA_STRUCT *pACIA , Uint8 CR )
  */
 static void	ACIA_UpdateIRQ ( ACIA_STRUCT *pACIA )
 {
-	Uint8		irq_bit_new;
+	uint8_t		irq_bit_new;
 
 	irq_bit_new = 0;
 
@@ -742,9 +742,9 @@ static void	ACIA_UpdateIRQ ( ACIA_STRUCT *pACIA )
  * Read SR.
  * Also update CTS ; when CTS is high, TDRE should always be masked to 0.
  */
-static Uint8	ACIA_Read_SR ( ACIA_STRUCT *pACIA )
+static uint8_t	ACIA_Read_SR ( ACIA_STRUCT *pACIA )
 {
-	Uint8	SR;
+	uint8_t	SR;
 
 
 	if ( pACIA->Get_Line_CTS() == 1 )
@@ -770,11 +770,11 @@ static Uint8	ACIA_Read_SR ( ACIA_STRUCT *pACIA )
 /**
  * Write to CR.
  */
-static void	ACIA_Write_CR ( ACIA_STRUCT *pACIA , Uint8 CR )
+static void	ACIA_Write_CR ( ACIA_STRUCT *pACIA , uint8_t CR )
 {
 	int	Divide;
 	int	Force_rts_bit;
-	Uint8	rts_bit=0;
+	uint8_t	rts_bit=0;
 
 	LOG_TRACE ( TRACE_ACIA, "acia %s write cr data=0x%02x VBL=%d HBL=%d\n" , pACIA->ACIA_Name , CR , nVBLs , nHBL );
 
@@ -841,7 +841,7 @@ static void	ACIA_Write_CR ( ACIA_STRUCT *pACIA , Uint8 CR )
  * - IRQ bit should be updated depending on the new values of BIT_RDRF,
  *   BIT_DCD and BIT_OVRN.
  */
-static Uint8	ACIA_Read_RDR ( ACIA_STRUCT *pACIA )
+static uint8_t	ACIA_Read_RDR ( ACIA_STRUCT *pACIA )
 {
 	pACIA->SR &= ~( ACIA_SR_BIT_RDRF | ACIA_SR_BIT_PE );
 
@@ -878,7 +878,7 @@ static Uint8	ACIA_Read_RDR ( ACIA_STRUCT *pACIA )
  * immediately, to ensure that BIT_TDRE remains clear until the next bit
  * is sent (BIT_TDRE will be set again in ACIA_Clock_TX).
  */
-static void	ACIA_Write_TDR ( ACIA_STRUCT *pACIA , Uint8 TDR )
+static void	ACIA_Write_TDR ( ACIA_STRUCT *pACIA , uint8_t TDR )
 {
 	LOG_TRACE ( TRACE_ACIA, "acia %s write tdr data=0x%02x overwrite=%s tx_state=%d VBL=%d HBL=%d\n" , pACIA->ACIA_Name , TDR ,
 		( pACIA->SR & ACIA_SR_BIT_TDRE ) ? "no" : "yes" , pACIA->TX_State , nVBLs , nHBL );
@@ -942,7 +942,7 @@ static void	ACIA_Prepare_RX ( ACIA_STRUCT *pACIA )
 static void	ACIA_Clock_TX ( ACIA_STRUCT *pACIA )
 {
 	int	StateNext;
-	Uint8	tx_bit;
+	uint8_t	tx_bit;
 
 
 	LOG_TRACE ( TRACE_ACIA, "acia %s clock_tx tx_state=%d VBL=%d HBL=%d\n" , pACIA->ACIA_Name , pACIA->TX_State , nVBLs , nHBL );
@@ -1026,7 +1026,7 @@ static void	ACIA_Clock_TX ( ACIA_STRUCT *pACIA )
 static void	ACIA_Clock_RX ( ACIA_STRUCT *pACIA )
 {
 	int	StateNext;
-	Uint8	rx_bit;
+	uint8_t	rx_bit;
 
 
 	rx_bit = pACIA->Get_Line_RX();
@@ -1132,7 +1132,7 @@ static void	ACIA_Clock_RX ( ACIA_STRUCT *pACIA )
 }
 
 
-void ACIA_Info(FILE *fp, Uint32 dummy)
+void ACIA_Info(FILE *fp, uint32_t dummy)
 {
 	fprintf(fp, "Keyboard ACIA:\n");
 	fprintf(fp, "- Control / status: 0x%02x\n", IoMem[0xfffc00]);

commit 73cddffc26c8746f9a21884355ee400426297380

Complete earlier SDL_types.h independence commit
Earlier commits missed some of the SDL types in the files.

Remaining ones were converted with:
  for x in 8 16 32 64; do
    sed -i \
      -e "s/\bUint${x}\b/uint${x}_t/g" \
      -e "s/\bSint${x}\b/int${x}_t/g" \
      <files>;
  done

Fixes: 887c862e446
Fixes: 842d4bb32a8
Fixes: b29bcad91c4

src/gemdos.c
@@ -89,15 +89,15 @@ EMULATEDDRIVE **emudrives = NULL;
 
 typedef struct {
   /* GEMDOS internals */
-  Uint8 index[2];
-  Uint8 magic[4];
+  uint8_t index[2];
+  uint8_t magic[4];
   char dta_pat[TOS_NAMELEN]; /* unused */
   char dta_sattrib;          /* unused */
   /* TOS API */
   char dta_attrib;
-  Uint8 dta_time[2];
-  Uint8 dta_date[2];
-  Uint8 dta_size[4];
+  uint8_t dta_time[2];
+  uint8_t dta_date[2];
+  uint8_t dta_size[4];
   char dta_name[TOS_NAMELEN];
 } DTA;
 
@@ -272,9 +272,9 @@ static bool GemDOS_SetFileInformation(int Handle, DATETIME *DateTime)
 /**
  * Convert from FindFirstFile/FindNextFile attribute to GemDOS format
  */
-static Uint8 GemDOS_ConvertAttribute(mode_t mode)
+static uint8_t GemDOS_ConvertAttribute(mode_t mode)
 {
-	Uint8 Attrib = 0;
+	uint8_t Attrib = 0;
 
 	/* Directory attribute */
 	if (S_ISDIR(mode))
@@ -2334,7 +2334,7 @@ static bool GemDOS_Read(uint32_t Params)
 	}
 
 	/* Limit to size of file to prevent errors */
-	if (Size > (Uint32)nBytesLeft)
+	if (Size > (uint32_t)nBytesLeft)
 		Size = nBytesLeft;
 
 	/* Check that read is to valid memory area */
@@ -2377,7 +2377,7 @@ static bool GemDOS_Write(uint32_t Params)
 	char *pBuffer;
 	long nBytesWritten;
 	uint32_t Addr;
-	Sint32 Size;
+	int32_t Size;
 	int Handle, fh_idx;
 	FILE *fp;
 
@@ -2514,7 +2514,7 @@ static bool GemDOS_LSeek(uint32_t Params)
 	FILE *fhndl;
 
 	/* Read details from stack */
-	Offset = (Sint32)STMemory_ReadLong(Params);
+	Offset = (int32_t)STMemory_ReadLong(Params);
 	Handle = STMemory_ReadWord(Params+SIZE_LONG);
 	Mode = STMemory_ReadWord(Params+SIZE_LONG+SIZE_WORD);
 
@@ -3332,7 +3332,7 @@ static bool GemDOS_Pterm0(uint32_t Params)
 static bool GemDOS_Ptermres(uint32_t Params)
 {
 	LOG_TRACE(TRACE_OS_GEMDOS|TRACE_OS_BASE, "GEMDOS 0x31 Ptermres(0x%X, %hd) at PC 0x%X\n",
-		  STMemory_ReadLong(Params), (Sint16)STMemory_ReadWord(Params+SIZE_WORD),
+		  STMemory_ReadLong(Params), (int16_t)STMemory_ReadWord(Params+SIZE_WORD),
 		  CallingPC);
 	GemDOS_TerminateClose();
 	return false;

src/sound.c
@@ -317,8 +317,8 @@ ymsample	YM_Buffer_250[ YM_BUFFER_250_SIZE ];	/* Ring buffer to store YM samples
 static int	YM_Buffer_250_pos_write;		/* Current writing position into above buffer */
 static int	YM_Buffer_250_pos_read;			/* Current reading position into above buffer */
 
-static Uint64	YM2149_Clock_250;			/* 250 kHz counter */
-static Uint64	YM2149_Clock_250_CpuClock;		/* Corresponding value of CyclesGlobalClockCounter at the time YM2149_Clock_250 was updated */
+static uint64_t	YM2149_Clock_250;			/* 250 kHz counter */
+static uint64_t	YM2149_Clock_250_CpuClock;		/* Corresponding value of CyclesGlobalClockCounter at the time YM2149_Clock_250 was updated */
 
 
 
@@ -347,7 +347,7 @@ static void	YM2149_Normalise_5bit_Table(ymu16 *in_5bit , yms16 *out_5bit, unsign
 
 static void	YM2149_EnvBuild		(void);
 static void	Ym2149_BuildVolumeTable	(void);
-static void	YM2149_UpdateClock_250	( Uint64 CpuClock );
+static void	YM2149_UpdateClock_250	( uint64_t CpuClock );
 static void	Ym2149_Init		(void);
 static void	Ym2149_Reset		(void);
 
@@ -356,8 +356,8 @@ static ymu16	YM2149_TonePer		(ymu8 rHigh , ymu8 rLow);
 static ymu16	YM2149_NoisePer		(ymu8 rNoise);
 static ymu16	YM2149_EnvPer		(ymu8 rHigh , ymu8 rLow);
 
-static void	YM2149_Run		( Uint64 CPU_Clock );
-static int	Sound_GenerateSamples	( Uint64 CPU_Clock);
+static void	YM2149_Run		( uint64_t CPU_Clock );
+static int	Sound_GenerateSamples	( uint64_t CPU_Clock);
 static void	YM2149_DoSamples_250	( int SamplesToGenerate_250 );
 #ifdef YM_250_DEBUG
 static void	YM2149_DoSamples_250_Debug ( int SamplesToGenerate , int pos );
@@ -800,11 +800,11 @@ static void	Ym2149_BuildVolumeTable(void)
 #if 0
 /* integer version : use it when YM2149's clock is the same as CPU's clock (eg STF) */
 
-static void	YM2149_UpdateClock_250_int ( Uint64 CpuClock )
+static void	YM2149_UpdateClock_250_int ( uint64_t CpuClock )
 {
-	Uint64		CpuClockDiff;
-	Uint64		YM_Div;
-	Uint64		YM_Inc;
+	uint64_t		CpuClockDiff;
+	uint64_t		YM_Div;
+	uint64_t		YM_Inc;
 
 	/* We divide CpuClockDiff by YM_Div to get a 250 Hz YM clock increment (YM_Div=32 for an STF with a 8 MHz CPU) */
 	YM_Div = 32 << nCpuFreqShift;
@@ -828,11 +828,11 @@ static void	YM2149_UpdateClock_250_int ( Uint64 CpuClock )
 
 /* floating point version : use it when YM2149's clock is different from CPU's clock (eg STE) */
 
-static void	YM2149_UpdateClock_250_float ( Uint64 CpuClock )
+static void	YM2149_UpdateClock_250_float ( uint64_t CpuClock )
 {
-	Uint64		CpuClockDiff;
+	uint64_t		CpuClockDiff;
 	double		YM_Div;
-	Uint64		YM_Inc;
+	uint64_t		YM_Inc;
 
 	/* We divide CpuClockDiff by YM_Div to get a 250 Hz YM clock increment (YM_Div=32.0425 for an STE with a 8 MHz CPU) */
 	YM_Div = ((double)MachineClocks.CPU_Freq_Emul) / YM_ATARI_CLOCK_COUNTER;
@@ -842,7 +842,7 @@ static void	YM2149_UpdateClock_250_float ( Uint64 CpuClock )
 	CpuClockDiff = CpuClock - YM2149_Clock_250_CpuClock;
 	if ( CpuClockDiff >= YM_Div )
 	{
-		YM_Inc = CpuClockDiff / YM_Div;			/* will truncate to lower integer when casting to Uint64 */
+		YM_Inc = CpuClockDiff / YM_Div;			/* will truncate to lower integer when casting to uint64_t */
 //fprintf ( stderr , "update_250  in div=%f clock_cpu=%lu cpu_diff=%lu inc=%lu clock_250_in=%lu\n" , YM_Div, CpuClock, CpuClockDiff, YM_Inc, YM2149_Clock_250 );
 		YM2149_Clock_250 += YM_Inc;
 		YM2149_Clock_250_CpuClock = CpuClock - round ( fmod ( CpuClockDiff , YM_Div ) );
@@ -854,9 +854,9 @@ static void	YM2149_UpdateClock_250_float ( Uint64 CpuClock )
 #endif
 
 
-static void	YM2149_UpdateClock_250_int_new ( Uint64 CpuClock )
+static void	YM2149_UpdateClock_250_int_new ( uint64_t CpuClock )
 {
-	Uint64		CpuClockDiff;
+	uint64_t		CpuClockDiff;
 
 
 	CpuClockDiff = CpuClock - YM2149_Clock_250_CpuClock;
@@ -880,7 +880,7 @@ static void	YM2149_UpdateClock_250_int_new ( Uint64 CpuClock )
  * In the end, 'integer' and 'floating point' versions will sound the same because
  * floating point precision should be good enough to avoid rounding errors.
  */
-static void	YM2149_UpdateClock_250 ( Uint64 CpuClock )
+static void	YM2149_UpdateClock_250 ( uint64_t CpuClock )
 {
 	if ( ConfigureParams.System.nMachineType == MACHINE_ST || ConfigureParams.System.nMachineType == MACHINE_MEGA_ST )
 {
@@ -1138,7 +1138,7 @@ static void	YM2149_DoSamples_250 ( int SamplesToGenerate_250 )
  */
 static void	YM2149_DoSamples_250_Debug ( int SamplesToGenerate , int pos )
 {
-	static Uint8 WavHeader[] =
+	static uint8_t WavHeader[] =
 	{
 		/* RIFF chunk */
 		'R', 'I', 'F', 'F',      /* "RIFF" (ASCII Characters) */
@@ -1173,21 +1173,21 @@ static void	YM2149_DoSamples_250_Debug ( int SamplesToGenerate , int pos )
 	{
 		file_ptr = fopen( "hatari_250.wav", "wb");
 		/* Patch mono, 2 bytes per sample */
-		WavHeader[22] = (Uint8)0x01;
-		WavHeader[32] = (Uint8)0x02;
+		WavHeader[22] = (uint8_t)0x01;
+		WavHeader[32] = (uint8_t)0x02;
 
 		/* Patch sample frequency in header structure */
 		val = 250000;
-		WavHeader[24] = (Uint8)val;
-		WavHeader[25] = (Uint8)(val >> 8);
-		WavHeader[26] = (Uint8)(val >> 16);
-		WavHeader[27] = (Uint8)(val >> 24);
+		WavHeader[24] = (uint8_t)val;
+		WavHeader[25] = (uint8_t)(val >> 8);
+		WavHeader[26] = (uint8_t)(val >> 16);
+		WavHeader[27] = (uint8_t)(val >> 24);
 		/* Patch bytes per second in header structure */
 		val = 250000 * 2;
-		WavHeader[28] = (Uint8)val;
-		WavHeader[29] = (Uint8)(val >> 8);
-		WavHeader[30] = (Uint8)(val >> 16);
-		WavHeader[31] = (Uint8)(val >> 24);
+		WavHeader[28] = (uint8_t)val;
+		WavHeader[29] = (uint8_t)(val >> 8);
+		WavHeader[30] = (uint8_t)(val >> 16);
+		WavHeader[31] = (uint8_t)(val >> 24);
 
 		fwrite ( &WavHeader, sizeof(WavHeader), 1, file_ptr );
 	}
@@ -1202,15 +1202,15 @@ static void	YM2149_DoSamples_250_Debug ( int SamplesToGenerate , int pos )
 
 	/* Update sizes in header */
 	val = 12+24+8+wav_size-8;			/* RIFF size */
-	WavHeader[4] = (Uint8)val;
-	WavHeader[5] = (Uint8)(val >> 8);
-	WavHeader[6] = (Uint8)(val >> 16);
-	WavHeader[7] = (Uint8)(val >> 24);
+	WavHeader[4] = (uint8_t)val;
+	WavHeader[5] = (uint8_t)(val >> 8);
+	WavHeader[6] = (uint8_t)(val >> 16);
+	WavHeader[7] = (uint8_t)(val >> 24);
 	val = wav_size;					/* data size */
-	WavHeader[40] = (Uint8)val;
-	WavHeader[41] = (Uint8)(val >> 8);
-	WavHeader[42] = (Uint8)(val >> 16);
-	WavHeader[43] = (Uint8)(val >> 24);
+	WavHeader[40] = (uint8_t)val;
+	WavHeader[41] = (uint8_t)(val >> 8);
+	WavHeader[42] = (uint8_t)(val >> 16);
+	WavHeader[43] = (uint8_t)(val >> 24);
 
 	rewind ( file_ptr );
 	fwrite ( &WavHeader, sizeof(WavHeader), 1, file_ptr );
@@ -1234,9 +1234,9 @@ static void	YM2149_DoSamples_250_Debug ( int SamplesToGenerate , int pos )
  * On each call, we consider samples were already generated up to (and including) counter value
  * YM2149_Clock_250_prev. We must generate as many samples to reach (and include) YM2149_Clock_250.
  */
-static void	YM2149_Run ( Uint64 CPU_Clock )
+static void	YM2149_Run ( uint64_t CPU_Clock )
 {
-	Uint64		YM2149_Clock_250_prev;
+	uint64_t		YM2149_Clock_250_prev;
 	int		YM2149_Nb_Updates_250;
 
 
@@ -1344,9 +1344,9 @@ static ymsample	YM2149_Next_Resample_Weighted_Average_2 ( void )
  */
  static ymsample	YM2149_Next_Resample_Weighted_Average_N ( void )
 {
-	static Uint32	pos_fract = 0;
-	Uint32		interval_fract;
-	Sint64		total;
+	static uint32_t	pos_fract = 0;
+	uint32_t		interval_fract;
+	int64_t		total;
 	ymsample	sample;
 
 
@@ -1357,7 +1357,7 @@ static ymsample	YM2149_Next_Resample_Weighted_Average_2 ( void )
 
 	if ( pos_fract )				/* start position : 0xffff <= pos_fract <= 0 */
 	{
-		total += ((Sint64)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * ( 0x10000 - pos_fract );
+		total += ((int64_t)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * ( 0x10000 - pos_fract );
 		YM_Buffer_250_pos_read = ( YM_Buffer_250_pos_read + 1 ) & YM_BUFFER_250_SIZE_MASK;
 		pos_fract -= 0x10000;			/* next sample */
 	}
@@ -1366,14 +1366,14 @@ static ymsample	YM2149_Next_Resample_Weighted_Average_2 ( void )
 
 	while ( pos_fract & 0xffff0000 )		/* check integer part */
 	{
-		total += ((Sint64)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * 0x10000;
+		total += ((int64_t)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * 0x10000;
 		YM_Buffer_250_pos_read = ( YM_Buffer_250_pos_read + 1 ) & YM_BUFFER_250_SIZE_MASK;
 		pos_fract -= 0x10000;			/* next sample */
 	}
 
 	if ( pos_fract )				/* partial end sample if 0xffff <= pos_fract < 0 */
 	{
-		total += ((Sint64)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * pos_fract;
+		total += ((int64_t)YM_Buffer_250[ YM_Buffer_250_pos_read ]) * pos_fract;
 	}
 
 //fprintf ( stderr , "next 2 %d\n" , YM_Buffer_250_pos_read );
@@ -1690,7 +1690,7 @@ void Sound_Stats_Show ( void )
 /**
  * Generate output samples for all channels (YM2149, DMA or crossbar) during this time-frame
  */
-static int Sound_GenerateSamples(Uint64 CPU_Clock)
+static int Sound_GenerateSamples(uint64_t CPU_Clock)
 {
 	int	idx;
 	int	ym_margin;

src/tos.c
@@ -82,7 +82,7 @@ enum
 typedef struct
 {
 	uint16_t Version;       /* TOS version number */
-	Sint16 Country;       /* TOS country code: -1 if it does not matter, 0=US, 1=Germany, 2=France, etc. */
+	int16_t Country;       /* TOS country code: -1 if it does not matter, 0=US, 1=Germany, 2=France, etc. */
 	const char *pszName;  /* Name of the patch */
 	int Flags;            /* When should the patch be applied? (see enum above) */
 	uint32_t Address;       /* Where the patch should be applied */

tests/debugger/test-breakcond.c
@@ -16,16 +16,16 @@
 #define CMD_REMOVE_ALL "all"
 
 
-static bool SetCpuRegister(const char *regname, Uint32 value)
+static bool SetCpuRegister(const char *regname, uint32_t value)
 {
-	Uint32 *addr;
+	uint32_t *addr;
 	
 	switch (DebugCpu_GetRegisterAddress(regname, &addr)) {
 	case 32:
 		*addr = value;
 		break;
 	case 16:
-		*(Uint16*)addr = value;
+		*(uint16_t*)addr = value;
 		break;
 	default:
 		fprintf(stderr, "SETUP ERROR: Register '%s' to set (to %x) is unrecognized!\n", regname, value);
@@ -35,16 +35,16 @@ static bool SetCpuRegister(const char *regname, Uint32 value)
 }
 
 #if 0
-static bool SetDspRegister(const char *regname, Uint32 value)
+static bool SetDspRegister(const char *regname, uint32_t value)
 {
-	Uint32 *addr, mask;
+	uint32_t *addr, mask;
 
 	switch (DSP_GetRegisterAddress(regname, &addr, &mask)) {
 	case 32:
 		*addr = value & mask;
 		break;
 	case 16:
-		*(Uint16*)addr = value & mask;
+		*(uint16_t*)addr = value & mask;
 		break;
 	default:
 		return false;

tests/debugger/test-dummies.c
@@ -50,11 +50,11 @@ uint16_t STMemory_ReadWord(uint32_t addr) {
 	val = (STRam[addr] << 8) | STRam[addr+1];
 	return val;
 }
-Uint8 STMemory_ReadByte(uint32_t addr) {
+uint8_t STMemory_ReadByte(uint32_t addr) {
 	if (addr >= STRamEnd) return 0;
 	return STRam[addr];
 }
-void STMemory_WriteByte(uint32_t addr, Uint8 val) {
+void STMemory_WriteByte(uint32_t addr, uint8_t val) {
 	if (addr < STRamEnd)
 		STRam[addr] = val;
 }