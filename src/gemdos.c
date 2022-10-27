hatari/hatari

commit 799020b84d0224a4bb38f2b892a8ecdd1c11bcc1

GEMDOS HD: allow writes to files opened as read-only
See: https://www.atari-forum.com/viewtopic.php?p=438987

src/gemdos.c
@@ -133,6 +133,7 @@ static struct {
 typedef struct
 {
 	bool bUsed;
+	bool bReadOnly;
 	char szMode[4];     /* enough for all used fopen() modes: rb/rb+/wb+ */
 	uint32_t Basepage;
 	FILE *FileHandle;
@@ -2144,6 +2145,7 @@ static bool GemDOS_Open(uint32_t Params)
 		FileHandles[Index].FileHandle = OverrideHandle;
 		RealMode = "read-only";
 		ModeStr = "rb";
+		Mode = 0;
 	}
 	else
 	{
@@ -2168,15 +2170,13 @@ static bool GemDOS_Open(uint32_t Params)
 		 * support write-only without truncating the file).
 		 *
 		 * Read-only status is used if:
-		 * - requested by Atari program
 		 * - Hatari write protection is enabled
 		 * - File itself is read-only
 		 * Latter is done to help cases where application
 		 * needlessly requests write access, but file is
 		 * on read-only media (like CD/DVD).
 		 */
-		if (Mode == 0 ||
-		    ConfigureParams.HardDisk.nWriteProtection == WRITEPROT_ON ||
+		if (ConfigureParams.HardDisk.nWriteProtection == WRITEPROT_ON ||
 		    (stat(szActualFileName, &FileStat) == 0 && !(FileStat.st_mode & S_IWUSR)))
 		{
 			ModeStr = "rb";
@@ -2194,6 +2194,7 @@ static bool GemDOS_Open(uint32_t Params)
 	{
 		/* Tag handle table entry as used in this process and return handle */
 		FileHandles[Index].bUsed = true;
+		FileHandles[Index].bReadOnly = (Mode == 0);
 		strcpy(FileHandles[Index].szMode, ModeStr);
 		FileHandles[Index].Basepage = STMemory_ReadLong(act_pd);
 		snprintf(FileHandles[Index].szActualName,
@@ -2436,6 +2437,9 @@ static bool GemDOS_Write(uint32_t Params)
 		fflush(fp);
 		Regs[REG_D0] = nBytesWritten;      /* OK */
 	}
+	if (FileHandles[fh_idx].bReadOnly)
+		Log_Printf(LOG_WARN, "GEMDOS Fwrite() to a file opened as read-only: %s\n",
+			   FileHandles[fh_idx].szActualName);
 	return true;
 }
 

commit 68a9cc999a6167427aaf50c55f6ad6c2c5055425

Avoid stale GEMDOS HD read-only status
And resulting spurious Fwrite() warnings with FDs created
either with Fcreate(), or state restore.

Reported-By: Uwe Seimet

Fixes: 799020b84d02

Also:
* Correct virtual INF file Fopen() mode in GEMDOS trace
* Update warning msg now that it's also about files that are
  not supposed to be written, not just ones Fopen()ed read-only
* Show FD read-only status also with "info gemdos"

src/gemdos.c
@@ -896,6 +896,8 @@ static void restore_file_handle_info(int i, FILE_HANDLE *handle)
 		fclose(fp);
 		return;
 	}
+	/* used only for warnings, ignore those after restore */
+	handle->bReadOnly = false;
 	handle->FileHandle = fp;
 }
 
@@ -2021,8 +2023,11 @@ static bool GemDOS_Create(uint32_t Params)
 		 * - GEMDOS_FILE_ATTRIB_WRITECLOSE   (FA_ARCHIVE)
 		 *   (set automatically by GemDOS >= 0.15)
 		 */
+		FileHandles[Index].bReadOnly = false;
 		if (Mode & GEMDOS_FILE_ATTRIB_READONLY)
 		{
+			/* enable write warnings */
+			FileHandles[Index].bReadOnly = true;
 			/* after closing, file should be read-only */
 			if (chmod(szActualFileName, S_IRUSR|S_IRGRP|S_IROTH))
 			{
@@ -2127,9 +2132,9 @@ static bool GemDOS_Open(uint32_t Params)
 	{
 		strcpy(szActualFileName, pszFileName);
 		FileHandles[Index].FileHandle = OverrideHandle;
+		FileHandles[Index].bReadOnly = true;
 		RealMode = "read-only";
 		ModeStr = "rb";
-		Mode = 0;
 	}
 	else
 	{
@@ -2165,11 +2170,13 @@ static bool GemDOS_Open(uint32_t Params)
 		{
 			ModeStr = "rb";
 			RealMode = "read-only";
+			FileHandles[Index].bReadOnly = true;
 		}
 		else
 		{
 			ModeStr = "rb+";
 			RealMode = "read+write";
+			FileHandles[Index].bReadOnly = (Mode == 0);
 		}
 		FileHandles[Index].FileHandle = fopen(szActualFileName, ModeStr);
 	}
@@ -2178,7 +2185,6 @@ static bool GemDOS_Open(uint32_t Params)
 	{
 		/* Tag handle table entry as used in this process and return handle */
 		FileHandles[Index].bUsed = true;
-		FileHandles[Index].bReadOnly = (Mode == 0);
 		strcpy(FileHandles[Index].szMode, ModeStr);
 		FileHandles[Index].Basepage = STMemory_ReadLong(act_pd);
 		snprintf(FileHandles[Index].szActualName,
@@ -2422,7 +2428,7 @@ static bool GemDOS_Write(uint32_t Params)
 		Regs[REG_D0] = nBytesWritten;      /* OK */
 	}
 	if (FileHandles[fh_idx].bReadOnly)
-		Log_Printf(LOG_WARN, "GEMDOS Fwrite() to '%s' file, opened as read-only\n",
+		Log_Printf(LOG_WARN, "GEMDOS Fwrite() to a read-only file '%s'\n",
 			   File_Basename(FileHandles[fh_idx].szActualName));
 	return true;
 }
@@ -3883,8 +3889,9 @@ void GemDOS_Info(FILE *fp, uint32_t bShowOpcodes)
 	{
 		if (!FileHandles[i].bUsed)
 			continue;
-		fprintf(fp, "- %d (0x%x): %s\n", i + BASE_FILEHANDLE,
-			FileHandles[i].Basepage, FileHandles[i].szActualName);
+		fprintf(fp, "- %d (0x%x): %s (%s)\n", i + BASE_FILEHANDLE,
+			FileHandles[i].Basepage, FileHandles[i].szActualName,
+			FileHandles[i].bReadOnly ? "ro" : "rw");
 		used++;
 	}
 	if (!used)