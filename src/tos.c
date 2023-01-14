hatari/hatari

commit 8d0a7ce74ff5185f1c6f7ffa5fd89454d075ccf8

Add "--country" option for NVRAM-less machines with EmuTOS
Added also option helper function for country codes now that there are
3 options needing it, and split TOS_ShowCountryCodes() off from
TOS_ParseCountryCode() for better option error reporting.

Noted in NVRAM layout option what are potential issues when either
that, or this new option changes TOS keyboard layout.

src/tos.c
@@ -30,6 +30,7 @@ const char TOS_fileid[] = "Hatari tos.c";
 #include "log.h"
 #include "m68000.h"
 #include "memorySnapShot.h"
+#include "nvram.h"
 #include "stMemory.h"
 #include "str.h"
 #include "tos.h"
@@ -1103,6 +1104,7 @@ int TOS_InitImage(void)
 {
 	uint8_t *pTosFile = NULL;
 	Uint32 logopatch_addr = 0;
+	Uint16 osconf, countrycode;
 
 	bTosImageLoaded = false;
 
@@ -1179,6 +1181,21 @@ int TOS_InitImage(void)
 		Log_Printf(LOG_DEBUG, "Skipped TOS patches.\n");
 	}
 
+	/* whether to override EmuTOS country code */
+	osconf = STMemory_ReadWord(TosAddress+0x1C);
+	countrycode = osconf >> 1;
+	if (bIsEmuTOS && countrycode == TOS_LANG_ALL && !NvRam_Present() &&
+	    ConfigureParams.Keyboard.nCountryCode != TOS_LANG_UNKNOWN)
+	{
+		countrycode = ConfigureParams.Keyboard.nLanguage;
+		/* low bit: us -> NTSC (0), any other -> PAL (1) */
+		osconf = (countrycode << 1) | (countrycode?1:0);
+		STMemory_WriteWord(TosAddress+0x1C, osconf);
+		Log_Printf(LOG_WARN, "=> EmuTOS country code: %d (%s), %s\n",
+			   countrycode, TOS_LanguageName(countrycode),
+			   (osconf & 1) ? "PAL" : "NTSC");
+	}
+
 	/*
 	 * patch some values into the "Draw logo" patch.
 	 * Needs to be called after final VDI resolution has been determined.
@@ -1252,20 +1269,19 @@ static const struct {
 /**
  * TOS_ValidCountryCode: returns parsed country code if
  * it's recognized, otherwise TOS_LANG_UNKNOWN is returned.
- * If info is set, valid ones are shown in latter case
  */
-int TOS_ParseCountryCode(const char *code, const char *info)
+int TOS_ParseCountryCode(const char *code)
 {
 	for (int i = 0; i < ARRAY_SIZE(countries); i++) {
 		if (strcmp(code, countries[i].code) == 0) {
 			return countries[i].value;
 		}
 	}
-	if (!info) {
-		return TOS_LANG_UNKNOWN;
-	}
-	fprintf(stderr, "Unrecognized %s code '%s'!\n", info, code);
+	return TOS_LANG_UNKNOWN;
+}
 
+void TOS_ShowCountryCodes(void)
+{
 	fprintf(stderr, "\nTOS v4 supports:\n");
 	for (int i = 0; i < ARRAY_SIZE(countries); i++) {
 		if (i == 7)
@@ -1273,7 +1289,6 @@ int TOS_ParseCountryCode(const char *code, const char *info)
 		fprintf(stderr, "- %s : %s\n",
 			countries[i].code, countries[i].name);
 	}
-	return TOS_LANG_UNKNOWN;
 }
 
 /**
@@ -1289,14 +1304,14 @@ int TOS_DefaultLanguage(void)
 
 	len = strlen(lang);
 	if (len == 2)
-		return TOS_ParseCountryCode(lang, NULL);
+		return TOS_ParseCountryCode(lang);
 
 	if (len >= 5 && lang[2] == '_') {
 		char cc[3];
 		cc[0] = tolower(lang[3]);
 		cc[1] = tolower(lang[4]);
 		cc[2] = '\0';
-		return TOS_ParseCountryCode(cc, NULL);
+		return TOS_ParseCountryCode(cc);
 	}
 	return TOS_LANG_UNKNOWN;
 }

doc/hatari.1
@@ -65,10 +65,29 @@ files
 Load keyboard mapping from <file>. "Symbolic" mapping will be used as
 fallback for keys not defined there
 .TP
+.B \-\-country <x>
+Set EmuTOS ROM country code on Mega/ST/STe machines lacking NVRAM,
+when EmuTOS indicates supporting multiple ones.
+
+In 512k EmuTOS images, country code selects the TOS keyboard layout
+and screen refresh (US = 60Hz NTSC, 50Hz PAL otherwise).  In 1024k
+EmuTOS images (coming with Hatari binaries and supporting multiple
+languages), country code selects also TOS language.
+
+Alternatively, one can use "tos-lang-change" tool from EmuTOS project
+to modify country code in the ROM image file itself. That works
+also for TOS v4
+.TP
 .B \-\-layout <x>
 Set NVRAM keyboard layout value. While both TT and Falcon machines
-have NVRAM, only TOS v4 and EmuTOS 1024k ROM versions support multiple
-layouts
+have NVRAM, only TOS v4 and EmuTOS 512k / 1024k ROM versions support
+multiple layouts.
+
+Regardless of whether keyboard layout change is done through the ROM
+country code or NVRAM setting, it may impact your key mappings in
+Hatari key mapping files, Hatari Python UI arguments, or key injection
+in your automation scripts for Hatari debugger, command FIFO or
+hconsole tool
 .TP
 .B \-\-language <x>
 Set NVRAM language value. While both TT and Falcon machines have

doc/manual.html
@@ -569,10 +569,30 @@ user configuration files
 <p class="paramdesc">load keyboard mapping from &lt;file&gt;.
 "Symbolic" mapping will be used as fallback for keys not defined there
 </p>
+<p class="parameter">--country &lt;x&gt;</p>
+<p class="paramdesc">
+Set EmuTOS ROM country code on Mega/ST/STe machines lacking NVRAM,
+when EmuTOS indicates supporting multiple ones.
+</p><p class="paramdesc">
+In 512k EmuTOS images, country code selects the TOS keyboard layout
+and screen refresh (US = 60Hz NTSC, 50Hz PAL otherwise).  In 1024k
+EmuTOS images (coming with Hatari binaries and supporting multiple
+languages), country code selects also TOS language.
+</p><p class="paramdesc">
+Alternatively, one can use "tos-lang-change" tool from EmuTOS project
+to modify country code in the ROM image file itself. That works
+also for TOS v4</p>
 <p class="parameter">--layout &lt;x&gt;</p>
 <p class="paramdesc">Set NVRAM keyboard layout value.
-While both TT and Falcon machines have NVRAM, only TOS v4 and EmuTOS
-1024k ROM versions support multiple layouts</p>
+Set NVRAM keyboard layout value. While both TT and Falcon machines
+have NVRAM, only TOS v4 and EmuTOS 512k / 1024k ROM versions support
+multiple layouts.</p>
+<p class="paramdesc">
+Regardless of whether keyboard layout change is done through the ROM
+country code or NVRAM setting, it may impact your key mappings in
+Hatari key mapping files, Hatari Python UI arguments, or key injection
+in your automation scripts for Hatari debugger, command FIFO or
+hconsole tool</p>
 <p class="parameter">--language &lt;x&gt;</p>
 <p class="paramdesc">Set NVRAM language value.
 While both TT and Falcon machines have NVRAM, only TOS v4 and EmuTOS

src/configuration.c
@@ -197,6 +197,7 @@ static const struct Config_Tag configs_Keyboard[] =
 {
 	{ "bDisableKeyRepeat", Bool_Tag, &ConfigureParams.Keyboard.bDisableKeyRepeat },
 	{ "nKeymapType", Int_Tag, &ConfigureParams.Keyboard.nKeymapType },
+	{ "nCountryCode", Int_Tag, &ConfigureParams.Keyboard.nCountryCode },
 	{ "nKbdLayout", Int_Tag, &ConfigureParams.Keyboard.nKbdLayout },
 	{ "nLanguage", Int_Tag, &ConfigureParams.Keyboard.nLanguage },
 	{ "szMappingFileName", String_Tag, ConfigureParams.Keyboard.szMappingFileName },
@@ -626,6 +627,7 @@ void Configuration_SetDefault(void)
 	/* Set defaults for Keyboard */
 	ConfigureParams.Keyboard.bDisableKeyRepeat = false;
 	ConfigureParams.Keyboard.nKeymapType = KEYMAP_SYMBOLIC;
+	ConfigureParams.Keyboard.nCountryCode = TOS_LANG_UNKNOWN;
 	ConfigureParams.Keyboard.nKbdLayout = TOS_LANG_UNKNOWN;
 	ConfigureParams.Keyboard.nLanguage = TOS_LANG_UNKNOWN;
 	strcpy(ConfigureParams.Keyboard.szMappingFileName, "");

src/falcon/nvram.h
@@ -49,4 +49,11 @@ extern void NvRam_Data_ReadByte(void);
 extern void NvRam_Data_WriteByte(void);
 extern void NvRam_Info(FILE *fp, Uint32 dummy);
 
+/* for tos.c */
+static inline bool NvRam_Present(void)
+{
+	return ConfigureParams.System.nMachineType == MACHINE_TT ||
+	       ConfigureParams.System.nMachineType == MACHINE_FALCON;
+}
+
 #endif /* HATARI_NVRAM_H */

src/includes/configuration.h
@@ -102,6 +102,7 @@ typedef struct
 {
   bool bDisableKeyRepeat;
   KEYMAPTYPE nKeymapType;
+  int nCountryCode;
   int nKbdLayout;
   int nLanguage;
   char szMappingFileName[FILENAME_MAX];

src/includes/tos.h
@@ -52,7 +52,8 @@ extern int TOS_InitImage(void);
 extern void TOS_SetTestPrgName(const char *testprg);
 
 extern int TOS_DefaultLanguage(void);
-extern int TOS_ParseCountryCode(const char *code, const char *info);
+extern int TOS_ParseCountryCode(const char *code);
+extern void TOS_ShowCountryCodes(void);
 extern const char *TOS_LanguageName(int code);
 
 #endif

src/options.c
@@ -65,6 +65,7 @@ enum {
 	OPT_CONFIRMQUIT,
 	OPT_CONFIGFILE,
 	OPT_KEYMAPFILE,
+	OPT_COUNTRY_CODE,
 	OPT_KBD_LAYOUT,
 	OPT_LANGUAGE,
 	OPT_FASTFORWARD,
@@ -228,10 +229,12 @@ static const opt_t HatariOptions[] = {
 	  "<file>", "Read (additional) configuration values from <file>" },
 	{ OPT_KEYMAPFILE, "-k", "--keymap",
 	  "<file>", "Read (additional) keyboard mappings from <file>" },
+	{ OPT_COUNTRY_CODE, NULL, "--country",
+	  "<x>", "Set country code for multi-code EmuTOS ROM" },
 	{ OPT_KBD_LAYOUT, NULL, "--layout",
-	  "<x>", "Set (TT/Falcon) NVRAM keyboard layout value" },
+	  "<x>", "Set (TT/Falcon) NVRAM keyboard layout" },
 	{ OPT_LANGUAGE, NULL, "--language",
-	  "<x>", "Set (TT/Falcon) NVRAM language value" },
+	  "<x>", "Set (TT/Falcon) NVRAM language" },
 	{ OPT_FASTFORWARD, NULL, "--fast-forward",
 	  "<bool>", "Help skipping stuff on fast machine" },
 	{ OPT_AUTOSTART, NULL, "--auto",
@@ -757,6 +760,23 @@ static bool Opt_Bool(const char *arg, int optid, bool *conf)
 	return Opt_ShowError(optid, orig, "Not a <bool> value");
 }
 
+/**
+ * If 'conf' given, set it to parsed country code value
+ * Return false for any other value, otherwise true
+ */
+static bool Opt_CountryCode(const char *arg, int optid, int *conf)
+{
+	int val = TOS_ParseCountryCode(arg);
+	if (val != TOS_LANG_UNKNOWN)
+	{
+		*conf = val;
+		return true;
+	}
+	Opt_ShowError(optid, arg, "Invalid value");
+	TOS_ShowCountryCodes();
+	return false;
+
+}
 
 /**
  * checks str argument against options of type "--option<digit>".
@@ -2018,20 +2038,16 @@ bool Opt_ParseParameters(int argc, const char * const argv[])
 			ok = Opt_Bool(argv[++i], OPT_MICROPHONE, &ConfigureParams.Sound.bEnableMicrophone);
 			break;
 
+		case OPT_COUNTRY_CODE:
+			ok = Opt_CountryCode(argv[++i], OPT_COUNTRY_CODE, &ConfigureParams.Keyboard.nCountryCode);
+			break;
+
 		case OPT_LANGUAGE:
-			val = TOS_ParseCountryCode(argv[++i], "language");
-			if (val != TOS_LANG_UNKNOWN)
-				ConfigureParams.Keyboard.nLanguage = val;
-			else
-				ok = false;
+			ok = Opt_CountryCode(argv[++i], OPT_LANGUAGE, &ConfigureParams.Keyboard.nLanguage);
 			break;
 
 		case OPT_KBD_LAYOUT:
-			val = TOS_ParseCountryCode(argv[++i], "keyboard layout");
-			if (val != TOS_LANG_UNKNOWN)
-				ConfigureParams.Keyboard.nKbdLayout = val;
-			else
-				ok = false;
+			ok = Opt_CountryCode(argv[++i], OPT_KBD_LAYOUT, &ConfigureParams.Keyboard.nKbdLayout);
 			break;
 
 		case OPT_KEYMAPFILE:

commit a59995ac376067a57dcbdc801fd226a48f9461ca

Fix: use country code setting to set TOS country code
Fixes: 8d0a7ce74ff5185f1c6f7ffa5fd89454d075ccf8

src/tos.c
@@ -1184,7 +1184,7 @@ int TOS_InitImage(void)
 	if (bIsEmuTOS && countrycode == TOS_LANG_ALL && !NvRam_Present() &&
 	    ConfigureParams.Keyboard.nCountryCode != TOS_LANG_UNKNOWN)
 	{
-		countrycode = ConfigureParams.Keyboard.nLanguage;
+		countrycode = ConfigureParams.Keyboard.nCountryCode;
 		/* low bit: us -> NTSC (0), any other -> PAL (1) */
 		osconf = (countrycode << 1) | (countrycode?1:0);
 		STMemory_WriteWord(TosAddress+0x1C, osconf);