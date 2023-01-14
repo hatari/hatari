hatari/hatari

commit 62331b8e592c2d8dac8117d203bcfb9ab257c9a6

Add debug symbols to WinUAE CPU core disassembly
WinUAE CPU core appends provided symbol name (associated to given
instruction memory address) to its disassembly output.

src/cpu/disasm.c
@@ -289,16 +289,17 @@ static void showea_val(TCHAR *buffer, uae_u16 opcode, uaecptr addr, int size)
 		}
 	}
 skip:
-#ifndef WINUAE_FOR_HATARI
 	for (int i = 0; i < size; i++) {
+#ifndef WINUAE_FOR_HATARI
 		TCHAR name[256];
 		if (debugmem_get_symbol(addr + i, name, sizeof(name) / sizeof(TCHAR))) {
+#else
+		const char *name;
+		if ((name = Symbols_GetByCpuAddress(addr + i, SYMTYPE_TEXT))) {
+#endif
 			_stprintf(buffer + _tcslen(buffer), _T(" %s"), name);
 		}
 	}
-#else
-	return;
-#endif
 }
 
 uaecptr ShowEA_disp(uaecptr *pcp, uaecptr base, TCHAR *buffer, const TCHAR *name, bool pcrel)

src/cpu/debugmem.h
@@ -5,6 +5,8 @@
 /* We use some #define / static inline to replace with 'empty' equivalent at compile time */
 /* and to avoid useless overhead at runtime */
 
+#include "symbols.h"  // use functions from here instead */
+
 #define	debugmem_trace	0
 
 static inline int debugmem_get_segment(uaecptr addr, bool *exact, bool *ext, TCHAR *out, TCHAR *name)

commit 8f8752e25542d693695d81b3dfc7bec0682bc1b5

Fix: CPU disassembler crashes with longer symbols
(This is more of a workaround.  I think whole WinUAE CPU core
disassembler code would need refactoring to pass around the buffer
sizes, and check them.)

Reported-By: Miro Kropáček
Fixes: 62331b8e592c2d8dac8

src/cpu/disasm.c
@@ -18,6 +18,8 @@ int disasm_min_words = 5;
 int disasm_max_words = 16;
 TCHAR disasm_hexprefix[3] = { '$', 0 };
 
+#define LINE_BUF_SIZE 80
+
 static TCHAR disasm_areg, disasm_dreg, disasm_byte, disasm_word, disasm_long;
 static TCHAR disasm_pcreg[3], disasm_fpreg[3];
 static bool absshort_long = false;
@@ -299,11 +301,13 @@ skip:
 #ifndef WINUAE_FOR_HATARI
 		TCHAR name[256];
 		if (debugmem_get_symbol(addr + i, name, sizeof(name) / sizeof(TCHAR))) {
+			_stprintf(buffer + _tcslen(buffer), _T(" %s"), name);
 #else
 		const char *name;
 		if ((name = Symbols_GetByCpuAddress(addr + i, SYMTYPE_TEXT))) {
+			int len = _tcslen(buffer);
+			snprintf(buffer + len, LINE_BUF_SIZE - len, _T(" %s"), name);
 #endif
-			_stprintf(buffer + _tcslen(buffer), _T(" %s"), name);
 		}
 	}
 }
@@ -508,7 +512,7 @@ uaecptr ShowEA(void *f, uaecptr pc, uae_u16 opcode, int reg, amodes mode, wordsi
 	uaecptr addr = pc;
 	uae_s16 disp16;
 	uae_s32 offset = 0;
-	TCHAR buffer[80];
+	TCHAR buffer[LINE_BUF_SIZE];
 
 	if (actualea)
 		*actualea = 1;