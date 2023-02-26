d0dba8a504e8b46d9087477dcc602329267940db
Nicolas Pomarède <npomarede@corp.free.fr>
2023-02-26 17:50:14 +0100

    Warp mode boot + Statefile accurate blitter and event support (from WinUAE 4.10.1+ 2023/02/10)

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -708,11 +708,11 @@ static void cputracefunc_x_do_cycles(int cycles)
 	while (cycles >= CYCLE_UNIT) {
 		cputrace.cyclecounter += CYCLE_UNIT;
 		cycles -= CYCLE_UNIT;
-		x2_do_cycles (CYCLE_UNIT);
+		x2_do_cycles(CYCLE_UNIT);
 	}
 	if (cycles > 0) {
 		cputrace.cyclecounter += cycles;
-		x2_do_cycles (cycles);
+		x2_do_cycles(cycles);
 	}
 }
 
@@ -724,10 +724,10 @@ static void cputracefunc2_x_do_cycles(int cycles)
 	}
 	cycles -= cputrace.cyclecounter;
 	cputrace.cyclecounter = 0;
-	check_trace ();
+	check_trace();
 	x_do_cycles = x2_do_cycles;
 	if (cycles > 0)
-		x_do_cycles (cycles);
+		x_do_cycles(cycles);
 }
 
 static void cputracefunc_x_do_cycles_pre(int cycles)
@@ -737,10 +737,10 @@ static void cputracefunc_x_do_cycles_pre(int cycles)
 	while (cycles >= CYCLE_UNIT) {
 		cycles -= CYCLE_UNIT;
 		cputrace.cyclecounter_pre += CYCLE_UNIT;
-		x2_do_cycles (CYCLE_UNIT);
+		x2_do_cycles_pre(CYCLE_UNIT);
 	}
 	if (cycles > 0) {
-		x2_do_cycles (cycles);
+		x2_do_cycles_pre(cycles);
 		cputrace.cyclecounter_pre += cycles;
 	}
 	cputrace.cyclecounter_pre = 0;
@@ -767,12 +767,13 @@ static void cputracefunc2_x_do_cycles_pre (int cycles)
 		x_do_cycles (cycles);
 }
 
-static void cputracefunc_x_do_cycles_post (int cycles, uae_u32 v)
+static void cputracefunc_x_do_cycles_post(int cycles, uae_u32 v)
 {
 	if (cputrace.memoryoffset < 1) {
 #if CPUTRACE_DEBUG
 		write_log (_T("cputracefunc_x_do_cycles_post memoryoffset=%d!\n"), cputrace.memoryoffset);
 #endif
+		x2_do_cycles_post(cycles, v);
 		return;
 	}
 	struct cputracememory *ctm = &cputrace.ctm[cputrace.memoryoffset - 1];
@@ -782,11 +783,11 @@ static void cputracefunc_x_do_cycles_post (int cycles, uae_u32 v)
 	while (cycles >= CYCLE_UNIT) {
 		cycles -= CYCLE_UNIT;
 		cputrace.cyclecounter_post -= CYCLE_UNIT;
-		x2_do_cycles (CYCLE_UNIT);
+		x2_do_cycles_post(CYCLE_UNIT, v);
 	}
 	if (cycles > 0) {
 		cputrace.cyclecounter_post -= cycles;
-		x2_do_cycles (cycles);
+		x2_do_cycles_post(CYCLE_UNIT, v);
 	}
 	cputrace.cyclecounter_post = 0;
 }
@@ -7687,6 +7688,13 @@ void m68k_go (int may_quit)
 			custom_prepare ();
 			mman_set_barriers(false);
 			protect_roms (true);
+		}
+		if ((cpu_keyboardreset || hardboot) && !restored) {
+			if (currprefs.turbo_boot) {
+				warpmode(1);
+				currprefs.turbo_emulation = changed_prefs.turbo_emulation = 2;
+
+			}
 		}
 		cpu_hardreset = false;
 		cpu_keyboardreset = false;
