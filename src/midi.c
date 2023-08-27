hatari/hatari

commit 957d7db465970d1e34fde4c17e067c2009b32b57

Show PortMidi device prefix matches also in GUI
Midi functionality already did prefix matching on devices and opened
them, but it did not update the config value to full MIDI device name.
This was to avoid breaking prefix matching for devices which names
(e.g. port) changes frequently.

With this change, same matching is done for names shown in GUI, to
avoid confusing users.  Downside of this is that visiting devices
dialog WILL replace the prefix in config setting with a full device
name.  I.e. saving settings afterwards, will override the prefix
in config file.

src/midi.c
@@ -425,7 +425,7 @@ static void Midi_Host_Close(void)
  * with given offset and direction.
  *
  * Offset interpretation:
- *   0: return matching device name
+ *   0: return matching device name, with prefix match as fallback
  *  <0: return name of device before matching one
  *  >0: return name of device after matching one
  *
@@ -436,8 +436,9 @@ const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 {
 	const PmDeviceInfo* info;
 	const char *prev = NULL;
+	const char *prefixmatch = NULL;
 	bool prev_matched = false;
-	int i, count;
+	int i, count, len = strlen(name);
 
 	// -- find port with given offset from named one
 	count = Pm_CountDevices();
@@ -456,6 +457,7 @@ const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 				return NULL;
 			return info->name;
 		}
+		/* matches */
 		if (!strcmp(info->name, name))
 		{
 			if (!offset)
@@ -468,7 +470,11 @@ const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 		if (prev_matched)
 			return info->name;
 		prev = info->name;
+		if (!strncmp(info->name, name, len))
+			prefixmatch = info->name;
 	}
+	if (!offset && prefixmatch)
+		return prefixmatch;
 	return NULL;
 }
 

doc/manual.html
@@ -2009,7 +2009,9 @@ PS. For MIDI devices which names are not constant, but include e.g.
 instance identifier at end, Hatari supports matching device by first
 part of the name when there's no full match.  To use this, remove the
 device identifier part from the end of the device name in saved Hatari
-configuration file.
+configuration file.  Note that using GUI Devices dialog afterwards,
+will override prefix in config with a full name of a device (that
+matched the prefix at that point).
 </p>
 
 

commit 373fb663772299a862806d0b873c555f5ba41a0e

Fix Midi_Host_GetPortName() crash
Fix (Mac regression) crash when Midi_Host_GetPortName() is called with
NULL, and return first port name also when called with empty string
(using positive name offset).

Reported-by: Chris Jenkins
Fixes: 957d7db465970d1e

src/midi.c
@@ -429,8 +429,9 @@ static void Midi_Host_Close(void)
  *  <0: return name of device before matching one
  *  >0: return name of device after matching one
  *
- * As special case, for NULL name with positive offset,
- * name of the first port in correct direction is returned.
+ * As special case, for NULL/empty name with positive offset
+ * (i.e. before any port has been selected for the first time),
+ * name of the first port in that direction is returned.
  */
 const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 {
@@ -438,7 +439,9 @@ const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 	const char *prev = NULL;
 	const char *prefixmatch = NULL;
 	bool prev_matched = false;
-	int i, count, len = strlen(name);
+	int i, count, len;
+
+	len = name ? strlen(name) : 0;
 
 	// -- find port with given offset from named one
 	count = Pm_CountDevices();
@@ -451,7 +454,7 @@ const char* Midi_Host_GetPortName(const char *name, int offset, bool forInput)
 			continue;
 		if (!forInput && info->input)
 			continue;
-		if (!name)
+		if (len == 0)
 		{
 			if (offset <= 0)
 				return NULL;