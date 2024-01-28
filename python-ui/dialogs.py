hatari/hatari

commit 5eff40495db9ff92a2dbcb11c6cfb3297329eb06

hatariui.py : update parameters' name for SCC A / SCC A Lan / SCC B in [RS232] config

python-ui/hatari.py
@@ -274,16 +274,20 @@ class Hatari:
 # it needs actual method for (each) setting.
 class HatariConfigMapping(ConfigStore):
     _paths = {
-        "memauto": ("[Memory]", "szAutoSaveFileName", "Automatic memory snapshot"),
-        "memsave": ("[Memory]", "szMemoryCaptureFileName", "Manual memory snapshot"),
-        "midiin":  ("[Midi]", "sMidiInFileName", "Midi input"),
-        "midiout": ("[Midi]", "sMidiOutFileName", "Midi output"),
-        "rs232in": ("[RS232]", "szInFileName", "RS232 (MFP) I/O input"),
-        "rs232out":("[RS232]", "szOutFileName", "RS232 (MFP) I/O output"),
-#        "sccbin":  ("[RS232]", "sSccBInFileName", "RS232 (SCC-B) I/O input"),
-        "sccbout": ("[RS232]", "sSccBOutFileName", "RS232 (SCC-B) I/O output"),
-        "printout":("[Printer]", "szPrintToFileName", "Printer output"),
-        "soundout":("[Sound]", "szYMCaptureFileName", "Sound output")
+        "memauto":    ("[Memory]", "szAutoSaveFileName", "Automatic memory snapshot"),
+        "memsave":    ("[Memory]", "szMemoryCaptureFileName", "Manual memory snapshot"),
+        "midiin":     ("[Midi]", "sMidiInFileName", "Midi input"),
+        "midiout":    ("[Midi]", "sMidiOutFileName", "Midi output"),
+        "rs232in":    ("[RS232]", "szInFileName", "RS232 (MFP) I/O input"),
+        "rs232out":   ("[RS232]", "szOutFileName", "RS232 (MFP) I/O output"),
+        "sccain":     ("[RS232]", "SccAInFileName", "RS232 (SCC-A) I/O input"),
+        "sccaout":    ("[RS232]", "SccAOutFileName", "RS232 (SCC-A) I/O output"),
+        "sccalanin":  ("[RS232]", "SccAInFileName", "RS232 (SCC-A Lan) I/O input"),
+        "sccalanout": ("[RS232]", "SccAOutFileName", "RS232 (SCC-A Lan) I/O output"),
+        "sccbin":     ("[RS232]", "SccBInFileName", "RS232 (SCC-B) I/O input"),
+        "sccbout":    ("[RS232]", "SccBOutFileName", "RS232 (SCC-B) I/O output"),
+        "printout":   ("[Printer]", "szPrintToFileName", "Printer output"),
+        "soundout":   ("[Sound]", "szYMCaptureFileName", "Sound output")
     }
     has_hd_sections = True # from v2.2 onwards separate ACSI/SCSI/IDE sections
     has_modeltype = True   # from v2.0 onwards
@@ -396,11 +400,25 @@ class HatariConfigMapping(ConfigStore):
         self.set("[RS232]", "bEnableRS232", value)
         self._hatari.set_device("rs232", value)
 
+    def get_scca(self):
+        return self.get("[RS232]", "EnableSccA")
+
+    def set_scca(self, value):
+        self.set("[RS232]", "EnableSccA", value)
+        self._hatari.set_device("scca", value)
+
+    def get_scca_lan(self):
+        return self.get("[RS232]", "EnableSccALan")
+
+    def set_scca_lan(self, value):
+        self.set("[RS232]", "EnableSccALan", value)
+        self._hatari.set_device("scca lan", value)
+
     def get_sccb(self):
-        return self.get("[RS232]", "bEnableSccB")
+        return self.get("[RS232]", "EnableSccB")
 
     def set_sccb(self, value):
-        self.set("[RS232]", "bEnableSccB", value)
+        self.set("[RS232]", "EnableSccB", value)
         self._hatari.set_device("sccb", value)
 
     # ------------ machine ---------------

commit e4f54e41730955f777b1a920b370496b3e0e9f15

Python UI: Add SCC-A support to GUI
- Add SCC-A enabling options
- Fix Hatari remote API "sccalan" command name
- Sort paths dialog entries by label instead of Hatari command name,
  and change path labels so that they start with the device type i.e.
  get ordered nicer in the dialog

Fixes: 5eff40495db

python-ui/dialogs.py
@@ -1,7 +1,7 @@
 #
 # Classes for the Hatari UI dialogs
 #
-# Copyright (C) 2008-2023 by Eero Tamminen
+# Copyright (C) 2008-2024 by Eero Tamminen
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -587,10 +587,16 @@ class PeripheralDialog(HatariUIDialog):
         printer = Gtk.CheckButton("Enable printer output")
         printer.set_active(config.get_printer())
 
-        rs232 = Gtk.CheckButton("Enable MFP RS232 (ST/MegaST/STE/MegaSTE/TT)")
+        rs232 = Gtk.CheckButton("Enable RS232/MFP (!Falcon)")
         rs232.set_active(config.get_rs232())
 
-        sccb = Gtk.CheckButton("Enable SCC-B RS232 output (MegaSTE/TT/Falcon")
+        scca = Gtk.CheckButton("Enable RS232/SCC-A output (MegaSTE/TT/Falcon")
+        scca.set_active(config.get_scca())
+
+        scca_lan = Gtk.CheckButton("Enable RS232/SCC-A Lan output (MegaSTE/TT/Falcon")
+        scca_lan.set_active(config.get_scca_lan())
+
+        sccb = Gtk.CheckButton("Enable RS232/SCC-B output (MegaSTE/TT/Falcon")
         sccb.set_active(config.get_sccb())
 
         dialog = Gtk.Dialog("Peripherals", self.parent,
@@ -600,12 +606,16 @@ class PeripheralDialog(HatariUIDialog):
         dialog.vbox.add(midi)
         dialog.vbox.add(printer)
         dialog.vbox.add(rs232)
+        dialog.vbox.add(scca)
+        dialog.vbox.add(scca_lan)
         dialog.vbox.add(sccb)
         dialog.vbox.show_all()
 
         self.dialog = dialog
         self.printer = printer
         self.rs232 = rs232
+        self.scca = scca
+        self.scca_lan = scca_lan
         self.sccb = sccb
         self.midi = midi
 
@@ -621,6 +631,8 @@ class PeripheralDialog(HatariUIDialog):
             config.set_midi(self.midi.get_active())
             config.set_printer(self.printer.get_active())
             config.set_rs232(self.rs232.get_active())
+            config.set_scca(self.scca.get_active())
+            config.set_scca_lan(self.scca_lan.get_active())
             config.set_sccb(self.sccb.get_active())
             config.flush_updates()
 
@@ -632,9 +644,10 @@ class PathDialog(HatariUIDialog):
     def _create_dialog(self, config):
         paths = config.get_paths()
         table, self.dialog = create_table_dialog(self.parent, "File path settings", len(paths), 2)
-        paths.sort()
         row = 0
         self.paths = []
+        # sort by path label
+        paths.sort(key=lambda info: info[2])
         for (key, path, label) in paths:
             label = "%s:" % label
             fsel = FselEntry(self.dialog, title=label, validate=self._validate_fname, data=key)

python-ui/hatari.py
@@ -274,18 +274,18 @@ class Hatari:
 # it needs actual method for (each) setting.
 class HatariConfigMapping(ConfigStore):
     _paths = {
-        "memauto":    ("[Memory]", "szAutoSaveFileName", "Automatic memory snapshot"),
-        "memsave":    ("[Memory]", "szMemoryCaptureFileName", "Manual memory snapshot"),
+        "memauto":    ("[Memory]", "szAutoSaveFileName", "Memory snapshot, automatic"),
+        "memsave":    ("[Memory]", "szMemoryCaptureFileName", "Memory snapshot, manual"),
         "midiin":     ("[Midi]", "sMidiInFileName", "Midi input"),
         "midiout":    ("[Midi]", "sMidiOutFileName", "Midi output"),
-        "rs232in":    ("[RS232]", "szInFileName", "RS232 (MFP) I/O input"),
-        "rs232out":   ("[RS232]", "szOutFileName", "RS232 (MFP) I/O output"),
-        "sccain":     ("[RS232]", "SccAInFileName", "RS232 (SCC-A) I/O input"),
-        "sccaout":    ("[RS232]", "SccAOutFileName", "RS232 (SCC-A) I/O output"),
-        "sccalanin":  ("[RS232]", "SccAInFileName", "RS232 (SCC-A Lan) I/O input"),
-        "sccalanout": ("[RS232]", "SccAOutFileName", "RS232 (SCC-A Lan) I/O output"),
-        "sccbin":     ("[RS232]", "SccBInFileName", "RS232 (SCC-B) I/O input"),
-        "sccbout":    ("[RS232]", "SccBOutFileName", "RS232 (SCC-B) I/O output"),
+        "rs232in":    ("[RS232]", "szInFileName", "RS232: MFP IO input"),
+        "rs232out":   ("[RS232]", "szOutFileName", "RS232: MFP IO output"),
+        "sccain":     ("[RS232]", "SccAInFileName", "RS232: SCC-A IO input"),
+        "sccaout":    ("[RS232]", "SccAOutFileName", "RS232: SCC-A IO output"),
+        "sccalanin":  ("[RS232]", "SccAInFileName", "RS232: SCC-A Lan IO input"),
+        "sccalanout": ("[RS232]", "SccAOutFileName", "RS232: SCC-A Lan IO output"),
+        "sccbin":     ("[RS232]", "SccBInFileName", "RS232: SCC-B IO input"),
+        "sccbout":    ("[RS232]", "SccBOutFileName", "RS232: SCC-B IO output"),
         "printout":   ("[Printer]", "szPrintToFileName", "Printer output"),
         "soundout":   ("[Sound]", "szYMCaptureFileName", "Sound output")
     }
@@ -412,7 +412,7 @@ class HatariConfigMapping(ConfigStore):
 
     def set_scca_lan(self, value):
         self.set("[RS232]", "EnableSccALan", value)
-        self._hatari.set_device("scca lan", value)
+        self._hatari.set_device("sccalan", value)
 
     def get_sccb(self):
         return self.get("[RS232]", "EnableSccB")