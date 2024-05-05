hatari/hatari

commit ea6cbab756758a0188976a22559da1729ff071b6

Fix: debugger memdump command count argument handling
* Avoid infinite looping by rejecting negative counts
* Show only requested number of items, instead of
  rounding number of items to next MEMDUMP_COLS
  (reported by J. Young)

src/debug/debugcpu.c
@@ -506,7 +506,7 @@ int DebugCpu_MemDump(int nArgc, char *psArgs[])
 		if (nArgc > arg)
 		{
 			int count = atoi(psArgs[arg]);
-			if (!count)
+			if (count < 1)
 			{
 				fprintf(stderr, "Invalid count %d!\n", count);
 				return DEBUGGER_CMDDONE;
@@ -523,10 +523,13 @@ int DebugCpu_MemDump(int nArgc, char *psArgs[])
 
 	while (memdump_addr < memdump_upper)
 	{
-		fprintf(debugOutput, "%08X: ", memdump_addr);
+		int cols;
+		uint32_t memdump_line = memdump_addr;
+		fprintf(debugOutput, "%08X: ", memdump_line);
 		
 		/* print HEX data */
-		for (i = 0; i < MEMDUMP_COLS/size; i++)
+		cols = MEMDUMP_COLS/size;
+		for (i = 0; i < cols && memdump_addr < memdump_upper; i++)
 		{
 			switch (mode)
 			{
@@ -547,9 +550,11 @@ int DebugCpu_MemDump(int nArgc, char *psArgs[])
 
 		/* print ASCII data */
 		fprintf(debugOutput, "  ");
-		for (i = 0; i < MEMDUMP_COLS; i++)
+
+		cols = i*size;
+		for (i = 0; i < cols; i++)
 		{
-			c = STMemory_ReadByte(memdump_addr-MEMDUMP_COLS+i);
+			c = STMemory_ReadByte(memdump_line + i);
 			if(!isprint((unsigned)c))
 				c = NON_PRINT_CHAR;             /* non-printable as dots */
 			fprintf(debugOutput,"%c", c);

commit b6f8d4301d646b2b82f45e9a31e11f309120389a

Always align memdump ASCII columns
Fixes: ea6cbab756

src/debug/debugcpu.c
@@ -574,7 +574,7 @@ int DebugCpu_MemDump(int nArgc, char *psArgs[])
 
 	while (memdump_addr < memdump_upper)
 	{
-		uint8_t all, cols;
+		uint8_t all, cols, align;
 		uint32_t memdump_line = memdump_addr;
 
 		/* how many colums to print */
@@ -589,7 +589,8 @@ int DebugCpu_MemDump(int nArgc, char *psArgs[])
 		print_mem_hex(memdump_addr, cols, size);
 
 		/* print ASCII data */
-		fprintf(debugOutput, " ");
+		align = (all-cols)*(2*size+1);
+		fprintf(debugOutput, "%*c", align + 2, ' ');
 		print_mem_ascii(memdump_line, cols*size);
 		fprintf(debugOutput, "\n");
 