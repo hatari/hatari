hatari/hatari

commit bd8a07aef850a0ff2abfd588fef5a1e07f464100

Ignore first key-up event in sdl gui in case the key had been pressed before
Otherwise we could exit the dialog immediately though the key was not
meant for controlling the dialog, but rather for the emulation side.

src/gui-sdl/sdlgui.c
@@ -1123,6 +1123,8 @@ int SDLGui_DoDialogExt(SGOBJ *dlg, bool (*isEventOut)(SDL_EventType), SDL_Event 
 	SDL_Surface *pBgSurface;
 	SDL_Rect dlgrect, bgrect;
 	SDL_Joystick *joy = NULL;
+	const Uint8 *keystates;
+	bool ignore_first_keyup;
 
 	/* either both, or neither of these should be present */
 	assert((isEventOut && pEventOut) || (!isEventOut && !pEventOut));
@@ -1176,6 +1178,16 @@ int SDLGui_DoDialogExt(SGOBJ *dlg, bool (*isEventOut)(SDL_EventType), SDL_Event 
 
 	/* (Re-)draw the dialog */
 	SDLGui_DrawDialog(dlg);
+
+	/* If one of the keys that could exit the dialog is already held
+	 * before we start, then ignore the first keyup event since the
+	 * key press does not belong to the dialog, but rather to whatever
+	 * happened before the dialog */
+	keystates = SDL_GetKeyboardState(NULL);
+	ignore_first_keyup = keystates[SDL_SCANCODE_RETURN]
+	                     || keystates[SDL_SCANCODE_KP_ENTER]
+	                     || keystates[SDL_SCANCODE_SPACE]
+	                     || keystates[SDL_SCANCODE_ESCAPE];
 
 	/* Is the left mouse button still pressed? Yes -> Handle TOUCHEXIT objects here */
 	SDL_PumpEvents();
@@ -1401,9 +1413,19 @@ int SDLGui_DoDialogExt(SGOBJ *dlg, bool (*isEventOut)(SDL_EventType), SDL_Event 
 				 case SDLK_SPACE:
 				 case SDLK_RETURN:
 				 case SDLK_KP_ENTER:
+					if (ignore_first_keyup)
+					{
+						ignore_first_keyup = false;
+						break;
+					}
 					retbutton = SDLGui_HandleSelection(dlg, focused, focused);
 					break;
 				 case SDLK_ESCAPE:
+					if (ignore_first_keyup)
+					{
+						ignore_first_keyup = false;
+						break;
+					}
 					retbutton = SDLGui_SearchFlags(dlg, SG_CANCEL);
 					break;
 				 default:

commit e1ed88b389b96519cce67b471dbdbacb933484d5

Fix: regression for exiting fileselector using keyboard
Fixes: bd8a07aef850

Which caused exit keys NOT to work with fileselector
(almost always i.e. randomly from users point of view).

Disable that corner-case fix when event function exits already on
first keydown, like file selector does, so that user can still exit
such dialogs using keyboard.

src/gui-sdl/sdlgui.c
@@ -1182,12 +1182,18 @@ int SDLGui_DoDialogExt(SGOBJ *dlg, bool (*isEventOut)(SDL_EventType), SDL_Event 
 	/* If one of the keys that could exit the dialog is already held
 	 * before we start, then ignore the first keyup event since the
 	 * key press does not belong to the dialog, but rather to whatever
-	 * happened before the dialog */
+	 * happened before the dialog.
+	 *
+	 * Cannot be used when asked to return already on key down
+	 * (e.g. with file selector).
+	 */
 	keystates = SDL_GetKeyboardState(NULL);
-	ignore_first_keyup = keystates[SDL_GetScancodeFromKey(SDLK_RETURN)] ||
+	ignore_first_keyup = !(isEventOut && isEventOut(SDL_KEYDOWN)) && (
+	                     keystates[SDL_GetScancodeFromKey(SDLK_RETURN)] ||
 	                     keystates[SDL_GetScancodeFromKey(SDLK_KP_ENTER)] ||
 	                     keystates[SDL_GetScancodeFromKey(SDLK_SPACE)] ||
-	                     keystates[SDL_GetScancodeFromKey(SDLK_ESCAPE)];
+	                     keystates[SDL_GetScancodeFromKey(SDLK_ESCAPE)]
+	);
 
 	/* Is the left mouse button still pressed? Yes -> Handle TOUCHEXIT objects here */
 	SDL_PumpEvents();