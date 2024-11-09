2fe127859ebc1d575e0362972eb1731b35c208b3
Nicolas Pomarede <npomarede@corp.free.fr>
2024-11-09 14:58:33 +0100

    Fix warnings with cmake >= 3.31 regarding policy CMP0175 and DEPENDS option CMP0175 specifies: * The ``TARGET`` form does not accept ``DEPENDS``, ``DEPENDS_EXPLICIT_ONLY``,   ``DEPFILE``, ``IMPLICIT_DEPENDS``, ``MAIN_DEPENDENCY``, ``JOB_POOL``,   ``JOB_SERVER_AWARE``, or ``USES_TERMINAL`` keywords.

--- cmake/DistClean.cmake
+++ cmake/DistClean.cmake
@@ -13,15 +13,13 @@ if(UNIX)
 			src/*cpu/cpudefs.c src/*cpu/cpuemu*.c
 			src/*cpu/cpustbl.c src/*cpu/cputbl.h)
 		add_custom_command(TARGET distclean POST_BUILD
-			COMMAND rm -f ${CLEAN_FILE}
-			DEPENDS clean)
+			COMMAND rm -f ${CLEAN_FILE})
 	endforeach(CLEAN_FILE)
 	# Clean up files that can appear at multiple places:
 	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt cmake_install.cmake
 			CTestTestfile.cmake Makefile Testing
 			'*.a' '*.1.gz' '*.pyc')
 		add_custom_command(TARGET distclean POST_BUILD
-			COMMAND find . -depth -name ${CLEAN_FILE} | xargs rm -rf
-			DEPENDS clean)
+			COMMAND find . -depth -name ${CLEAN_FILE} | xargs rm -rf)
 	endforeach(CLEAN_FILE)
 endif(UNIX)
