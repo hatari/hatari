cb95026b28ea525adb3155f138d0aca967e395e5
Nicolas Pomarede <npomarede@corp.free.fr>
2025-01-07 23:07:13 +0100

    Sync misc changes (no change for HAtari) (from WinUAE 6.0.0 beta1 2025/01/04)

--- src/cpu/newcpu.h
+++ src/cpu/newcpu.h
@@ -341,7 +341,6 @@ extern bool cpu_bus_rmw;			// WINUAE_FOR_HATARI
 extern void safe_interrupt_set(int, int, bool);
 
 #define SPCFLAG_CPUINRESET 2
-//#define SPCFLAG_COPPER 4
 #define SPCFLAG_INT 8
 #define SPCFLAG_BRK 16
 #define SPCFLAG_UAEINT 32
@@ -789,7 +788,6 @@ extern void m68k_dumpstate(uaecptr *, uaecptr);
 extern void m68k_dumpstate_file (FILE *f, uaecptr *nextpc, uaecptr prevpc);
 #endif
 extern void m68k_dumpcache(bool);
-extern bool m68k_readcache(uaecptr memaddr, bool dc, uae_u32* valp);
 extern int getMulu68kCycles(uae_u16 src);
 extern int getMuls68kCycles(uae_u16 src);
 extern int getDivu68kCycles (uae_u32 dividend, uae_u16 divisor);
