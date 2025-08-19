hatari/hatari

commit 7d091a95becd46ee5f4f2f9a8751b5a671b20f38

Open Windows console automatically for Debugger

src/debug/debugui.c
@@ -1231,6 +1231,10 @@ void DebugUI(debug_reason_t reason)
 
 	if (welcome)
 	{
+#ifdef WIN32
+		/* in case user forgot -W option */
+		Win_ForceCon();
+#endif
 		fputs(welcome, stderr);
 		welcome = NULL;
 	}

src/gui-win/opencon.c
@@ -24,10 +24,15 @@ static void Win_OpenInternal(void)
 		return;
 	opened = true;
 
-	AllocConsole();
-	freopen("CON", "w", stdout);
-	freopen("CON", "r", stdin);
-	freopen("CON", "w", stderr);
+	/* Fails if process already has a console, returns true on success:
+	 * https://learn.microsoft.com/en-us/windows/console/allocconsole
+	 */
+	if (AllocConsole())
+	{
+		freopen("CON", "w", stdout);
+		freopen("CON", "r", stdin);
+		freopen("CON", "w", stderr);
+	}
 }
 
 void Win_OpenCon(void)

commit 4c2a562b8192fcec15cd75b9d56a8b9b9236f000

Fix MinGW build (warning)
Fixes: 7d091a95becd46

src/debug/debugui.c
@@ -48,6 +48,9 @@ const char DebugUI_fileid[] = "Hatari debugui.c";
 #include "profile.h"
 #include "symbols.h"
 #include "vars.h"
+#ifdef WIN32
+#include "gui-win/opencon.h"
+#endif
 
 FILE *debugOutput;
 