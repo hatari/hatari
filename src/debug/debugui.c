hatari/hatari

commit 4c2a562b8192fcec15cd75b9d56a8b9b9236f000

Fix MinGW build (warning)
Fixes: 7d091a95becd46

src/debug/debugui.c
@@ -48,6 +48,9 @@ const char DebugUI_fileid[] = "Hatari debugui.c";
 #include "profile.h"
 #include "symbols.h"
 #include "vars.h"
+#ifdef WIN32
+#include "gui-win/opencon.h"
+#endif
 
 FILE *debugOutput;
 

commit 7faf7131da4e679fac3935d5796c43b0725861fe

Really fix MinGW build
Fixes: 4c2a562b8192fcec

(Which turned build warning into an error.)

src/debug/debugui.c
@@ -49,7 +49,7 @@ const char DebugUI_fileid[] = "Hatari debugui.c";
 #include "symbols.h"
 #include "vars.h"
 #ifdef WIN32
-#include "gui-win/opencon.h"
+#include "../gui-win/opencon.h"
 #endif
 
 FILE *debugOutput;