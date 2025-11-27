hatari/hatari

commit 30c708af1edf245e3a57dc92f2f1e99800ce77ff

Events: fix build when tracing support is disabled
Fixes: f2b6bd0c78f840

src/debug/log.c
@@ -619,6 +619,8 @@ const char* Log_SetExceptionDebugMask (const char *FlagsStr)
 	return errstr;
 }
 
+#if ENABLE_TRACING
+
 /**
  * Parse/check specified trace flags.
  * Return error string or NULL for success.
@@ -628,8 +630,6 @@ const char* Log_CheckTraceOptions (const char *FlagsStr)
 	uint64_t flags = LogTraceFlags;
 	return Log_ParseOptionFlags(FlagsStr, TraceFlags, ARRAY_SIZE(TraceFlags), &flags);
 }
-
-#if ENABLE_TRACING
 
 /**
  * Parse trace flags and store results in LogTraceFlags.
@@ -707,9 +707,14 @@ void Log_Trace(const char *format, ...)
 #else	/* !ENABLE_TRACING */
 
 /** dummy */
+const char* Log_CheckTraceOptions (const char *FlagsStr)
+	return "Hatari has been compiled without ENABLE_TRACING!";
+}
+
+/** dummy */
 const char* Log_SetTraceOptions (const char *FlagsStr)
 {
-	return "Hatari has been compiled without ENABLE_TRACING!";
+	return Log_CheckTraceOptions(FlagsStr);
 }
 
 /** dummy */

commit 9ea9582f4f0ea1571b04cc30f33a44cee3be34e4

Events: really fix the build with trace support disabled
Sigh, should have tested also with trace support disabled, but
I was in middle of refactoring, and thought fix simple enough.

Fixes: 30c708af1edf24

src/debug/log.c
@@ -708,6 +708,7 @@ void Log_Trace(const char *format, ...)
 
 /** dummy */
 const char* Log_CheckTraceOptions (const char *FlagsStr)
+{
 	return "Hatari has been compiled without ENABLE_TRACING!";
 }
 