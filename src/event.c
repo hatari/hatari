hatari/hatari

commit f2b6bd0c78f840e59665cfd334b8a03d6d81b2b8

Events: event prefix support for (4) CLI debug options
"<event>:" prefix can be used with arguments for options to:
- Parse debugger commands file
- Set exception debug mask
- Set trace flags
- Set log level

Prefix post-pones given option impact until given event.

Possible events are (in the order they happen):
- "boot": Emulation boot/reset
- "inf": TOS loads virtual INF file on boot
- "prg": Pexec() call on GEMDOS HD

Example:
  --trace boot:none --trace inf:os_base --trace prg:os_all

Because exception mask and trace flag settings can be rather
complex, their values are checked immediately at start, not
only when they are applied later on.

src/event.c
@@ -1,0 +1,116 @@
+/*
+  Hatari - event.c
+
+  This file is distributed under the GNU General Public License, version 2
+  or at your option any later version. Read the file gpl.txt for details.
+
+  Functions called to do user configured actions at specific points ("events")
+  of Hatari emulation.  Currently these actions are emulator (not emulation)
+  setting changes.
+*/
+const char Event_fileid[] = "Hatari event.c";
+
+#include "main.h"
+#include "configuration.h"
+#include "debugui.h"
+#include "event.h"
+#include "log.h"
+
+static event_actions_t resetActions;
+static event_actions_t infLoadActions;
+static event_actions_t prgExecActions;
+
+
+/**
+ * Check given option argument for event prefix and set
+ * given string pointer to a point after the prefix.
+ *
+ * Return pointer to specified actions struct, or NULL for no match.
+ */
+event_actions_t *Event_GetPrefixActions(const char **str)
+{
+	struct {
+		const char *prefix;
+		event_actions_t *actions;
+	} act[] = {
+		{ "boot:", &resetActions },
+		{ "inf:", &infLoadActions },
+		{ "prg:", &prgExecActions },
+	};
+	for (int i = 0; i < ARRAY_SIZE(act); i++) {
+		const char *prefix = act[i].prefix;
+		size_t len = strlen(prefix);
+
+		if (strncmp(*str, prefix, len) == 0) {
+			*str += len;
+			return act[i].actions;
+		}
+	}
+	return NULL;
+}
+
+/* ----------------------------------------------------------- */
+
+/**
+ * Perform actions specified in the references actions structure
+ */
+static void Event_PerformActions(event_actions_t *act)
+{
+	/* parse debugger commands? */
+	if (act->parseFile) {
+		Log_Printf(LOG_WARN, "Debugger file: '%s'\n", act->parseFile);
+		DebugUI_AddParseFile(act->parseFile);
+	}
+
+	/* change log level? */
+	if (act->logLevel) {
+		ConfigureParams.Log.nTextLogLevel = Log_ParseOptions(act->logLevel);
+		Log_Printf(LOG_WARN, "Log level: '%s'\n", act->logLevel);
+		Log_SetLevels();
+	}
+
+	/* set tracing? */
+	if (act->traceFlags) {
+		Log_SetTraceOptions(act->traceFlags);
+		Log_Printf(LOG_WARN, "Trace flags: '%s'\n", act->traceFlags);
+	}
+
+	/* set exception debug mask? */
+	if (act->exceptionMask) {
+		Log_SetExceptionDebugMask(act->exceptionMask);
+		Log_Printf(LOG_WARN, "Exception flags: '%s'\n", act->exceptionMask);
+	}
+}
+
+/**
+ * Perform actions related to emulation boot/reset event
+ */
+void Event_DoResetActions(void)
+{
+	Event_PerformActions(&resetActions);
+}
+
+/**
+ * Perform actions related to Atari program (GEMDOS HD) Pexec() event
+ */
+void Event_DoPrgExecActions(void)
+{
+	Event_PerformActions(&prgExecActions);
+}
+
+/**
+ * Perform actions related to virtual TOS INF loading event
+ */
+void Event_DoInfLoadActions(void)
+{
+	Event_PerformActions(&infLoadActions);
+
+	/* legacy/backwards compatible action */
+	if (ConfigureParams.Debugger.nExceptionDebugMask & EXCEPT_AUTOSTART) {
+		int mask = ConfigureParams.Debugger.nExceptionDebugMask & ~EXCEPT_AUTOSTART;
+		if (ExceptionDebugMask != mask) {
+			ExceptionDebugMask = mask;
+			Log_Printf(LOG_WARN, "Exception flags: 0x%x)\n", mask);
+		}
+	}
+}

src/CMakeLists.txt
@@ -2,7 +2,7 @@
 set(SOURCES
 	acia.c avi_record.c bios.c blitter.c cart.c cfgopts.c
 	clocks_timings.c configuration.c options.c change.c control.c
-	conv_gen.c conv_st.c cycInt.c cycles.c dialog.c dmaSnd.c fdc.c file.c
+	conv_gen.c conv_st.c cycInt.c cycles.c dialog.c dmaSnd.c event.c fdc.c file.c
 	floppy.c floppy_ipf.c floppy_stx.c gemdos.c hdc.c ide.c ikbd.c ioMem.c
 	ioMemTabST.c ioMemTabSTE.c ioMemTabTT.c ioMemTabFalcon.c joy.c
 	m68000.c main.c midi.c memorySnapShot.c mfp.c nf_scsidrv.c

src/debug/log.c
@@ -592,6 +592,16 @@ Log_ParseOptionFlags (const char *FlagsStr, flagname_t *Flags, int MaxFlags, uin
 }
 
 /**
+ * Parse/check specified exception flags.
+ * Return error string or NULL for success.
+ */
+const char* Log_CheckExceptionDebugMask (const char *FlagsStr)
+{
+	uint64_t mask = ConfigureParams.Debugger.nExceptionDebugMask;
+	return Log_ParseOptionFlags(FlagsStr, ExceptionFlags, ARRAY_SIZE(ExceptionFlags), &mask);
+}
+
+/**
  * Parse exception flags and store results in ExceptionDebugMask.
  * Return error string or NULL for success.
  * 
@@ -607,6 +617,15 @@ const char* Log_SetExceptionDebugMask (const char *FlagsStr)
 	return errstr;
 }
 
+/**
+ * Parse/check specified trace flags.
+ * Return error string or NULL for success.
+ */
+const char* Log_CheckTraceOptions (const char *FlagsStr)
+{
+	uint64_t flags = LogTraceFlags;
+	return Log_ParseOptionFlags(FlagsStr, TraceFlags, ARRAY_SIZE(TraceFlags), &flags);
+}
 
 #if ENABLE_TRACING
 

src/debug/log.h
@@ -42,6 +42,7 @@
 #define DEFAULT_EXCEPTIONS (EXCEPT_BUS|EXCEPT_ADDRESS|EXCEPT_DSP)
 
 extern int ExceptionDebugMask;
+extern const char* Log_CheckExceptionDebugMask(const char *FlagsStr);
 extern const char* Log_SetExceptionDebugMask(const char *OptionsStr);
 
 
@@ -81,6 +82,7 @@ extern void Log_Printf(LOGTYPE nType, const char *psFormat, ...)
 extern void Log_AlertDlg(LOGTYPE nType, const char *psFormat, ...)
 	__attribute__ ((format (printf, 2, 3)));
 extern LOGTYPE Log_ParseOptions(const char *OptionStr);
+extern const char* Log_CheckTraceOptions(const char *FlagsStr);
 extern const char* Log_SetTraceOptions(const char *OptionsStr);
 extern char *Log_MatchTrace(const char *text, int state);
 extern void Log_ToggleMsgRepeat(void);

src/gemdos.c
@@ -45,6 +45,7 @@ const char Gemdos_fileid[] = "Hatari gemdos.c";
 #include "main.h"
 #include "cart.h"
 #include "configuration.h"
+#include "event.h"
 #include "file.h"
 #include "floppy.h"
 #include "ide.h"
@@ -4567,6 +4568,9 @@ void GemDOS_PexecBpCreated(void)
 		STMemory_WriteWord(nSavedPexecParams, TosVersion >= 0x104 ? 6 : 4);
 		STMemory_WriteLong(nSavedPexecParams + 6, Regs[REG_D0]);
 		sr |= SR_OVERFLOW;
+
+		/* do user-configured program exec "event" actions */
+		Event_DoPrgExecActions();
 	}
 	else
 	{

src/includes/event.h
@@ -1,0 +1,25 @@
+/*
+  Hatari - event.h
+
+  This file is distributed under the GNU General Public License, version 2
+  or at your option any later version. Read the file gpl.txt for details.
+*/
+
+#ifndef HATARI_EVENT_H
+#define HATARI_EVENT_H
+
+/* option arguments that can be changed on Hatari "events" */
+typedef struct {
+	const char *parseFile;
+	const char *logLevel;
+	const char *traceFlags;
+	const char *exceptionMask;
+} event_actions_t;
+
+extern event_actions_t *Event_GetPrefixActions(const char **str);
+
+extern void Event_DoResetActions(void);
+extern void Event_DoInfLoadActions(void);
+extern void Event_DoPrgExecActions(void);
+
+#endif /* ifndef HATARI_EVENT_H */

src/inffile.c
@@ -18,6 +18,7 @@ const char INFFILE_fileid[] = "Hatari inffile.c";
 #include "main.h"
 #include "configuration.h"
 #include "conv_st.h"
+#include "event.h"
 #include "inffile.h"
 #include "options.h"
 #include "gemdos.h"
@@ -1069,19 +1070,17 @@ bool INF_Overriding(autostart_t t)
 
 /*-----------------------------------------------------------------------*/
 /**
- * If given name matches virtual INF file name, return its handle, NULL otherwise
+ * INF file (resolution/autostart) overriding; if given name matches
+ * virtual INF file name, return its handle, NULL otherwise.
+ *
+ * Runs also user-configured actions for this event.
  */
 FILE *INF_OpenOverride(const char *filename)
 {
 	if (TosOverride.file && strcmp(filename, TosOverride.infname) == 0)
 	{
-		/* whether to "autostart" also exception debugging? */
-		if (ConfigureParams.Debugger.nExceptionDebugMask & EXCEPT_AUTOSTART)
-		{
-			ExceptionDebugMask = ConfigureParams.Debugger.nExceptionDebugMask & ~EXCEPT_AUTOSTART;
-			Log_Printf(LOG_INFO, "Exception debugging enabled (0x%x).\n", ExceptionDebugMask);
-		}
 		Log_Printf(LOG_DEBUG, "Virtual INF file '%s' matched.\n", filename);
+		Event_DoInfLoadActions();
 		return TosOverride.file;
 	}
 	return NULL;

src/main.c
@@ -25,6 +25,7 @@ const char Main_fileid[] = "Hatari main.c";
 #include "conv_st.h"
 #include "options.h"
 #include "dialog.h"
+#include "event.h"
 #include "audio.h"
 #include "joy_ui.h"
 #include "file.h"

commit 521c46f13b290355406102857585ffb9648910d6

Events: fix debugger file parsing event
Parse file directly, do not just mark it to be parsed later.

Fixes: f2b6bd0c78f840e

src/event.c
@@ -13,7 +13,7 @@ const char Event_fileid[] = "Hatari event.c";
 #include "main.h"
 #include "avi_record.h"
 #include "configuration.h"
-#include "debugui.h"
+#include "debug_priv.h"
 #include "event.h"
 #include "log.h"
 #include "timing.h"
@@ -123,7 +123,7 @@ static void Event_PerformActions(event_actions_t *act)
 	/* parse debugger commands? */
 	if (act->parseFile) {
 		LOG_TRACE(TRACE_EVENT_ACTION, "EVENT: Debugger file: '%s'\n", act->parseFile);
-		DebugUI_AddParseFile(act->parseFile);
+		DebugUI_ParseFile(act->parseFile, true, true);
 	}
 
 	/* change log level? */