hatari/hatari

commit 48d8d9728acf74d7455975e59a23d2ee50268e44

breakpoints aren't part of emulated state, save them to separate file from memory snapshot

src/debug/breakcond.c
@@ -105,61 +105,40 @@ static bool BreakCond_Remove(int position, bool bForDsp);
 static void BreakCond_Print(bc_breakpoint_t *bp);
 
 
-/* --------------------- memory snapshot ------------------- */
-
 /**
- * Save/Restore snapshot of local breakpoint variables
+ * Save breakpoints as debugger input file
+ * return true for success, false for failure
  */
-void BreakCond_MemorySnapShot_Capture(bool bSave)
+bool BreakCond_Save(const char *filename)
 {
-	char tmp[256], **str;
-	int i, idx, count;
+	FILE *fp;
+	int i;
 
-	if (!bSave) {
-		/* free current data before restore */
-		for (i = 0; i < BreakPointCpuCount; i++) {
-			free(BreakPointsCpu[i].expression);
+	if (!(BreakPointCpuCount || BreakPointDspCount)) {
+		if (remove(filename)) {
+			perror("ERROR");
+			return false;
 		}
-		for (i = 0; i < BreakPointDspCount; i++) {
-			free(BreakPointsDsp[i].expression);
-		}
+		return true;
 	}
-	
-	/* save/restore arrays & counts */
-	MemorySnapShot_Store(&BreakPointCpuCount, sizeof(BreakPointCpuCount));
-	MemorySnapShot_Store(&BreakPointDspCount, sizeof(BreakPointDspCount));
-	MemorySnapShot_Store(&BreakPointsCpu, sizeof(BreakPointsCpu));
-	MemorySnapShot_Store(&BreakPointsDsp, sizeof(BreakPointsDsp));
 
-	/* save/restore dynamically allocated strings */
-	for (i = 0; i < 2*BC_MAX_CONDITION_BREAKPOINTS; i++) {
-		if (i >= BC_MAX_CONDITION_BREAKPOINTS) {
-			idx = i-BC_MAX_CONDITION_BREAKPOINTS;
-			str = &(BreakPointsDsp[idx].expression);
-			count = BreakPointDspCount;
-		} else {
-			idx = i;
-			str = &(BreakPointsCpu[idx].expression);
-			count = BreakPointCpuCount;
-		}
-		if (bSave) {
-			/* clean + zero-terminate, copy & save */
-			memset(tmp, 0, sizeof(tmp));
-			if (idx < count) {
-				strncpy(tmp, *str, sizeof(tmp)-1);
-			}
-			MemorySnapShot_Store(&tmp, sizeof(tmp));
-		} else {
-			MemorySnapShot_Store(&tmp, sizeof(tmp));
-			if (idx < count) {
-				*str = strdup(tmp);
-				assert(*str);
-			} else {
-				*str = NULL;
-			}
-		}
+	fprintf(stderr, "Saving breakpoints to '%s'...\n", filename);
+	fp = fopen(filename, "w");
+	if (!fp) {
+		perror("ERROR");
+		return false;
 	}
+	/* save conditional breakpoints as debugger input file */
+	for (i = 0; i < BreakPointCpuCount; i++) {
+		fprintf(fp, "b %s\n", BreakPointsCpu[i].expression);
+	}
+	for (i = 0; i < BreakPointDspCount; i++) {
+		fprintf(fp, "db %s\n", BreakPointsDsp[i].expression);
+	}
+	fclose(fp);
+	return true;
 }
+
 
 /* --------------------- debugging code ------------------- */
 

src/debug/breakcond.h
@@ -9,7 +9,7 @@
 #define HATARI_BREAKCOND_H
 
 /* for debugui.c */
-extern void BreakCond_MemorySnapShot_Capture(bool bSave);
+extern bool BreakCond_Save(const char *filename);
 extern char *BreakCond_MatchCpuVariable(const char *text, int state);
 extern char *BreakCond_MatchDspVariable(const char *text, int state);
 

src/debug/debugcpu.c
@@ -46,8 +46,6 @@ void DebugCpu_MemorySnapShot_Capture(bool bSave)
 {
 	MemorySnapShot_Store(&disasm_addr, sizeof(disasm_addr));
 	MemorySnapShot_Store(&memdump_addr, sizeof(memdump_addr));
-	
-	MemorySnapShot_Store(&nCpuActiveCBs, sizeof(nCpuActiveCBs));
 }
 
 
@@ -358,7 +356,6 @@ error_msg:
 static int DebugCpu_BreakAddr(int nArgc, char *psArgs[])
 {
 	BreakAddr_Command(psArgs[1], false);
-	nCpuActiveCBs = BreakCond_BreakPointCount(false);
 	return DEBUGGER_CMDDONE;
 }
 
@@ -368,7 +365,6 @@ static int DebugCpu_BreakAddr(int nArgc, char *psArgs[])
 static int DebugCpu_BreakCond(int nArgc, char *psArgs[])
 {
 	BreakCond_Command(psArgs[1], false);
-	nCpuActiveCBs = BreakCond_BreakPointCount(false);
 	return DEBUGGER_CMDDONE;
 }
 
@@ -524,6 +520,7 @@ void DebugCpu_Check(void)
  */
 void DebugCpu_SetDebugging(void)
 {
+	nCpuActiveCBs = BreakCond_BreakPointCount(false);
 	if (nCpuActiveCBs || nCpuSteps)
 		M68000_SetSpecial(SPCFLAG_DEBUGGER);
 	else

src/debug/debugdsp.c
@@ -40,8 +40,6 @@ void DebugDsp_MemorySnapShot_Capture(bool bSave)
 	MemorySnapShot_Store(&dsp_disasm_addr, sizeof(dsp_disasm_addr));
 	MemorySnapShot_Store(&dsp_memdump_addr, sizeof(dsp_memdump_addr));
 	MemorySnapShot_Store(&dsp_mem_space, sizeof(dsp_mem_space));
-	
-	MemorySnapShot_Store(&nDspActiveCBs, sizeof(nDspActiveCBs));
 }
 
 
@@ -306,7 +304,6 @@ static int DebugDsp_Continue(int nArgc, char *psArgv[])
 static int DebugDsp_BreakAddr(int nArgc, char *psArgs[])
 {
 	BreakAddr_Command(psArgs[1], true);
-	nDspActiveCBs = BreakCond_BreakPointCount(true);
 	return DEBUGGER_CMDDONE;
 }
 
@@ -316,7 +313,6 @@ static int DebugDsp_BreakAddr(int nArgc, char *psArgs[])
 static int DebugDsp_BreakCond(int nArgc, char *psArgs[])
 {
 	BreakCond_Command(psArgs[1], true);
-	nDspActiveCBs = BreakCond_BreakPointCount(true);
 	return DEBUGGER_CMDDONE;
 }
 
@@ -348,6 +344,7 @@ void DebugDsp_Check(void)
  */
 void DebugDsp_SetDebugging(void)
 {
+	nDspActiveCBs = BreakCond_BreakPointCount(true);
 	if (nDspActiveCBs || nDspSteps)
 		DSP_SetDebugging(true);
 	else

src/debug/debugui.c
@@ -59,11 +59,36 @@ static bool DebugUI_ParseFile(const char *path);
 /**
  * Save/Restore snapshot of debugging session variables
  */
-void DebugUI_MemorySnapShot_Capture(bool bSave)
+void DebugUI_MemorySnapShot_Capture(const char *path, bool bSave)
 {
+	char *filename;
+	
 	DebugCpu_MemorySnapShot_Capture(bSave);
 	DebugDsp_MemorySnapShot_Capture(bSave);
-	BreakCond_MemorySnapShot_Capture(bSave);
+
+	filename = malloc(strlen(path) + strlen(".break") + 1);
+	assert(filename);
+	strcpy(filename, path);
+	strcat(filename, ".break");
+	
+	if (bSave)
+	{
+		/* save breakpoints as debugger input file */
+		BreakCond_Save(filename);
+	}
+	else
+	{
+		/* remove current CPU and DSP breakpoints */
+		BreakCond_Command("all", false);
+		BreakCond_Command("all", true);
+
+		if (File_Exists(filename))
+		{
+			/* and parse back the saved breakpoints */
+			DebugUI_ParseFile(filename);
+		}
+	}
+	free(filename);
 }
 
 
@@ -991,6 +1016,9 @@ static bool DebugUI_ParseFile(const char *path)
 	}
 
 	free(input);
+
+	DebugCpu_SetDebugging();
+	DebugDsp_SetDebugging();
 	return true;
 }
 

src/debug/debugui.h
@@ -24,6 +24,6 @@ extern void DebugUI(void);
 extern void DebugUI_Init(void);
 extern bool DebugUI_RemoteParse(char *input);
 extern bool DebugUI_SetParseFile(const char *input);
-extern void DebugUI_MemorySnapShot_Capture(bool bSave);
+extern void DebugUI_MemorySnapShot_Capture(const char *path, bool bSave);
 
 #endif /* HATARI_DEBUGUI_H */

src/memorySnapShot.c
@@ -245,7 +245,7 @@ void MemorySnapShot_Capture(const char *pszFileName, bool bConfirm)
 		Blitter_MemorySnapShot_Capture(true);
 		DmaSnd_MemorySnapShot_Capture(true);
 		DSP_MemorySnapShot_Capture(true);
-		DebugUI_MemorySnapShot_Capture(true);
+		DebugUI_MemorySnapShot_Capture(pszFileName, true);
 
 		/* And close */
 		MemorySnapShot_CloseFile();
@@ -291,7 +291,7 @@ void MemorySnapShot_Restore(const char *pszFileName, bool bConfirm)
 		Blitter_MemorySnapShot_Capture(false);
 		DmaSnd_MemorySnapShot_Capture(false);
 		DSP_MemorySnapShot_Capture(false);
-		DebugUI_MemorySnapShot_Capture(false);
+		DebugUI_MemorySnapShot_Capture(pszFileName, false);
 
 		/* And close */
 		MemorySnapShot_CloseFile();

commit d9da1cd886aa3c5c29889945fedcfae83ec80557

Debugger: fix breakpoint options missing from debugger state save
Conditional breakpoints were added in Hatari v1.3.0, and this
regression came in v1.4.0.

After this fix there are still few potential pitfalls:
* Breakpoint file paths will be relative to saved state file
* Loaded debugger symbols are not saved / restored
* Debugger setting are not saved, in case user changed them
  after starting Hatari

Fixes: 48d8d9728ac

src/debug/breakcond.c
@@ -113,7 +113,7 @@ static bc_breakpoints_t DspBreakPoints = {
 /* forward declarations */
 static void BreakCond_DoDelayedActions(bc_breakpoints_t *bps);
 static bool BreakCond_Remove(bc_breakpoints_t *bps, int position);
-static void BreakCond_Print(bc_breakpoint_t *bp);
+static void BreakCond_Print(FILE *fp, const char *prefix, bc_breakpoint_t *bp);
 
 
 /**
@@ -143,10 +143,16 @@ bool BreakCond_Save(const char *filename)
 	}
 	/* save conditional breakpoints as debugger input file */
 	for (i = 0; i < CpuBreakPoints.count; i++) {
-		fprintf(fp, "b %s\n", CpuBreakPoints.breakpoint[i].expression);
+		bc_breakpoint_t *bp = &CpuBreakPoints.breakpoint[i];
+		if (!bp->options.deleted) {
+			BreakCond_Print(fp, "b ", bp);
+		}
 	}
 	for (i = 0; i < DspBreakPoints.count; i++) {
-		fprintf(fp, "db %s\n", DspBreakPoints.breakpoint[i].expression);
+		bc_breakpoint_t *bp = &DspBreakPoints.breakpoint[i];
+		if (!bp->options.deleted) {
+			BreakCond_Print(fp, "b ", bp);
+		}
 	}
 	fclose(fp);
 	return true;
@@ -335,7 +341,7 @@ static bool BreakCond_MatchBreakPoints(bc_breakpoints_t *bps)
 			if (!bp->options.quiet) {
 				fprintf(stderr, "%d. %s breakpoint condition(s) matched %d times.\n",
 					i+1, bps->name, bp->hits);
-				BreakCond_Print(bp);
+				BreakCond_Print(stderr, "\t", bp);
 			}
 			History_Mark(bps->reason);
 
@@ -1290,40 +1296,40 @@ static bool BreakCond_Parse(const char *expression, bc_options_t *options, bool 
 /**
  * print single breakpoint
  */
-static void BreakCond_Print(bc_breakpoint_t *bp)
+static void BreakCond_Print(FILE *fp, const char *prefix, bc_breakpoint_t *bp)
 {
-	fprintf(stderr, "\t%s", bp->expression);
+	fprintf(fp, "%s%s", prefix, bp->expression);
 	if (bp->options.skip) {
-		fprintf(stderr, " :%d", bp->options.skip);
+		fprintf(fp, " :%d", bp->options.skip);
 	}
 	if (bp->options.once) {
-		fprintf(stderr, " :once");
+		fprintf(fp, " :once");
 	}
 	if (bp->options.quiet) {
-		fprintf(stderr, " :quiet");
+		fprintf(fp, " :quiet");
 	}
 	if (bp->options.trace) {
-		fprintf(stderr, " :trace");
+		fprintf(fp, " :trace");
 		if (bp->options.info) {
-			fprintf(stderr, " :info");
+			fprintf(fp, " :info");
 		}
 		if (bp->options.lock) {
-			fprintf(stderr, " :lock");
+			fprintf(fp, " :lock");
 		}
 		if (bp->options.noinit) {
-			fprintf(stderr, " :noinit");
+			fprintf(fp, " :noinit");
 		}
 	}
 	if (bp->options.filename) {
-		fprintf(stderr, " :file %s", bp->options.filename);
+		fprintf(fp, " :file %s", bp->options.filename);
 	}
 	if (bp->options.print) {
-		fprintf(stderr, " :print %s", bp->options.print);
+		fprintf(fp, " :print %s", bp->options.print);
 	}
 	if (bp->options.deleted) {
-		fprintf(stderr, " (deleted)");
+		fprintf(fp, " (deleted)");
 	}
-	fprintf(stderr, "\n");
+	fprintf(fp, "\n");
 }
 
 /**
@@ -1342,7 +1348,7 @@ static void BreakCond_List(bc_breakpoints_t *bps)
 	bp = bps->breakpoint;
 	for (i = 1; i <= bps->count; bp++, i++) {
 		fprintf(stderr, "%4d:", i);
-		BreakCond_Print(bp);
+		BreakCond_Print(stderr, "\t", bp);
 	}
 }
 
@@ -1369,7 +1375,7 @@ static bool BreakCond_Remove(bc_breakpoints_t *bps, int position)
 	}
 	if (!bp->options.quiet) {
 		fprintf(stderr, "Removed %s breakpoint %d:\n", bps->name, position);
-		BreakCond_Print(bp);
+		BreakCond_Print(stderr, "\t", bp);
 	}
 	free(bp->expression);
 	free(bp->conditions);