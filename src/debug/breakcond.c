hatari/hatari

commit d9da1cd886aa3c5c29889945fedcfae83ec80557

Debugger: fix breakpoint options missing from debugger state save
Conditional breakpoints were added in Hatari v1.3.0, and this
regression came in v1.4.0.

After this fix there are still few potential pitfalls:
* Breakpoint file paths will be relative to saved state file
* Loaded debugger symbols are not saved / restored
* Debugger setting are not saved, in case user changed them
  after starting Hatari

Fixes: 48d8d9728ac

src/debug/breakcond.c
@@ -113,7 +113,7 @@ static bc_breakpoints_t DspBreakPoints = {
 /* forward declarations */
 static void BreakCond_DoDelayedActions(bc_breakpoints_t *bps);
 static bool BreakCond_Remove(bc_breakpoints_t *bps, int position);
-static void BreakCond_Print(bc_breakpoint_t *bp);
+static void BreakCond_Print(FILE *fp, const char *prefix, bc_breakpoint_t *bp);
 
 
 /**
@@ -143,10 +143,16 @@ bool BreakCond_Save(const char *filename)
 	}
 	/* save conditional breakpoints as debugger input file */
 	for (i = 0; i < CpuBreakPoints.count; i++) {
-		fprintf(fp, "b %s\n", CpuBreakPoints.breakpoint[i].expression);
+		bc_breakpoint_t *bp = &CpuBreakPoints.breakpoint[i];
+		if (!bp->options.deleted) {
+			BreakCond_Print(fp, "b ", bp);
+		}
 	}
 	for (i = 0; i < DspBreakPoints.count; i++) {
-		fprintf(fp, "db %s\n", DspBreakPoints.breakpoint[i].expression);
+		bc_breakpoint_t *bp = &DspBreakPoints.breakpoint[i];
+		if (!bp->options.deleted) {
+			BreakCond_Print(fp, "b ", bp);
+		}
 	}
 	fclose(fp);
 	return true;
@@ -335,7 +341,7 @@ static bool BreakCond_MatchBreakPoints(bc_breakpoints_t *bps)
 			if (!bp->options.quiet) {
 				fprintf(stderr, "%d. %s breakpoint condition(s) matched %d times.\n",
 					i+1, bps->name, bp->hits);
-				BreakCond_Print(bp);
+				BreakCond_Print(stderr, "\t", bp);
 			}
 			History_Mark(bps->reason);
 
@@ -1290,40 +1296,40 @@ static bool BreakCond_Parse(const char *expression, bc_options_t *options, bool 
 /**
  * print single breakpoint
  */
-static void BreakCond_Print(bc_breakpoint_t *bp)
+static void BreakCond_Print(FILE *fp, const char *prefix, bc_breakpoint_t *bp)
 {
-	fprintf(stderr, "\t%s", bp->expression);
+	fprintf(fp, "%s%s", prefix, bp->expression);
 	if (bp->options.skip) {
-		fprintf(stderr, " :%d", bp->options.skip);
+		fprintf(fp, " :%d", bp->options.skip);
 	}
 	if (bp->options.once) {
-		fprintf(stderr, " :once");
+		fprintf(fp, " :once");
 	}
 	if (bp->options.quiet) {
-		fprintf(stderr, " :quiet");
+		fprintf(fp, " :quiet");
 	}
 	if (bp->options.trace) {
-		fprintf(stderr, " :trace");
+		fprintf(fp, " :trace");
 		if (bp->options.info) {
-			fprintf(stderr, " :info");
+			fprintf(fp, " :info");
 		}
 		if (bp->options.lock) {
-			fprintf(stderr, " :lock");
+			fprintf(fp, " :lock");
 		}
 		if (bp->options.noinit) {
-			fprintf(stderr, " :noinit");
+			fprintf(fp, " :noinit");
 		}
 	}
 	if (bp->options.filename) {
-		fprintf(stderr, " :file %s", bp->options.filename);
+		fprintf(fp, " :file %s", bp->options.filename);
 	}
 	if (bp->options.print) {
-		fprintf(stderr, " :print %s", bp->options.print);
+		fprintf(fp, " :print %s", bp->options.print);
 	}
 	if (bp->options.deleted) {
-		fprintf(stderr, " (deleted)");
+		fprintf(fp, " (deleted)");
 	}
-	fprintf(stderr, "\n");
+	fprintf(fp, "\n");
 }
 
 /**
@@ -1342,7 +1348,7 @@ static void BreakCond_List(bc_breakpoints_t *bps)
 	bp = bps->breakpoint;
 	for (i = 1; i <= bps->count; bp++, i++) {
 		fprintf(stderr, "%4d:", i);
-		BreakCond_Print(bp);
+		BreakCond_Print(stderr, "\t", bp);
 	}
 }
 
@@ -1369,7 +1375,7 @@ static bool BreakCond_Remove(bc_breakpoints_t *bps, int position)
 	}
 	if (!bp->options.quiet) {
 		fprintf(stderr, "Removed %s breakpoint %d:\n", bps->name, position);
-		BreakCond_Print(bp);
+		BreakCond_Print(stderr, "\t", bp);
 	}
 	free(bp->expression);
 	free(bp->conditions);

commit 4c9ef5aeba68c771e88e93cd4981e2a5c79e3cc3

Debugger: support relative paths in saved debugger state
Breakpoint options can refer to additional debugger input files,
and those can refer to further files.

Preceeding breakpoint commands with 'cd $CWD' command when such
options are used, allows relative paths to work.

Also change state save output to use logging functions and
fix ("b -> db") DSP breakpoint typo in previous commit.

Fixes: d9da1cd886aa3c5c

---

After this commit, there's still some debugger state that is not saved
/ restored yet, and which could differ from what's expected by the
debugger commands (in files referred by the saved debugger state file).

Following ones can affect how the breakpoint commands work:
- Number base (for numbers missing base prefix)
- Loaded symbols + symbol autoload setting
- Exception debugging mask

=> These should probably be saved too

And following can affect debugger output from breakpoints:
- Trace options & log level
- Disassembly options
- Profiling state
- Lock option

=> These I'd leave for user, there may be good reason to
   change them from what was active during state save.

doc/debugger.html
@@ -1099,6 +1099,25 @@ these debugger input files also with the debugger "file" command,
 command line option.
 </p>
 
+
+<h4 id="State_saves">Breakpoints in emulation state saves</h4>
+
+<p>
+When emulation state is saved, currently active breakpoints are saved
+to a separate file with additional ".debug" extension.  If breakpoints
+refer to other files, that file starts with a "cd" command that sets
+Hatari working directory to what it was when state was saved.
+</p>
+
+<p>
+This way the relative paths referred by breakpoint ":file" options
+(and possible further files referred from those files), should work
+fine. However, if any of the related files are moved, rest of the them
+need to be moved too, and the "cd" command path in the ".debug" state
+save file updated accordingly.
+</p>
+
+
 <h3 id="Stepping">Stepping through code</h3>
 
 <p>

src/debug/breakcond.c
@@ -123,7 +123,11 @@ static void BreakCond_Print(FILE *fp, const char *prefix, bc_breakpoint_t *bp);
 bool BreakCond_Save(const char *filename)
 {
 	FILE *fp;
-	int i;
+	int i, j;
+	bool ext_files;
+	const bc_breakpoints_t *bps[] = {
+		&CpuBreakPoints, &DspBreakPoints
+	};
 
 	if (!(CpuBreakPoints.count || DspBreakPoints.count)) {
 		if (File_Exists(filename)) {
@@ -135,12 +139,40 @@ bool BreakCond_Save(const char *filename)
 		return true;
 	}
 
-	fprintf(stderr, "Saving breakpoints to '%s'...\n", filename);
+	Log_Printf(LOG_INFO, "Saving breakpoints to '%s'...\n", filename);
 	fp = fopen(filename, "w");
 	if (!fp) {
 		perror("ERROR");
 		return false;
 	}
+
+	/* check whether breakpoints would parse debugger files
+	 * (which could then load further more files)?
+	 */
+	ext_files = false;
+	for (j = 0; j < ARRAY_SIZE(bps); j++) {
+		for (i = 0; i < bps[j]->count; i++) {
+			if (bps[j]->breakpoint[i].options.filename) {
+				ext_files = true;
+				break;
+			}
+		}
+	}
+
+	if (ext_files) {
+		/* make sure possible relative paths in debugger input
+		 * files will work, as they would work currently.
+		 */
+		char *cwd = malloc(FILENAME_MAX);
+		if (cwd && getcwd(cwd, FILENAME_MAX)) {
+			Log_Printf(LOG_WARN, "breakpoints refer to other files => adding 'cd $CWD'\n");
+			fprintf(fp, "cd %s\n", cwd);
+		} else {
+			perror("getting CWD for breakpoint paths failed");
+		}
+		free(cwd);
+	}
+
 	/* save conditional breakpoints as debugger input file */
 	for (i = 0; i < CpuBreakPoints.count; i++) {
 		bc_breakpoint_t *bp = &CpuBreakPoints.breakpoint[i];
@@ -151,9 +183,10 @@ bool BreakCond_Save(const char *filename)
 	for (i = 0; i < DspBreakPoints.count; i++) {
 		bc_breakpoint_t *bp = &DspBreakPoints.breakpoint[i];
 		if (!bp->options.deleted) {
-			BreakCond_Print(fp, "b ", bp);
+			BreakCond_Print(fp, "db ", bp);
 		}
 	}
+
 	fclose(fp);
 	return true;
 }

tests/debugger/test-dummies.c
@@ -17,6 +17,7 @@ char *rl_filename_completion_function(const char *text, int state) { return NULL
 uint64_t LogTraceFlags = 0;
 FILE *TraceFile;
 void Log_Trace(const char *format, ...) { }
+void Log_Printf(LOGTYPE, const char *format, ...) { }
 void Log_ResetMsgRepeat(void) {}
 
 /* fake Hatari configuration variables for number parsing */