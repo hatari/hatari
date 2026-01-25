hatari/hatari

commit 410136d06afb3fd5765cedc1aa27b2e5443e0e93

GEMDOS HD: fix TeraDesk "Show parent" functionality
Reported by Miro. When "View" -> "Set Mask..." -> "Show parent"
TeraDesk option is used, ".." directory entries are missing when
GEMDOS HD is used.

Roger: in a standard TOS/DOS root directory, there are no
"." or ".." entries.  In a subdirectory, both exist, and Fsfirst /
Fsnext("somedir\*.*") will return both entries (1st entry: ".", 2nd
entry: ".."). Hatari GEMDOS HD drive never returns those entries.

This is not a regression, GEMDOS HD has never returned them.
Now it does.

src/gemdos.c
@@ -425,12 +425,12 @@ static void GemDOS_FreeAllInternalDTAs(void)
 /**
  * Match a TOS file name to a dir mask.
  */
-static bool fsfirst_match(const char *pat, const char *name)
+static bool fsfirst_match(const char *pat, const char *name, bool dots)
 {
 	const char *dot, *p=pat, *n=name;
 
-	if (name[0] == '.')
-		return false;           /* skip .* files */
+	if (name[0] == '.' && !dots)
+		return false;           /* skip .* entries for drive root */
 
 	dot = strrchr(name, '.');	/* '*' matches everything except last dot in name */
 	if (dot && p[0] == '*' && p[1] == 0)
@@ -473,12 +473,13 @@ static bool fsfirst_match(const char *pat, const char *name)
 
 /*-----------------------------------------------------------------------*/
 /**
- * Parse directory from sfirst mask
+ * Parse directory from sfirst mask 'string' to 'newstr'
  * - e.g.: input:  "hdemudir/auto/mask*.*" outputs: "hdemudir/auto"
+ * Return true if it matches drive 'rootdir'
  */
-static void fsfirst_dirname(const char *string, char *newstr)
+static bool fsfirst_dirname(const char *rootdir, const char *string, char *newstr)
 {
-	int i=0;
+	int i = 0, rootlen = strlen(rootdir);
 
 	strcpy(newstr, string);
 
@@ -489,10 +490,24 @@ static void fsfirst_dirname(const char *string, char *newstr)
 			newstr[i] = PATHSEP;
 		i++;
 	}
+	if (i <= rootlen)
+	{
+		assert(strcmp(rootdir, newstr) == 0);
+		return true;
+	}
+
 	/* find last slash and terminate string */
 	while (i && newstr[i] != PATHSEP)
 		i--;
 	newstr[i] = '\0';
+
+	if (i <= rootlen)
+	{
+		assert(strcmp(rootdir, newstr) == 0);
+		return true;
+	}
+
+	return false;
 }
 
 /*-----------------------------------------------------------------------*/
@@ -1165,7 +1180,7 @@ static char* match_host_dir_entry(const char *path, const char *name, bool patte
 		{
 			char *d_name = entry->d_name;
 			Str_DecomposedToPrecomposedUtf8(d_name, d_name);   /* for OSX */
-			if (fsfirst_match(name, d_name))
+			if (fsfirst_match(name, d_name, false))
 			{
 				match = strdup(d_name);
 				break;
@@ -3031,6 +3046,7 @@ static bool GemDOS_SFirst(uint32_t Params)
 	uint32_t DTA_Gemdos;
 	uint16_t useidx;
 	uint16_t attrib;
+	bool isRoot;
 
 	attrib = STMemory_ReadWord(Params+SIZE_LONG);
 	pszFileName = STMemory_GetStringPointer(STMemory_ReadLong(Params));
@@ -3113,7 +3129,8 @@ static bool GemDOS_SFirst(uint32_t Params)
 	/* open directory
 	 * TODO: host path may not fit into InternalDTA
 	 */
-	fsfirst_dirname(szActualFileName, InternalDTAs[useidx].path);
+	const char *rootdir = emudrives[Drive-2]->hd_emulation_dir;
+	isRoot = fsfirst_dirname(rootdir, szActualFileName, InternalDTAs[useidx].path);
 	fsdir = opendir(InternalDTAs[useidx].path);
 
 	if (fsdir == NULL)
@@ -3140,9 +3157,12 @@ static bool GemDOS_SFirst(uint32_t Params)
 	j = 0;
 	for (i=0; i < count; i++)
 	{
+		/* root dir does not include "." & ".." entries, others do */
+		const bool dots = !isRoot;
 		char *d_name = files[i]->d_name;
+
 		Str_DecomposedToPrecomposedUtf8(d_name, d_name);   /* for OSX */
-		if (fsfirst_match(dirmask, d_name))
+		if (fsfirst_match(dirmask, d_name, dots))
 		{
 			InternalDTAs[useidx].found[j] = files[i];
 			j++;

src/str.c
@@ -272,9 +272,12 @@ void Str_Filename_Host2Atari(const char *source, char *dst)
 			dot[4] = '\0';
 
 		/* if there are extra dots, convert them */
-		for (tmp = src; tmp < dot; tmp++)
-			if (*tmp == '.')
-				*tmp = INVALID_CHAR;
+		if (len != 2 || strcmp(src, "..") != 0)
+		{
+			for (tmp = src; tmp < dot; tmp++)
+				if (*tmp == '.')
+					*tmp = INVALID_CHAR;
+		}
 
 		/* limit part before extension to 8 chars */
 		if (dot - src > 8)

commit f1270a8e4e4381399152cbc5929628f09df152ed

GEMDOS HD: skip dot files for Fsfirst/Fsnext like in earlier releases
I.e. return only "." and "..", and not even those for root dir, and
ignore other dot files completely for pattern matches.

Otherwise it's too easy to get duplicate host files shown for GEMDOS
HD directories (files not intended for Atari / FAT file systems,
showing up e.g. in Atari Desktops that show dot files, unlike normal
TOS desktop).

Renamed "dots" argument accordingly, as "subdir".

Fixes: 410136d06afb

PS. Atari programs can still open dot-files when their names are
unique (when truncated to 3 letters reserved for FAT file extension).
They just cannot search for them with Fsfirst/Fsnext.  This is enough
for almost all *nix programs ported to Atari, that use such config
files.

src/gemdos.c
@@ -426,16 +426,23 @@ static void GemDOS_FreeAllInternalDTAs(void)
 /**
  * Match a TOS file name to a dir mask.
  *
- * Set 'dots' if dotfiles are acccepted, and 'only_invalid'
+ * Set 'subdir' when file is not at drive root, and 'only_invalid'
  * when '?' in (host file) name pattern should match only
  * characters that are invalid for Atari GEMDOS file names.
  */
-static bool fsfirst_match(const char *pat, const char *name, bool dots, bool only_invalid)
+static bool fsfirst_match(const char *pat, const char *name, bool subdir, bool only_invalid)
 {
 	const char *dot, *p=pat, *n=name;
 
-	if (name[0] == '.' && !dots)
-		return false;           /* skip .* entries for drive root */
+	if (name[0] == '.')
+	{
+		if (!subdir)
+			/* skip all ".*" entries for drive root */
+			return false;
+		if (strcmp(name, ".") && strcmp(name, ".."))
+			/* skip other than "." and ".." entries for subdirs */
+			return false;
+	}
 
 	dot = strrchr(name, '.');	/* '*' matches everything except last dot in name */
 	if (dot && p[0] == '*' && p[1] == 0)
@@ -1186,13 +1193,13 @@ static char* match_host_dir_entry(const char *path, const char *name,
 	if (pattern)
 	{
 		/* TOS handles "." / ".." */
-		const bool dots = false;
+		const bool subdir = false;
 
 		while ((entry = readdir(dir)))
 		{
 			char *d_name = entry->d_name;
 			Str_DecomposedToPrecomposedUtf8(d_name, d_name);   /* for OSX */
-			if (fsfirst_match(name, d_name, dots, only_invalid))
+			if (fsfirst_match(name, d_name, subdir, only_invalid))
 			{
 				match = strdup(d_name);
 				break;
@@ -3175,12 +3182,12 @@ static bool GemDOS_SFirst(uint32_t Params)
 	for (i=0; i < count; i++)
 	{
 		/* root dir does not include "." & ".." entries, others do */
-		const bool dots = !isRoot;
+		const bool subdir = !isRoot;
 		const bool only_invalid = false;
 		char *d_name = files[i]->d_name;
 
 		Str_DecomposedToPrecomposedUtf8(d_name, d_name);   /* for OSX */
-		if (fsfirst_match(dirmask, d_name, dots, only_invalid))
+		if (fsfirst_match(dirmask, d_name, subdir, only_invalid))
 		{
 			InternalDTAs[useidx].found[j] = files[i];
 			j++;