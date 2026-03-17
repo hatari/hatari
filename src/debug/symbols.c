hatari/hatari

commit 720861794fa15f2233542fcc1591846a8c93c509

Symbols: fix "--symload exec"
Earlier code worked only when program symbols were loaded from an
external (ASCII) symbols file.  This makes it to work also for loading
symbols from the (unstripped) program binary itself.

Basepage is not completely set up when Pexec() is called on the
program, so code needed to be changed to pass TEXT / DATA / BSS
offsets down to symbol loading.

Fixes: 9927e8428ceb2ad

src/debug/symbols.c
@@ -276,36 +276,54 @@ static void symbols_split_addresses(symbol_list_t* list)
 
 /**
  * Set sections to match running process by adding TEXT/DATA/BSS
- * start addresses to section offsets and ends, and return true if
- * results match it.
+ * start addresses to section offsets and ends.  If explicit section
+ *offsets are not given, they are taken from the program basepage.
+ * Return true if results match it.
  */
-static bool update_sections(prg_section_t *sections)
+static bool update_sections(uint32_t *offsets, prg_section_t *sections)
 {
 	/* offsets & max sizes for running program TEXT/DATA/BSS section symbols */
-	uint32_t start = DebugInfo_GetTEXT();
+	uint32_t start;
+
+	if (offsets) {
+		start = offsets[0];
+	} else {
+		start = DebugInfo_GetTEXT();
+	}
 	if (!start) {
-		fprintf(stderr, "ERROR: no valid program basepage!\n");
+		fprintf(stderr, "ERROR: program TEXT address invalid!\n");
 		return false;
 	}
 	sections[0].offset = start;
 	sections[0].end += start;
-	if (DebugInfo_GetTEXTEnd() != sections[0].end) {
-		fprintf(stderr, "ERROR: given program TEXT section size differs from one in RAM!\n");
+	if (!offsets && sections[0].end != DebugInfo_GetTEXTEnd()) {
+		fprintf(stderr, "ERROR: given program TEXT end (0x%x) does not match basepage info (0x%x)!\n",
+			sections[0].end, DebugInfo_GetTEXTEnd());
 		return false;
 	}
 
-	start = DebugInfo_GetDATA();
-	sections[1].offset = start;
-	if (sections[1].offset != sections[0].end) {
-		fprintf(stderr, "WARNING: DATA start doesn't match TEXT start + size!\n");
+	if (offsets) {
+		start = offsets[1];
+	} else {
+		start = DebugInfo_GetDATA();
 	}
+	if (start != sections[0].end) {
+		fprintf(stderr, "WARNING: program DATA start (0x%x) doesn't match TEXT start + size (0x%x)!\n",
+			start, sections[0].end);
+	}
+	sections[1].offset = start;
 	sections[1].end += start;
 
-	start = DebugInfo_GetBSS();
-	sections[2].offset = start;
-	if (sections[2].offset != sections[1].end) {
-		fprintf(stderr, "WARNING: BSS start doesn't match DATA start + size!\n");
+	if (offsets) {
+		start = offsets[2];
+	} else {
+		start = DebugInfo_GetBSS();
 	}
+	if (start != sections[1].end) {
+		fprintf(stderr, "WARNING: program BSS start (0x%x) doesn't match DATA start + size (0x%x)!\n",
+			start, sections[1].end);
+	}
+	sections[2].offset = start;
 	sections[2].end += start;
 
 	return true;
@@ -342,7 +360,7 @@ static symbol_list_t* Symbols_Load(const char *filename, uint32_t *offsets, uint
 		}
 		fprintf(stderr, "Reading symbols from program '%s' symbol table...\n", filename);
 		fp = fopen(filename, "rb");
-		list = symbols_load_binary(fp, &opts, update_sections);
+		list = symbols_load_binary(fp, offsets, update_sections, &opts);
 	} else {
 		fprintf(stderr, "Reading 'nm' style ASCII symbols from '%s'...\n", filename);
 		fp = fopen(filename, "r");
@@ -869,8 +887,8 @@ void Symbols_ShowCurrentProgramPath(FILE *fp)
  *
  * Assumes all (relevant) sections use the same load address.
  */
-static symbol_list_t *loadSymFile(const char *path, symtype_t symtype,
-				  uint32_t loadaddr, uint32_t maxaddr)
+static symbol_list_t *loadSymFile(const char *path, uint32_t *offsets,
+				  uint32_t maxaddr, symtype_t symtype)
 {
 	char symfile[PATH_MAX];
 	size_t len = strlen(path);
@@ -884,16 +902,15 @@ static symbol_list_t *loadSymFile(const char *path, symtype_t symtype,
 	if (!File_Exists(symfile)) {
 		return NULL;
 	}
-	fprintf(stderr, "Loading sym file: %s\n", symfile);
 
-	uint32_t offsets[3] = { loadaddr, loadaddr, loadaddr };
+	fprintf(stderr, "Loading sym file: %s\n", symfile);
 	return Symbols_Load(symfile, offsets, maxaddr, symtype);
 }
 
 /**
  * Load symbols for last opened program when symbol autoloading is enabled.
  *
- * When either textAddr or textEnd is zero, basepage values are used instead.
+ * When program section 'offsets' is NULL, basepage values are used instead.
  *
  * If there's file with same name as the program, but with '.sym'
  * extension, that overrides / is loaded instead of the symbol table
@@ -901,7 +918,7 @@ static symbol_list_t *loadSymFile(const char *path, symtype_t symtype,
  *
  * Called when debugger is invoked.
  */
-void Symbols_LoadCurrentProgram(uint32_t textAddr, uint32_t textEnd)
+void Symbols_LoadCurrentProgram(uint32_t *offsets, uint32_t textEnd)
 {
 	if (!ConfigureParams.Debugger.nSymbolsAutoLoad) {
 		return;
@@ -918,21 +935,19 @@ void Symbols_LoadCurrentProgram(uint32_t textAddr, uint32_t textEnd)
 		return;
 	}
 
-	uint32_t loadaddr, maxaddr;
-	if (textAddr & textEnd) {
-		loadaddr = textAddr;
+	uint32_t maxaddr;
+	if (offsets) {
 		maxaddr = textEnd;
 	} else {
-		loadaddr = DebugInfo_GetTEXT();
 		maxaddr = DebugInfo_GetTEXTEnd();
 	}
-	symbol_list_t *symbols;
 
-	symbols = loadSymFile(CurrentProgramPath, SYMTYPE_CODE, loadaddr, maxaddr);
+	symbol_list_t *symbols;
+	symbols = loadSymFile(CurrentProgramPath, offsets, maxaddr, SYMTYPE_CODE);
 	if (symbols) {
-		fprintf(stderr, "Symbols override loaded for: %s\n", CurrentProgramPath);
+		fprintf(stderr, "Symbols override file loaded for: %s\n", CurrentProgramPath);
 	} else {
-		symbols = Symbols_Load(CurrentProgramPath, NULL, 0,
+		symbols = Symbols_Load(CurrentProgramPath, offsets, maxaddr,
 				       SYMTYPE_CODE);
 	}
 	if (!symbols) {
@@ -965,8 +980,11 @@ void Symbols_LoadTOS(const char *path, uint32_t maxaddr)
 	if (CpuSymbolsList && CpuSymbolsAreFor == SYMBOLS_FOR_USER) {
 		return;
 	}
+
 	symbol_list_t *symbols;
-	symbols = loadSymFile(path, SYMTYPE_ALL, 0, maxaddr);
+	uint32_t offsets[3] = {0};
+
+	symbols = loadSymFile(path, offsets, maxaddr, SYMTYPE_ALL);
 	if (symbols) {
 		fprintf(stderr, "Loaded symbols for TOS: %s\n", path);
 		Symbols_UpdateCpu(symbols, SYMBOLS_FOR_TOS);

src/debug/debugui.c
@@ -1343,7 +1343,7 @@ void DebugUI(debug_reason_t reason)
 	}
 	DebugCpu_InitSession();
 	DebugDsp_InitSession();
-	Symbols_LoadCurrentProgram(0, 0);
+	Symbols_LoadCurrentProgram(NULL, 0);
 	DebugInfo_ShowSessionInfo();
 
 	/* override paused message so that user knows to look into console

src/debug/symbols-common.c
@@ -1393,8 +1393,9 @@ static bool symbols_print_prg_info(uint32_t tabletype, uint32_t prgflags, uint16
  *
  * Return symbols list or NULL for failure.
  */
-static symbol_list_t* symbols_load_binary(FILE *fp, const symbol_opts_t *opts,
-					  bool (*update_sections)(prg_section_t*))
+static symbol_list_t* symbols_load_binary(FILE *fp, uint32_t *offsets,
+					  bool (*update_sections)(uint32_t *, prg_section_t *),
+					  const symbol_opts_t *opts)
 {
 	uint32_t textlen, datalen, bsslen, tablesize, tabletype, prgflags;
 	prg_section_t sections[3];
@@ -1622,7 +1623,7 @@ static symbol_list_t* symbols_load_binary(FILE *fp, const symbol_opts_t *opts,
 	sections[1].end = datalen;
 	sections[2].end = bsslen;
 	/* add suitable offsets to section beginnings & ends, and validate them */
-	if (!update_sections(sections)) {
+	if (!update_sections(offsets, sections)) {
 		free(headers);
 		return NULL;
 	}

src/debug/symbols.h
@@ -55,7 +55,7 @@ extern int Symbols_DspCodeCount(void);
 extern void Symbols_RemoveCurrentProgram(void);
 extern void Symbols_ChangeCurrentProgram(const char *path);
 extern void Symbols_ShowCurrentProgramPath(FILE *fp);
-extern void Symbols_LoadCurrentProgram(uint32_t textAddr, uint32_t textEnd);
+extern void Symbols_LoadCurrentProgram(uint32_t *offsets, uint32_t textEnd);
 extern void Symbols_LoadTOS(const char *path, uint32_t maxaddr);
 extern void Symbols_FreeAll(void);
 /* symbols/dspsymbols command parsing */

src/gemdos.c
@@ -4379,7 +4379,7 @@ void GemDOS_Boot(void)
  * Load and relocate a PRG file into the memory of the emulated machine.
  */
 int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr,
-			uint32_t *textAddr, uint32_t *textEnd,
+			uint32_t *offsets, uint32_t *textEnd,
 			bool bFullBpSetup)
 {
 	long nFileSize, nRelTabIdx;
@@ -4438,15 +4438,18 @@ int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr,
 		return GEMDOS_EIMBA;
 	}
 
-	*textAddr = nTextAddr;
-	*textEnd = nTextAddr + nTextLen;
+	/* TEXT/DATA/BSS offsets */
+	offsets[0] = nTextAddr;
+	offsets[1] = offsets[0] + nTextLen;
+	offsets[2] = offsets[1] + nDataLen;
+	*textEnd = offsets[1];
 
 	/* Set up basepage */
-	STMemory_WriteLong(baseaddr + 8, nTextAddr);                        /* p_tbase */
-	STMemory_WriteLong(baseaddr + 12, nTextLen);                               /* p_tlen */
-	STMemory_WriteLong(baseaddr + 16, nTextAddr + nTextLen);            /* p_dbase */
-	STMemory_WriteLong(baseaddr + 20, nDataLen);                               /* p_dlen */
-	STMemory_WriteLong(baseaddr + 24, nTextAddr + nTextLen + nDataLen); /* p_bbase */
+	STMemory_WriteLong(baseaddr + 8, offsets[0]);   /* p_tbase */
+	STMemory_WriteLong(baseaddr + 12, nTextLen);    /* p_tlen */
+	STMemory_WriteLong(baseaddr + 16, offsets[1]);  /* p_dbase */
+	STMemory_WriteLong(baseaddr + 20, nDataLen);    /* p_dlen */
+	STMemory_WriteLong(baseaddr + 24, offsets[2]);  /* p_bbase */
 	STMemory_WriteLong(baseaddr + 28, nBssLen);                                /* p_blen */
 	/* In case we run without TOS, set some of the other values as good as possible, too */
 	if (bFullBpSetup)
@@ -4548,8 +4551,8 @@ void GemDOS_PexecBpCreated(void)
 	char sFileName[FILENAME_MAX];
 	char *sStFileName;
 	uint32_t errcode;
-	uint32_t textAddr = 0;
-	uint32_t textEnd = 0;
+	uint32_t offsets[3];
+	uint32_t textEnd;
 	uint16_t sr = M68000_GetSR();
 	uint16_t mode;
 	uint32_t prgname;
@@ -4570,7 +4573,7 @@ void GemDOS_PexecBpCreated(void)
 		GemDOS_CreateHardDriveFileName(drive, sStFileName, sFileName,
 		                               sizeof(sFileName));
 		errcode = GemDOS_LoadAndReloc(sFileName, Regs[REG_D0],
-					      &textAddr, &textEnd, false);
+					      offsets, &textEnd, false);
 	}
 	else
 	{
@@ -4594,7 +4597,7 @@ void GemDOS_PexecBpCreated(void)
 		/* whether to immediately load symbols for the executed program */
 		if (ConfigureParams.Debugger.nSymbolsAutoLoad == SYM_AUTOLOAD_EXEC)
 		{
-			Symbols_LoadCurrentProgram(textAddr, textEnd);
+			Symbols_LoadCurrentProgram(offsets, textEnd);
 		}
 
 		/* do user-configured program exec "event" actions */

src/includes/gemdos.h
@@ -30,7 +30,7 @@ extern void GemDOS_Info(FILE *fp, uint32_t bShowOpcodes);
 extern void GemDOS_InfoDTA(FILE *fp, uint32_t addrDTA);
 extern int GemDOS_Trap(void);
 extern void GemDOS_Boot(void);
-extern int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr, uint32_t *textAddr, uint32_t *textEnd, bool bFullBpSetup);
+extern int GemDOS_LoadAndReloc(const char *psPrgName, uint32_t baseaddr, uint32_t *offsets, uint32_t *textEnd, bool bFullBpSetup);
 extern void GemDOS_PexecBpCreated(void);
 
 #endif /* HATARI_GEMDOS_H */

src/tos.c
@@ -1266,10 +1266,10 @@ int TOS_InitImage(void)
 		/* Load test program (has to be done after memory has been cleared) */
 		if (psTestPrg)
 		{
-			uint32_t dummy1, dummy2;
+			uint32_t offsets[3], dummy;
 			Log_Printf(LOG_DEBUG, "Loading '%s' to 0x%x.\n",
 			           psTestPrg, TEST_PRG_START);
-			if (GemDOS_LoadAndReloc(psTestPrg, TEST_PRG_BASEPAGE, &dummy1, &dummy2, true))
+			if (GemDOS_LoadAndReloc(psTestPrg, TEST_PRG_BASEPAGE, offsets, &dummy, true))
 			{
 				Main_ErrorExit("Failed to load:", psTestPrg, 1);
 			}

tools/debugger/gst2ascii.c
@@ -109,7 +109,7 @@ static void usage(const char *msg)
  * Sections just follow each other, so just add their sizes
  * (initially in .end fields) to successive fields and return true
  */
-static bool update_sections(prg_section_t *sections)
+static bool update_sections(uint32_t *dummy, prg_section_t *sections)
 {
 	uint32_t offset;
 	sections[0].offset = 0;
@@ -132,6 +132,7 @@ static bool update_sections(prg_section_t *sections)
  */
 static symbol_list_t* symbols_load(const char *filename, const symbol_opts_t *opts)
 {
+	uint32_t offsets[3] = {0};
 	symbol_list_t *list;
 	uint16_t magic;
 	FILE *fp;
@@ -148,7 +149,7 @@ static symbol_list_t* symbols_load(const char *filename, const symbol_opts_t *op
 	if (be_swap16(magic) != ATARI_PROGRAM_MAGIC) {
 		usage("file isn't an Atari program file");
 	}
-	list = symbols_load_binary(fp, opts, update_sections);
+	list = symbols_load_binary(fp, offsets, update_sections, opts);
 	fclose(fp);
 
 	if (!list || !list->namecount) {

commit 3791b1b51bc6153f1efb8198c8eb0d18c1bef84d

symbols: fix crash when loading program symbols from a separate file
Fixes: 720861794fa

src/debug/symbols.c
@@ -365,6 +365,18 @@ static symbol_list_t* Symbols_Load(const char *filename, const uint32_t *offsets
 		fp = fopen(filename, "rb");
 		list = symbols_load_binary(fp, offsets, update_sections, &opts);
 	} else {
+		uint32_t prg_offsets[3];
+		if (!offsets) {
+			/* while symbol offsets within program binary are
+			 * relative to given section, symbol offsets in
+			 * ASCII files are all from TEXT start
+			 */
+			uint32_t textAddr =  DebugInfo_GetTEXT();
+			prg_offsets[0] = textAddr;
+			prg_offsets[1] = textAddr;
+			prg_offsets[2] = textAddr;
+			offsets = prg_offsets;
+		}
 		fprintf(stderr, "Reading 'nm' style ASCII symbols from '%s'...\n", filename);
 		fp = fopen(filename, "r");
 		list = symbols_load_ascii(fp, offsets, maxaddr, gettype, &opts);