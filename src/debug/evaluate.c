hatari/hatari

commit 17c930e03e7088aacc1205e9df956b04e828e591

debugger: add C++ method name matching fallback for CPU symbols
For conditional breakpoints and when evaluating debugger expressions.

Unmangled C++ symbols / full method signatures can contain nearly any
characters, so conditional breakpoints and expression evalution would
reject them with a syntax error.

This adds fallback for doing partial method name symbol matching.

src/debug/evaluate.c
@@ -197,6 +197,17 @@ static int getNumber(const char *str, uint32_t *number, int *nbase)
 	return end - start;
 }
 
+/**
+ * '_' - for normal C symbols
+ * ':' - partial C++ symbol support
+ */
+static bool is_special(char c)
+{
+	if (c == '_' || c == ':') {
+		return true;
+	}
+	return false;
+}
 
 /**
  * Parse unsigned register/symbol/number value and set it to "number"
@@ -210,7 +221,7 @@ static int getValue(const char *str, uint32_t *number, int *base, bool bForDsp)
 	uint32_t mask, *addr;
 	int len;
 
-	for (end = str; *end == '_' || isalnum((unsigned char)*end); end++);
+	for (end = str; is_special(*end) || isalnum((unsigned char)*end); end++);
 
 	len = end-str;
 	if (len >= (int)sizeof(name)) {
@@ -258,6 +269,10 @@ static int getValue(const char *str, uint32_t *number, int *base, bool bForDsp)
 			return len;
 		}
 		if (Symbols_GetCpuAddress(SYMTYPE_ALL, name, number)) {
+			return len;
+		}
+		/* exact match failed => try partial matching */
+		if (Symbols_GetCpuMethodAddress(SYMTYPE_ALL, name, number)) {
 			return len;
 		}
 	}

doc/debugger.html
@@ -518,12 +518,39 @@ $ gst2ascii scummvm.ttp | m68k-atari-mint-c++filt &gt; scummvm.sym
 <p>(When symbols are in a file named as "&lt;prgname&gt;.sym", Hatari
 debugger will load symbols from it, not from the program file.)
 
-<p>Note: many demangled C++ symbols contain special characters
-which prevent them from being given as arguments to breakpoints and
-other debugger commands.  One should use (resolved) symbol address
-instead for those commands.</p>
-<p>Addresses for symbols that match user specified substring,
-can be listed like this:</p>
+<p>Note: most demangled C++ symbols, such as method signatures, contain
+special characters which prevent them from being used as arguments to
+breakpoints and other debugger commands. To solve that, debugger
+commands can accept also a part of a C++ method symbol name, if it
+is a complete sub-part of that name, and it matches only a single
+symbol.
+</p>
+
+<p>For example, given the following C++ symbols:</p>
+<pre>
+- void ScummEngine::startScriptQuick()
+- int ScummEngine::startScriptQuick2(int, int)
+- int ScummEngine::startObject(int, char*)
+</pre>
+<p>Following partial names would be accepted:</p>
+<pre>
+- startScriptQuick
+- ScummEngine::startObject
+</pre>
+<p>But these would not:</p>
+<pre>
+- ScummEngine (multiple matches)
+- startObj    (incomplete symbol part match)
+</pre>
+
+<p>This leaves out destructor, template and operator overloading
+method names because those contain characters already reserved for
+breakpoint and debugger expression operations (and without those
+characters, partial name would produce multiple matches).</p>
+
+<p>For rest of the demangled symbols, one needs to use symbol addresses
+instead. Addresses for the loaded symbols (that match user specified
+substring), can be listed like this:</p>
 <pre>
 symbols name XMLParser::parse
 </pre>

src/debug/breakcond.c
@@ -519,8 +519,11 @@ static bool BreakCond_ParseSymbol(const char *name, bc_value_t *bc_value)
 	}
 
 	if (!Symbols_GetCpuAddress(symtype, name, &addr)) {
-		EXITFUNC(("-> false (CPU)\n"));
-		return false;
+		/* try partial (C++) symbol matching */
+		if (!Symbols_GetCpuMethodAddress(symtype, name, &addr)) {
+			EXITFUNC(("-> false (CPU)\n"));
+			return false;
+		}
 	}
 	if (addr & 1) {
 		/* only bytes can be at odd addresses */
@@ -1077,8 +1080,9 @@ static char *BreakCond_TokenizeExpression(const char *expression,
 		}
 		/* validate & copy other characters */
 		if (!sep) {
-			/* variable/register/symbol or number prefix? */
-			if (!(isalnum((unsigned char)*src) || *src == '_' ||
+			/* register/variable/symbol/C++/num-prefix? */
+			if (!(isalnum((unsigned char)*src) ||
+			      *src == '_' || *src == ':' ||
 			      *src == '$' || *src == '#' || *src == '%')) {
 				pstate->error = "invalid character";
 				pstate->arg = src-expression;
@@ -1546,18 +1550,18 @@ const char BreakCond_Description[] =
 static bool BreakCond_Options(char *str, bc_options_t *options, char marker)
 {
 	char *option, *next, *filename, *info;
+	char splitter[3] = {' ', marker, '\0'};
 	int skip;
 
 	memset(options, 0, sizeof(*options));
 
-	option = strchr(str, marker);
+	option = strstr(str, splitter);
 	if (option) {
-		/* end breakcond command at options */
-		*option = 0;
+		/* end breakcond command at options + skip space */
+		*option++ = 0;
 	}
 	for (next = option; option; option = next) {
 
-		char splitter[3] = {' ', marker, '\0'};
 		/* skip marker + end & trim this option */
 		option = next + 1;
 		next = strstr(option, splitter);
@@ -1753,9 +1757,9 @@ bool BreakAddr_Command(char *args, bool bForDsp)
 	}
 
 	/* split options */
-	if ((cut = strchr(args, ':'))) {
-		*cut = '\0';
-		cut = Str_Trim(cut+1);
+	if ((cut = strstr(args, " :"))) {
+		cut[1] = '\0';
+		cut = Str_Trim(cut+2);
 		if (strlen(cut) > 5) {
 			cut[5] = '\0';
 		}

src/debug/symbols.c
@@ -22,6 +22,7 @@ const char Symbols_fileid[] = "Hatari symbols.c";
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <fnmatch.h>
 #include <assert.h>
 
 #include "config.h"
@@ -651,6 +652,78 @@ bool Symbols_GetCpuAddress(symtype_t symtype, const char *name, uint32_t *addr)
 bool Symbols_GetDspAddress(symtype_t symtype, const char *name, uint32_t *addr)
 {
 	return Symbols_GetAddress(DspSymbolsList, symtype, name, addr);
+}
+
+/**
+ * Return true if char is valid for a symbol name, false otherwise
+ */
+static bool is_sym_char(char c)
+{
+	if (c == '_' || isalnum(c)) {
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Match (C++ method) symbol name substring, if it matches a specific
+ * part of it completely, i.e. is not surrounded by valid sym char(s).
+ *
+ * (See debugger manual for details.)
+ *
+ * If there's exactly one match, set symbol's address to 'addr'
+ * variable and return true, return false otherwise.
+ */
+bool Symbols_GetCpuMethodAddress(symtype_t symtype, const char *sub, uint32_t *addr)
+{
+	int matches = 0, sublen = strlen(sub);
+	symbol_list_t* list = CpuSymbolsList;
+	const symbol_t *entry;
+
+	if (!(list && list->names)) {
+		return false;
+	}
+
+	assert(addr && sub && *sub);
+
+	entry = list->names;
+	for (int i = 0; i < list->namecount; i++) {
+		const char *start, *name;
+		int offset, namelen;
+
+		/* correct type? */
+		if (!(entry[i].type & symtype)) {
+			continue;
+		}
+
+		/* substring? */
+		name = entry[i].name;
+		if (!(start = strstr(name, sub))) {
+			continue;
+		}
+
+		/* full method name part? */
+		offset = start - name;
+		if (offset > 0 && is_sym_char(name[offset-1])) {
+			continue;
+		}
+		offset += sublen;
+		namelen = strlen(name);
+		if (namelen > offset && is_sym_char(name[offset])) {
+			continue;
+		}
+
+		if (matches) {
+			fprintf(stderr, "Multiple matches for method substring '%s'.\n", sub);
+			return false;
+		}
+
+		/* first match */
+		*addr = entry[i].address;
+		matches++;
+	}
+
+	return (matches == 1);
 }
 
 

src/debug/symbols.h
@@ -40,6 +40,7 @@ extern char* Symbols_MatchDspDataAddress(const char *text, int state);
 /* symbol name -> address search */
 extern bool Symbols_GetCpuAddress(symtype_t symtype, const char *name, uint32_t *addr);
 extern bool Symbols_GetDspAddress(symtype_t symtype, const char *name, uint32_t *addr);
+extern bool Symbols_GetCpuMethodAddress(symtype_t symtype, const char *prefix, uint32_t *addr);
 /* symbol address -> name search */
 extern const char* Symbols_GetByCpuAddress(uint32_t addr, symtype_t symtype);
 extern const char* Symbols_GetByDspAddress(uint32_t addr, symtype_t symtype);

commit f4effd87649dadd0dfc4a67c4050565d18141d76

debugger: fix assert added with Symbols_GetCpuMethodAddress()
Evaluate command gets (also) number strings like "$22", which do not
match to special string chars, and therefore triggered empty string
assert in the new Symbols_GetCpuMethodAddress() function.

Fixed by checking evaluated special strings only when name string is
not empty.

Fixes: 17c930e03e7088aacc

src/debug/evaluate.c
@@ -1,7 +1,7 @@
 /*
   Hatari - evaluate.c
 
-  Copyright (C) 1994, 2009-2014, 2022-2023 by Eero Tamminen
+  Copyright (C) 1994, 2009-2026 by Eero Tamminen
 
   This file is distributed under the GNU General Public License, version 2
   or at your option any later version. Read the file gpl.txt for details.
@@ -210,6 +210,62 @@ static bool is_special(char c)
 }
 
 /**
+ * Check if item is Hatari variable CPU/DSP register name
+ * exact symbol match, or a partial C++ method symbol name.
+ * Return true if that's the case and set 'number' value.
+ */
+static bool getSpecial(const char *name, uint32_t *number, bool bForDsp)
+{
+	uint32_t mask, *addr;
+
+	/* internal Hatari variable? */
+	if (Vars_GetVariableValue(name, number)) {
+		return true;
+	}
+
+	if (bForDsp) {
+		int regsize = DSP_GetRegisterAddress(name, &addr, &mask);
+		/* DSP register or symbol? */
+		switch (regsize) {
+		case 16:
+			*number = (*((uint16_t*)addr) & mask);
+			return true;
+		case 32:
+			*number = (*addr & mask);
+			return true;
+		default:
+			if (Symbols_GetDspAddress(SYMTYPE_ALL, name, number)) {
+				return true;
+			}
+		}
+	} else {
+		/* a special case CPU register? */
+		if (strcasecmp(name, "PC") == 0) {
+			*number = M68000_GetPC();
+			return true;
+		}
+		if (strcasecmp(name, "SR") == 0) {
+			*number = M68000_GetSR();
+			return true;
+		}
+		/* a normal CPU  register or symbol? */
+		if (DebugCpu_GetRegisterAddress(name, &addr)) {
+			*number = *addr;
+			return true;
+		}
+		if (Symbols_GetCpuAddress(SYMTYPE_ALL, name, number)) {
+			return true;
+		}
+		/* exact match failed => try partial matching */
+		if (Symbols_GetCpuMethodAddress(SYMTYPE_ALL, name, number)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
  * Parse unsigned register/symbol/number value and set it to "number"
  * and the number base used for parsing to "base".
  * Return how many characters were parsed or zero for error.
@@ -218,7 +274,6 @@ static int getValue(const char *str, uint32_t *number, int *base, bool bForDsp)
 {
 	char name[64];
 	const char *end;
-	uint32_t mask, *addr;
 	int len;
 
 	for (end = str; is_special(*end) || isalnum((unsigned char)*end); end++);
@@ -233,48 +288,8 @@ static int getValue(const char *str, uint32_t *number, int *base, bool bForDsp)
 
 	*base = 0; /* no base (e.g. variable) */
 
-	/* internal Hatari variable? */
-	if (Vars_GetVariableValue(name, number)) {
+	if (len > 0 && getSpecial(name, number, bForDsp)) {
 		return len;
-	}
-
-	if (bForDsp) {
-		int regsize = DSP_GetRegisterAddress(name, &addr, &mask);
-		/* DSP register or symbol? */
-		switch (regsize) {
-		case 16:
-			*number = (*((uint16_t*)addr) & mask);
-			return len;
-		case 32:
-			*number = (*addr & mask);
-			return len;
-		default:
-			if (Symbols_GetDspAddress(SYMTYPE_ALL, name, number)) {
-				return len;
-			}
-		}
-	} else {
-		/* a special case CPU register? */
-		if (strcasecmp(name, "PC") == 0) {
-			*number = M68000_GetPC();
-			return len;
-		}
-		if (strcasecmp(name, "SR") == 0) {
-			*number = M68000_GetSR();
-			return len;
-		}
-		/* a normal CPU  register or symbol? */
-		if (DebugCpu_GetRegisterAddress(name, &addr)) {
-			*number = *addr;
-			return len;
-		}
-		if (Symbols_GetCpuAddress(SYMTYPE_ALL, name, number)) {
-			return len;
-		}
-		/* exact match failed => try partial matching */
-		if (Symbols_GetCpuMethodAddress(SYMTYPE_ALL, name, number)) {
-			return len;
-		}
 	}
 
 	/* none of above, assume it's a number */