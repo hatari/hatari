b7b0885eb885041c700fc4a27633375c34edc0be
Nicolas Pomarède <npomarede@corp.free.fr>
2026-09-25 21:48:32 +0200

    Make CHK2/CMP2 N/V flag probes wrap-safe (signed overflow UB) (PR from midwan) (from WinUAE 6.1.0 beta12+ 2026/08/29)

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -5216,7 +5216,7 @@ static int do_specialties (int cycles)
 #endif
 
 #ifndef WINUAE_FOR_HATARI
-	while ((spcflags & SPCFLAG_BLTNASTY) && dmaen (DMA_BLITTER) && cycles > 0 && ((currprefs.waiting_blits && currprefs.cpu_model >= 68020) || !currprefs.blitter_cycle_exact)) {
+	while ((spcflags & SPCFLAG_BLTNASTY) && dmaen (DMA_BLITTER) && cycles > 0 && ((currprefs.waiting_blits && currprefs.cpu_model >= 68020) || !blitter_cycle_exact)) {
 		int c = blitnasty();
 		if (c < 0) {
 			break;
